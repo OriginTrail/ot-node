64efd221ee93c06994873323b7ad9712ecfd1782
v.lelicanin <v.lelicanin@sae.edu>
2018-02-19 01:29:31 +0100

    python .env

--- importers/default_importer.py
+++ importers/default_importer.py
@@ -11,7 +11,7 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = join(dirname(__file__), '..','.env')
 load_dotenv(dotenv_path)
 
 from arango import ArangoClient
