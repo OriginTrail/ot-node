b800544b065faef510ad10f7de0a374896e46bae
Your Name <you@example.com>
2018-02-19 14:18:00 +0100

    install script fix

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y python3-pip
 
 pip3 install python-arango
 pip3 install xmljson
-pip install python-dotenv
+pip3 install python-dotenv
 
 git clone https://github.com/OriginTrail/ot-node.git
 cd ot-node
