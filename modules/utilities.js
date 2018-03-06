bc1f0be75f88ad6c060f90bc74c83522c6ebefe4
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-03-06 18:44:07 +0100

    escrow

--- modules/utilities.js
+++ modules/utilities.js
@@ -66,8 +66,8 @@ module.exports = function () {
 			return sortedObj;
 		},
 
-		getRandomIntRange: function (min, max) {
-			return min + Math.floor(Math.random() * Math.floor(max));
+		getRandomInt: function (min, max) {
+		  return min + Math.floor(Math.random() * Math.floor(max));
 		}
 	};
 
