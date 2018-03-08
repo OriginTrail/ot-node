ee44c44a4c12fcd04969d33293e3cb2d8359f28b
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-03-08 17:02:15 +0100

    holding fix

--- modules/holding.js
+++ modules/holding.js
@@ -9,53 +9,77 @@ module.exports = function(){
 
 				if(response.length == 0)
 				{
-					response = {};
-					response[dh_wallet] = [];
+					response = {data:[]};
 				}
 				
-				response[dh_wallet][data_id] = {data_id: data_id, public_key: public_key, confirmation_number: 0};
+				for(let i in response.data) {
+					
+					if (response.data[i].dh_wallet == dh_wallet && response.data[i].data_id == data_id)
+					{
+						utilities.executeCallback(callback, true);
+						return;
+					}
+				}
+
+				new_data = {dh_wallet: dh_wallet, data_id: data_id, public_key: public_key, confirmation_number: 0};
+				console.log(JSON.stringify(new_data))
+				response.data.push(new_data);
 
 				storage.storeObject('Holding', response, function(status){
 					utilities.executeCallback(callback, true);
-				});
+				})
 
-			});
+			})
 		},
 
 		getHoldingData: function(dh_wallet, data_id, callback) {
 			storage.getObject('Holding', function(response) {
 
-				if(response.length == 0 || response[dh_wallet][data_id] == undefined) {
-					utilities.executeCallback(callback, {});
-					return;
-				}
-				else {
-					utilities.executeCallback(callback, response[dh_wallet][data_id]);
+					if(response.length == 0 || response.data.length == 0) {
+						utilities.executeCallback(callback, {});
+						return;
+					}
+					
+					for(let i in response.data) {
+
+						if (response.data[i].dh_wallet == dh_wallet && response.data[i].data_id == data_id)
+						{
+							utilities.executeCallback(callback, response.data[i]);
+							return;
+						}
+
 				}
 
-			});
+				utilities.executeCallback(callback, {});
+
+			})
 		},	
 
 		increaseConfirmationVerificationNumber: function(dh_wallet, data_id, callback) {
 			storage.getObject('Holding', function(response) {
 
-				if(response.length == 0 || response[dh_wallet][data_id] == undefined)
-				{
-					utilities.executeCallback(callback, false);
-					return;
-				}
+					if(response.length == 0)
+					{
+						utilities.executeCallback(callback, false);
+						return;
+					}
+
+					for(let i in response.data) {
 					
-				console.log(response[dh_wallet][data_id]);
-				response[dh_wallet][data_id].confirmation_number += 1;
+						if (response.data[i].dh_wallet == dh_wallet && response.data[i].data_id == data_id)
+						{
+							response.data[i].confirmation_number += 1;
+						}
+					}
 
-				storage.storeObject('Holding', response, function(status){
-					utilities.executeCallback(callback, true);
-				});
+					storage.storeObject('Holding',response, function(status){
+						utilities.executeCallback(callback, true);
+					})
 
-			});
+				})
 		}
 	};
 
-	return holding;
+	return holding
 
-};
+}
