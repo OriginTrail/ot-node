dd1dcf73dcd6d37019335232e0f46a0e1a153e41
Vladimir <v.lelicanin@sae.edu>
2018-03-09 14:58:33 +0100

    logging

--- modules/SendTest.js
+++ modules/SendTest.js
@@ -31,10 +31,10 @@ class SendTests {
 	checkTests() {
 		log.info('Checking if there are tests to send');
 		testTable.getTests((test) => {
+
+			if(test.length === 0) return;
 			log.info('All tests:');
 			log.info(test);
-			if(test.length === 0) return;
-
 			let currentUnixTime = Math.floor(new Date() / 1000);
 
 			if (currentUnixTime > test.test_time) {
@@ -106,7 +106,7 @@ class SendTests {
 		} else {
 			log.warning('Answer not good');
 		}
-    }
+	}
 
 
 	createReceipt() {
--- modules/test_table.js
+++ modules/test_table.js
@@ -88,6 +88,7 @@ module.exports = function () {
 		getTests: function(callback) {
 			storage.getObject('Tests', function(response) {
 				if(response.length == 0) {
+					log.info('No tests it LevelDB');
 					utilities.executeCallback(callback, []);
 				} else {
 					utilities.executeCallback(callback, response);
