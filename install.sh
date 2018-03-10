f8803ee96eb214933ac88c069299ee34df3a866e
Vladimir Lelicanin <v.lelicanin@sae.edu>
2018-03-10 21:32:26 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ sudo apt-add-repository 'deb https://www.arangodb.com/repositories/arangodb3/xUb
 sudo apt-get update -y
 sudo apt-get install arangodb3
 
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
 sudo apt-get install -y python3-pip
 
 sudo pip3 install python-arango
