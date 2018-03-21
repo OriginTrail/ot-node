be2930c51e7b62f204cce73a3dba71d3961b645f
radomir.sebek <rasho.ieee@gmail.com>
2018-03-21 22:50:20 +0100

    adding test for createVertexCollection()

--- test/modules/database.test.js
+++ test/modules/database.test.js
@@ -3,15 +3,16 @@ const {
 } = require('mocha');
 const { assert, expect } = require('chai');
 const utilities = require('../../modules/utilities')();
+
+const config = utilities.getConfig();
 const database = require('../../modules/database')();
 // eslint-disable-next-line  prefer-destructuring
 const Database = require('arangojs').Database;
 
-const DB_USERNAME = 'otuser';
-const DB_PASSWORD = 'otpass';
-const DB_HOST = 'localhost';
-const DB_PORT = 8529;
-const DB_DATABASE = 'otnode';
+const myUserName = config.DB_USERNAME;
+const myPassword = config.DB_PASSWORD;
+const myDatabase = config.DB_DATABASE;
+const documentCollectionName = 'ot_vertices';
 
 let systemDb;
 let otnode;
@@ -21,8 +22,8 @@ describe.only('Database module ', async () => {
     before('create and use otnode db', async () => {
         systemDb = new Database();
         otnode = await systemDb.createDatabase(
-            DB_DATABASE,
-            [{ username: DB_USERNAME, passwd: DB_PASSWORD, active: true }],
+            myDatabase,
+            [{ username: myUserName, passwd: myPassword, active: true }],
         );
         // this should start using otnode
         db = database.getConnection();
@@ -30,7 +31,7 @@ describe.only('Database module ', async () => {
 
     after('drop otnode db', async () => {
         systemDb = new Database();
-        await systemDb.dropDatabase(DB_DATABASE);
+        await systemDb.dropDatabase(myDatabase);
     });
 
     it('some basics', async () => {
@@ -40,10 +41,30 @@ describe.only('Database module ', async () => {
         assert.equal(listOfDatabases[1], 'otnode');
     });
 
-    it('run a simple query', async () => {
+    it('.runQuery() should give back result', async () => {
         const now = Date.now();
+
         await database.runQuery('RETURN @value', (result) => {
-            console.log(result);
+            assert.approximately(result[0], now, 1000, 'Resulted time is approx same as current');
         }, { value: now });
+    });
+
+    it('.createVertexCollection() should create Document Collection', async () => {
+        // first time creating Document Collection
+        await database.createVertexCollection(documentCollectionName, (response) => {
+            assert.equal(response, true, 'collection should be created');
+        });
+
+        // this will cover 409 path
+        await database.createVertexCollection(documentCollectionName, (response) => {
+            assert.equal(response, true, 'collection has already been created');
+        });
+
+        const myCollection = db.collection(documentCollectionName);
+        const data = await myCollection.get();
+
+        assert.equal(data.code, 200);
+        assert.isFalse(data.isSystem);
+        assert.equal(data.name, documentCollectionName);
     });
 });
