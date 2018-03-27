fe7b58c6248e280d313bacb3140a2052c593c247
radomir.sebek <rasho.ieee@gmail.com>
2018-03-27 12:31:45 +0200

    adding .js file to lint check

--- isStartHealthy.js
+++ isStartHealthy.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 
-fs.readFile("start.log", function (err, data) {
+fs.readFile('start.log', (err, data) => {
     if (err) throw err;
-    if(data.indexOf('OriginTrail RPC server listening at http://[::]:8888') >= 0){
-        console.log("npm start is healthy!");
+    if (data.indexOf('OriginTrail RPC server listening at http://[::]:8888') >= 0) {
+        console.log('npm start is healthy!');
         process.exit(0);
     } else {
-        console.log("npm start is not healthy!")
-        console.log("RPC server did not start listening as expected, check npm start localy");
+        console.log('npm start is not healthy!');
+        console.log('RPC server did not start listening as expected, check npm start localy');
         process.exit(-1);
     }
-  });
+});
