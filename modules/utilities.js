1a318520fb391b4cb624a788088b3f9a9f18af93
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-03-30 11:03:47 +0200

    changes

--- modules/utilities.js
+++ modules/utilities.js
@@ -1,16 +1,17 @@
 const SystemStorage = require('./Database/systemStorage');
+var logger = require('winston');
 
 class Utilities {
-
     /**
      * Get configuration parameters from SystemStorage database, table node_config
      * @returns {Promise<void>}
      */
-    static getConfig() {
+    static loadConfig() {
         return new Promise((resolve, reject) => {
             const db = new SystemStorage();
             db.connect().then(() => {
                 db.runSystemQuery('SELECT * FROM node_config', []).then((rows) => {
+                    [this.config] = rows;
                     resolve(rows[0]);
                 }).catch((err) => {
                     reject(err);
@@ -22,14 +23,30 @@ class Utilities {
     }
 
     /**
+     * Returns winston logger
+     * @returns {*} - log function
+     */
+    static getLogger() {
+        try {
+            logger.add(logger.transports.File, { filename: 'log.log', colorize: true, prettyPrint: true });
+            logger.remove(logger.transports.Console);
+            logger.add(logger.transports.Console, { colorize: true });
+        } catch (e) {
+            console.log(e);
+        }
+        return logger;
+    }
+
+    /**
      * Get information of selected graph storage database
      * @returns {Promise<any>}
      */
-    static getSelectedDatabaseInfo() {
+    static loadSelectedDatabaseInfo() {
         return new Promise((resolve, reject) => {
             const db = new SystemStorage();
             db.connect().then(() => {
                 db.runSystemQuery('SELECT gd.* FROM node_config AS nc JOIN graph_database gd ON nc.selected_graph_database = gd.id', []).then((rows) => {
+                    [this.selectedDatabase] = rows;
                     resolve(rows[0]);
                 }).catch((err) => {
                     reject(err);
@@ -38,6 +55,22 @@ class Utilities {
                 reject(err);
             });
         });
+    }
+
+    static getConfig() {
+        if (!this.config) {
+            throw Error('Configuration not loaded from system database');
+        } else {
+            return this.config;
+        }
+    }
+
+    static getSelectedDatabaseInfo() {
+        if (!this.config) {
+            throw Error('Configuration not loaded from system database');
+        } else {
+            return this.config;
+        }
     }
 }
 
--- ot-node.js
+++ ot-node.js
@@ -1,16 +1,49 @@
 const Utilities = require('./modules/utilities');
 const GraphStorage = require('./modules/Database/graphStorage');
 
-Utilities.getConfig().then((response) => {
-    //   console.log(response);
+const log = Utilities.getLogger();
+
+var config;
+var graphDB;
+
+
+class OTNode {
+
+    bootstrap() {
+        return new Promise((resolve, reject) => {
+            Utilities.loadConfig().then((response) => {
+                config = Utilities.getConfig();
+                graphDB = new GraphStorage();
+
+                graphDB.connect().then((response) => {
+                    resolve('OK');
+
+                }).catch((err) => {
+                    console.log(err);
+                    reject(err);
+                });
+            }).catch((err) => {
+                console.log(err);
+                reject(err);
+            });
+        });
+    }
+}
+
+const otNode = new OTNode();
+otNode.bootstrap().then((response) => {
+    console.log(response);
+    console.log(config);
+    console.log(graphDB);
 }).catch((err) => {
     console.log(err);
-});
+})
 
+/*
 const graphDB = new GraphStorage();
 graphDB.connect().then((response) => {
     //  console.log(response);
-    graphDB.addDocument('ot_vertices', {_key:'123'}).then(() => {
+    graphDB.addDocument('ot_vertices', { _key: '123' }).then(() => {
         graphDB.runQuery('FOR v IN ot_vertices RETURN v._key').then((result) => {
             console.log(result);
         }).catch((err) => {
@@ -22,3 +55,4 @@ graphDB.connect().then((response) => {
 }).catch((err) => {
     console.log(err);
 });
+*/
