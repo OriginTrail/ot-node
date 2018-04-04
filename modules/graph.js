804c4fcd2e7d864f6d9701298a84d3e82cb6f9e7
Janko Simonovic <janko.simonovic@sbgenomics.com>
2018-04-04 15:20:10 +0200

    fix bfs, add more tests

--- modules/graph.js
+++ modules/graph.js
@@ -1,63 +1,74 @@
 class Graph {
-
     /**
      * Traversing through the trail graph in Breadth-first manner
+     * @param {object}  - trailGraph
+     * @param {string}  - startVertexUID
+     * @param {boolean} - restrictToOneBatch
+     * @returns {Array} - traversal path
      */
-    static bfs(trail, start_vertex_uid, restricted = false) {
-        const visited = [];
-        const traversalArray = [];
+    static bfs(trailGraph, startVertexUID, restrictToOneBatch = false) {
+        if (trailGraph == null) {
+            return []; // return empty traversal path
+        }
 
-        let start_vertex = null;
-
-        for (const i in trail) {
-            if (trail[i].identifiers.uid === start_vertex_uid) {
-                start_vertex = i;
+        let startVertexId = null;
+        for (const id in trailGraph) {
+            if (trailGraph[id].identifiers.uid === startVertexUID) {
+                startVertexId = id;
                 break;
             }
         }
-
-        if (start_vertex !== null) {
-            const queue = [];
-            queue.push(start_vertex);
-
-            visited[start_vertex] = true;
-
-            while (queue.length > 0) {
-                const curr = queue.shift();
-
-                if (trail[curr] === undefined) {
-                    // eslint-disable-next-line no-continue
-                    continue;
-                }
-
-                traversalArray.push(trail[curr]);
-
-                for (const i in trail[curr].outbound) {
-                    const e = trail[curr].outbound[i];
-                    const w = e.to;
-
-                    if (restricted && e.edge_type !== 'TRANSACTION_CONNECTION') {
-                        traversalArray.push(e);
-                    }
-
-                    if (visited[w] === undefined && trail[w] !== undefined && !(e.edge_type === 'TRANSACTION_CONNECTION' && e.TransactionFlow === 'Output') && (restricted === false || (restricted === true && trail[w].vertex_type !== 'BATCH' && e.edge_type !== 'TRANSACTION_CONNECTION'))) {
-                        visited[w] = true;
-                        queue.push(w);
-                    }
-                }
-            }
-
-            for (const i in traversalArray) {
-                // eslint-disable-next-line no-underscore-dangle
-                if (traversalArray[i]._checked !== undefined) {
-                    // eslint-disable-next-line no-underscore-dangle
-                    delete traversalArray[i]._checked;
-                }
-            }
-
-            return traversalArray;
+        if (startVertexId == null) {
+            return []; // return empty traversal path
         }
-        return traversalArray;
+
+        const visitedIds = [];
+        const traversalPath = [];
+
+        const queueToExplore = [];
+        queueToExplore.push(startVertexId);
+        visitedIds[startVertexId] = true;
+
+        while (queueToExplore.length > 0) {
+            const currentVertexId = queueToExplore.shift();
+
+            const currentVertex = trailGraph[currentVertexId];
+            if (currentVertex !== undefined) {
+                traversalPath.push(currentVertex);
+
+                for (const edgeId in currentVertex.outbound) {
+                    const edge = currentVertex.outbound[edgeId];
+                    const toVertexId = edge.to;
+
+                    if (edge.edge_type !== 'TRANSACTION_CONNECTION') {
+                        traversalPath.push(edge);
+                    }
+
+                    if (visitedIds[toVertexId] !== undefined) {
+                        // eslint-disable-next-line no-continue
+                        continue; // it's already visited
+                    }
+
+                    const toVertex = trailGraph[toVertexId];
+                    if (toVertex === undefined) {
+                        // eslint-disable-next-line no-continue
+                        continue; // it doesn't exist (should not happen)
+                    }
+
+                    // don't follow output flow
+                    if (edge.edge_type === 'TRANSACTION_CONNECTION' && edge.TransactionFlow === 'Output') {
+                        // eslint-disable-next-line no-continue
+                        continue; // don't follow output edges
+                    }
+
+                    if (restrictToOneBatch === false || (toVertex.vertex_type !== 'BATCH' && edge.edge_type !== 'TRANSACTION_CONNECTION')) {
+                        visitedIds[toVertexId] = true;
+                        queueToExplore.push(toVertexId);
+                    }
+                }
+            }
+        }
+        return traversalPath;
     }
 }
 
--- test/modules/graph.test.js
+++ test/modules/graph.test.js
@@ -1,12 +1,11 @@
-const { describe, before, it } = require('mocha');
-const { assert, expect } = require('chai');
+const { describe, it } = require('mocha');
+const { assert } = require('chai');
 
 const Graph = require('../../modules/graph');
 
 describe('graph module ', () => {
     it('BFS empty graph', () => {
-        const test_raw_graph = {
-        };
+        const test_raw_graph = {};
         const traversal = Graph.bfs(test_raw_graph, 1111, false);
         assert.equal(traversal.length, 0);
     });
@@ -41,7 +40,7 @@ describe('graph module ', () => {
             },
         };
         const traversal = Graph.bfs(test_raw_graph, 1111, false);
-        assert.equal(traversal.length, 4);
+        assert.equal(traversal.length, 7);
         assert.deepEqual(traversal, [
             {
                 identifiers: {
@@ -49,6 +48,8 @@ describe('graph module ', () => {
                 },
                 outbound: [{ to: 2 }, { to: 4 }],
             },
+            { to: 2 },
+            { to: 4 },
             {
                 identifiers: {
                     uid: 2222,
@@ -60,6 +61,7 @@ describe('graph module ', () => {
                 },
                 outbound: [{ to: 3 }],
             },
+            { to: 3 },
             {
                 identifiers: {
                     uid: 3333,
@@ -67,7 +69,6 @@ describe('graph module ', () => {
             },
         ]);
     });
-
     it('BFS not connected graph test', () => {
         const test_raw_graph = {
             1: {
@@ -93,7 +94,128 @@ describe('graph module ', () => {
             },
         };
         const traversal = Graph.bfs(test_raw_graph, 1111, false);
-        assert.equal(traversal.length, 3);
+        assert.equal(traversal.length, 5);
+    });
+    it('BFS connected graph restricted test I', () => {
+        const test_raw_graph = {
+            1: {
+                identifiers: {
+                    uid: 1111,
+                },
+                outbound: [{ to: 2 }, { to: 4 }],
+            },
+            2: {
+                identifiers: {
+                    uid: 2222,
+                },
+            },
+            3: {
+                identifiers: {
+                    uid: 3333,
+                },
+                vertex_type: 'BATCH',
+            },
+            4: {
+                identifiers: {
+                    uid: 4444,
+                },
+                outbound: [{ to: 3, edge_type: 'TRANSACTION_CONNECTION' }],
+            },
+            5: {
+                identifiers: {
+                    uid: 5555,
+                },
+            },
+        };
+        const traversal = Graph.bfs(test_raw_graph, 1111, true);
+        assert.equal(traversal.length, 5);
+        assert.deepEqual(traversal, [
+            {
+                identifiers: {
+                    uid: 1111,
+                },
+                outbound: [{ to: 2 }, { to: 4 }],
+            },
+            { to: 2 },
+            { to: 4 },
+            {
+                identifiers: {
+                    uid: 2222,
+                },
+            },
+            {
+                identifiers: {
+                    uid: 4444,
+                },
+                outbound: [{ to: 3, edge_type: 'TRANSACTION_CONNECTION' }],
+            },
+        ]);
+    });
+    it('BFS connected graph restricted test II', () => {
+        const test_raw_graph = {
+            1: {
+                identifiers: {
+                    uid: 1111,
+                },
+                outbound: [{ to: 2 }, { to: 4 }],
+            },
+            2: {
+                identifiers: {
+                    uid: 2222,
+                },
+            },
+            3: {
+                identifiers: {
+                    uid: 3333,
+                },
+                vertex_type: 'SOME_VERTEX',
+                outbound: [{ to: 5 }],
+            },
+            4: {
+                identifiers: {
+                    uid: 4444,
+                },
+                outbound: [{ to: 3, edge_type: 'SOME_EDGE' }],
+            },
+            5: {
+                identifiers: {
+                    uid: 5555,
+                },
+            },
+        };
+        const traversal = Graph.bfs(test_raw_graph, 1111, true);
+        assert.equal(traversal.length, 9);
+        assert.deepEqual(traversal, [
+            {
+                identifiers: {
+                    uid: 1111,
+                },
+                outbound: [{ to: 2 }, { to: 4 }],
+            },
+            { to: 2 },
+            { to: 4 },
+            {
+                identifiers: {
+                    uid: 2222,
+                },
+            },
+            {
+                identifiers: {
+                    uid: 4444,
+                },
+                outbound: [{ to: 3, edge_type: 'SOME_EDGE' }],
+            },
+            { to: 3, edge_type: 'SOME_EDGE' },
+            {
+                identifiers: {
+                    uid: 3333,
+                },
+                vertex_type: 'SOME_VERTEX',
+                outbound: [{ to: 5 }],
+            },
+            { to: 5 },
+            { identifiers: { uid: 5555 } },
+        ]);
     });
 });
 
