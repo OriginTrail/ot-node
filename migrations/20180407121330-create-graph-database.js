54bc6463ebd42e920fa672afe8c120ca1c040427
v.lelicanin <v.lelicanin@sae.edu>
2018-04-08 23:54:15 +0200

    deploy

--- migrations/20180407121330-create-graph-database.js
+++ migrations/20180407121330-create-graph-database.js
@@ -1,45 +1,41 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('graph_databases', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      database_system: {
-        type: Sequelize.STRING
-      },
-      username: {
-        type: Sequelize.STRING
-      },
-      password: {
-        type: Sequelize.STRING
-      },
-      host: {
-        type: Sequelize.STRING
-      },
-      port: {
-        type: Sequelize.INTEGER
-      },
-      max_path_length: {
-        type: Sequelize.INTEGER
-      },
-      database: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('graph_databases');
-  }
+    up: (queryInterface, Sequelize) => queryInterface.createTable('graph_database', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+        },
+        database_system: {
+            type: Sequelize.STRING,
+        },
+        username: {
+            type: Sequelize.STRING,
+        },
+        password: {
+            type: Sequelize.STRING,
+        },
+        host: {
+            type: Sequelize.STRING,
+        },
+        port: {
+            type: Sequelize.INTEGER,
+        },
+        max_path_length: {
+            type: Sequelize.INTEGER,
+        },
+        database: {
+            type: Sequelize.STRING,
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+    }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('graph_databases'),
 };
