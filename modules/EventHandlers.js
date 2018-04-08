cb31ad171236120a8b44abd67eb320483150b8a0
v.lelicanin <v.lelicanin@sae.edu>
2018-04-08 21:38:31 +0200

    refactor

--- modules/EventHandlers.js
+++ modules/EventHandlers.js
@@ -1,7 +1,16 @@
 const globalEvents = require('./GlobalEvents');
+const importer = require('./importer')();
+const MessageHandler = require('./MessageHandler');
 
 const { globalEmitter } = globalEvents;
 
 globalEmitter.on('import-request', (data) => {
-
+    importer.importXML(data.queryObject.filepath, (response) => {
+        // emit response
+    });
+});
+globalEmitter.on('gs1-import-request', (data) => {
+    importer.importXMLgs1(data.queryObject.filepath, (response) => {
+        // emit response
+    });
 });
