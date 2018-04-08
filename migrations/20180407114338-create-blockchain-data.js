3500698280e50ffcd04566a4bd95a6c9370089e8
v.lelicanin <v.lelicanin@sae.edu>
2018-04-09 00:06:35 +0200

    deploy

--- migrations/20180407114338-create-blockchain-data.js
+++ migrations/20180407114338-create-blockchain-data.js
@@ -19,6 +19,27 @@ module.exports = {
         gas_price: {
             type: Sequelize.INTEGER,
         },
+        ot_contract_address: {
+            type: Sequelize.STRING,
+        },
+        token_contract_address: {
+            type: Sequelize.STRING,
+        },
+        escrow_contract_address: {
+            type: Sequelize.STRING,
+        },
+        rpc_node_host: {
+            type: Sequelize.STRING,
+        },
+        rpc_node_port: {
+            type: Sequelize.INTEGER,
+        },
+        wallet_address: {
+            type: Sequelize.STRING,
+        },
+        wallet_private_key: {
+            type: Sequelize.STRING,
+        },
     }),
     down: (queryInterface, Sequelize) => queryInterface.dropTable('blockchain_data'),
 };
