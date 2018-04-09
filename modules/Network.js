854a07e0a4ba3c576e2b66296d88b4aca573e969
v.lelicanin <v.lelicanin@sae.edu>
2018-04-09 02:34:12 +0200

    deploy

--- modules/Network.js
+++ modules/Network.js
@@ -87,11 +87,11 @@ class Network {
         node.ot.quasar = node.ot.plugin(kadence.quasar());
 
         // Mitigate Spartacus attacks - Sybil
-        node.ot.spartacus = node.ot.plugin(kadence.spartacus(
-            this.xprivkey,
-            parseInt(config.child_derivation_index, 10),
-            kadence.constants.HD_KEY_DERIVATION_PATH,
-        ));
+        // node.ot.spartacus = node.ot.plugin(kadence.spartacus(
+        //     this.xprivkey,
+        //     parseInt(config.child_derivation_index, 10),
+        //     kadence.constants.HD_KEY_DERIVATION_PATH,
+        // ));
 
         log.info('Spartacus initialised');
 
