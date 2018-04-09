f9560833d8aa75aa3edf812d45bcf5d6374e8ce8
Vladimir <v.lelicanin@sae.edu>
2018-04-09 21:38:39 +0200

    deploy

--- modules/Challenge.js
+++ modules/Challenge.js
@@ -105,7 +105,7 @@ class Challenge {
      */
     static addTests(tests) {
         return new Promise((resolve, reject) => {
-            const db = new ();
+            const db = new SystemStorage();
             db.connect().then(() => {
                 // Delete any old tests
                 deasync(db.runSystemQuery(
