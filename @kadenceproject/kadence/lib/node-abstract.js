88b63c4c66dd31a7cbe83443247d42b4f7d7cbaa
v.lelicanin <v.lelicanin@sae.edu>
2018-04-13 23:50:28 +0200

    deploy

--- @kadenceproject/kadence/lib/node-abstract.js
+++ @kadenceproject/kadence/lib/node-abstract.js
@@ -251,7 +251,6 @@ class AbstractNode extends EventEmitter {
    * @param {AbstractNode~sendCallback} [callback]
    */
   send(method, params, target, handler = () => null) {
-      console.log(target);
     const { router } = this;
     const id = uuid();
     const timestamp = Date.now();
