a0bf50231763e6e357563c6f11a9e34e187074a5
v.lelicanin <v.lelicanin@sae.edu>
2018-04-13 23:55:52 +0200

    deploy

--- ot-node.js
+++ ot-node.js
@@ -20,7 +20,10 @@ var pjson = require('./package.json');
 const log = Utilities.getLogger();
 const { globalEmitter } = globalEvents;
 
-
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+    // application specific logging, throwing an error, or other logic here
+});
 /**
  * Main node object
  */
