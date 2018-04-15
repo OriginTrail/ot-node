a9955891db0aac42701572c707eb45ab8601881c
v.lelicanin <v.lelicanin@sae.edu>
2018-04-15 11:22:14 +0200

    deploy

--- modules/Network.js
+++ modules/Network.js
@@ -345,7 +345,7 @@ class Network {
         async.detectSeries(peers, (url, done) => {
             const contact = kadence.utils.parseContactURL(url);
             node.ot.join(contact, (err) => {
-                console.log(err)
+                console.log(contact)
                 done(null, (!err) && node.ot.router.size > 1);
             });
         }, (err, result) => {
