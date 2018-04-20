7c165aa6c2561063e2fd99db7dda0535029f3471
Janko Simonovic <jsimonovic@mainframe2.com>
2018-04-20 10:50:06 +0200

    fix lint

--- migrations/20180420084814-create-offers.js
+++ migrations/20180420084814-create-offers.js
@@ -1,48 +1,44 @@
-'use strict';
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('offers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      offer_id: {
-        type: Sequelize.STRING
-      },
-      data_lifespan: {
-        type: Sequelize.INTEGER
-      },
-      start_tender_time: {
-        type: Sequelize.INTEGER
-      },
-      tender_duration: {
-        type: Sequelize.INTEGER
-      },
-      min_number_applicants: {
-        type: Sequelize.INTEGER
-      },
-      price_tokens: {
-        type: Sequelize.REAL
-      },
-      data_size_bytes: {
-        type: Sequelize.INTEGER
-      },
-      root_hash: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('offers');
-  }
+    up: (queryInterface, Sequelize) => queryInterface.createTable('offers', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+        },
+        offer_id: {
+            type: Sequelize.STRING,
+        },
+        data_lifespan: {
+            type: Sequelize.INTEGER,
+        },
+        start_tender_time: {
+            type: Sequelize.INTEGER,
+        },
+        tender_duration: {
+            type: Sequelize.INTEGER,
+        },
+        min_number_applicants: {
+            type: Sequelize.INTEGER,
+        },
+        price_tokens: {
+            type: Sequelize.REAL,
+        },
+        data_size_bytes: {
+            type: Sequelize.INTEGER,
+        },
+        root_hash: {
+            type: Sequelize.STRING,
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE,
+        },
+    }),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('offers'),
 };
--- models/offers.js
+++ models/offers.js
@@ -1,17 +1,17 @@
-'use strict';
+
 module.exports = (sequelize, DataTypes) => {
-  var offers = sequelize.define('offers', {
-    offer_id: DataTypes.STRING,
-    data_lifespan: DataTypes.INTEGER,
-    start_tender_time: DataTypes.INTEGER,
-    tender_duration: DataTypes.INTEGER,
-    min_number_applicants: DataTypes.INTEGER,
-    price_tokens: DataTypes.REAL,
-    data_size_bytes: DataTypes.INTEGER,
-    root_hash: DataTypes.STRING
-  }, {});
-  offers.associate = function(models) {
+    var offers = sequelize.define('offers', {
+        offer_id: DataTypes.STRING,
+        data_lifespan: DataTypes.INTEGER,
+        start_tender_time: DataTypes.INTEGER,
+        tender_duration: DataTypes.INTEGER,
+        min_number_applicants: DataTypes.INTEGER,
+        price_tokens: DataTypes.REAL,
+        data_size_bytes: DataTypes.INTEGER,
+        root_hash: DataTypes.STRING,
+    }, {});
+    offers.associate = function (models) {
     // associations can be defined here
-  };
-  return offers;
+    };
+    return offers;
 };
