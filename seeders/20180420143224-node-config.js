5558523abd214f3a29771093921fde477ee51a2d
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-04-23 15:54:09 +0200

    Added missing config

--- seeders/20180420143224-node-config.js
+++ seeders/20180420143224-node-config.js
@@ -3,17 +3,20 @@ require('dotenv').config();
 module.exports = {
     up: (queryInterface, Sequelize) => queryInterface.bulkInsert('node_config', [{
         key: 'dh_min_price',
-        value: 1,
+        value: 1e9,
     },
     {
         key: 'dh_max_price',
-        value: 10,
+        value: 100e18,
     },
     {
         key: 'dh_max_data_size_bytes',
-        value: 1000,
+        value: 1000000,
+    },
+    {
+        key: 'dh_max_stake',
+        value: 1000e18,
     },
     ], {}),
-
     down: (queryInterface, Sequelize) => queryInterface.bulkDelete('node_config', null, {}),
 };
