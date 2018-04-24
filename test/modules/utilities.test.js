42f0de32e6a1ada35a19912b36437e3cff960c80
radomir-sebek <rasho.ieee@gmail.com>
2018-04-24 22:19:51 +0200

    MINOR: Enabling back loadSelectedBlockchainInfo() unit test (#83)
    * MINOR: Enabling back loadSelectedBlockchainInfo() unit test

--- test/modules/utilities.test.js
+++ test/modules/utilities.test.js
@@ -32,6 +32,14 @@ describe('Utilities module', () => {
         });
     });
 
+    it('loadSelectedBlockchainInfo()', async () => {
+        const myResult = await Utilities.loadSelectedBlockchainInfo();
+        assert.hasAllKeys(myResult, ['blockchain_title', 'id', 'network_id', 'gas_limit',
+            'gas_price', 'ot_contract_address', 'token_contract_address', 'escrow_contract_address',
+            'rpc_node_host', 'rpc_node_port', 'wallet_address', 'wallet_private_key', 'bidding_contract_address']);
+        assert.equal(myResult.blockchain_title, 'Ethereum');
+    });
+
     it('isEmptyObject check', () => {
         assert.isTrue(Utilities.isEmptyObject({}));
         assert.isFalse(Utilities.isEmptyObject([]));
