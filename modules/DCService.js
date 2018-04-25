3bafa2e66307dcc95a3d9c58d4e1ba08a86de922
v.lelicanin <v.lelicanin@sae.edu>
2018-04-25 08:48:55 +0200

    squash

--- modules/DCService.js
+++ modules/DCService.js
@@ -31,7 +31,7 @@ class DCService {
         });
 
         const importSizeInBytes = new BN(this._calculateImportSize(vertices));
-        const price = `${Utilities.getRandomIntRange(1, 10).toString()}000000000000000000`;
+        const price = `${Utilities.getRandomIntRange(1, 10).toString()}0000`;
         Models.offers.create({
             id: dataId,
             data_lifespan: totalEscrowTime,
