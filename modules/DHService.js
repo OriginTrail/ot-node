d6216d00cdec5ca0fc4e8fe980423672963205ce
Vladimir <v.lelicanin@sae.edu>
2018-04-25 10:57:58 +0200

    squash

--- modules/DHService.js
+++ modules/DHService.js
@@ -167,7 +167,7 @@ class DHService {
         }
         setTimeout(
         // change time period in order to test reveal
-            revealBid, 35 * 1000,
+            revealBid, 65 * 1000,
             dcWallet, dataId, price, stake, bidIndex,
         );
     }
