26d4a572f0ab8d696e6addfe7bb75faef6a63fe5
Janko Simonovic <simonovic86@gmail.com>
2018-05-03 16:49:24 +0200

    add Java8 and Neo4j installation steps

--- install.sh
+++ install.sh
@@ -19,6 +19,27 @@ sudo pip3 install python-arango
 sudo pip3 install xmljson
 sudo pip3 install python-dotenv
 
+echo "Installing Java 8 as user `whoami` "
+
+sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+
+# Accept license non-iteractive
+sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
+sudo apt-get update
+sudo apt-get install -y oracle-java8-installer
+
+# Make sure Java 8 becomes default java
+sudo apt-get install -y oracle-java8-set-default
+
+# Install Neo4j
+wget --no-check-certificate -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add -
+sudo echo 'deb http://debian.neo4j.org/repo stable/' | sudo tee /etc/apt/sources.list.d/neo4j.list
+sudo apt update -y
+sudo apt install neo4j
+sudo service neo4j start
+
 sudo apt-get install git
 
 git clone -b master https://github.com/OriginTrail/ot-node.git
