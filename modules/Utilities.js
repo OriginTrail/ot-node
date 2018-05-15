38d5e03b5af2fdea6ec2e889d41a8881615fb22a
radomir.sebek <rasho.ieee@gmail.com>
2018-05-15 13:23:22 +0200

    fixing according to CR comments

--- modules/Utilities.js
+++ modules/Utilities.js
@@ -524,14 +524,18 @@ class Utilities {
         });
     }
 
-    static checkIfArangoIsRunning() {
+    static getArangoDbVersion() {
         return new Promise((resolve, reject) => {
             request
                 .get(`http://${process.env.DB_HOST}:${process.env.DB_PORT}/_api/version`)
                 .auth(process.env.DB_USERNAME, process.env.DB_PASSWORD)
                 .then((res) => {
-                    assert.strictEqual(res.status, 200);
-                    resolve(res.body);
+                    if (res.status === 200) {
+                        resolve(res.body);
+                    } else {
+                        // eslint-disable-next-line prefer-promise-reject-errors
+                        reject('Failed to contact DB');
+                    }
                 }).catch((err) => {
                     reject(err);
                 });
