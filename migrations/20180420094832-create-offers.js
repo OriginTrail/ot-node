d478d09773563dd28046a762d7cc533d0cd243a7
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-05-16 17:15:18 +0200

    Supported new createOffer params

--- migrations/20180420094832-create-offers.js
+++ migrations/20180420094832-create-offers.js
@@ -3,36 +3,45 @@ module.exports = {
     up: (queryInterface, Sequelize) => queryInterface.createTable('offers', {
         id: {
             allowNull: false,
-            autoIncrement: true,
+            autoIncrement: false,
             primaryKey: true,
             type: Sequelize.INTEGER,
         },
-        data_lifespan: {
+        total_escrow_time: {
             type: Sequelize.INTEGER,
-        },
-        start_tender_time: {
-            type: Sequelize.INTEGER,
-        },
-        tender_duration: {
-            type: Sequelize.INTEGER,
-        },
-        min_number_applicants: {
-            type: Sequelize.INTEGER,
-        },
-        price_tokens: {
-            type: Sequelize.STRING,
-        },
-        data_size_bytes: {
-            type: Sequelize.STRING,
-        },
-        replication_number: {
-            type: Sequelize.INTEGER,
-        },
-        root_hash: {
-            type: Sequelize.STRING,
+            allowNull: false,
         },
         max_token_amount: {
             type: Sequelize.STRING,
+            allowNull: false,
+        },
+        min_stake_amount: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        min_reputation: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+        },
+        data_hash: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        data_size_bytes: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        dh_wallets: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        dh_ids: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        start_tender_time: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
         },
     }),
     down: (queryInterface, Sequelize) => queryInterface.dropTable('offers'),
