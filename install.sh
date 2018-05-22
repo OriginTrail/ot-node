53bf6f1f2777bc19a9276623e755ec9e4b20719e
Vuk Đorić <vukdoric@Vuks-MacBook-Pro.local>
2018-05-22 17:16:16 +0200

    install.sh fix

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ if [ $db = "neo4j" ]; then
   sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
   sudo echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
   sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-  sudo apt-get update
+  sudo apt-get update -y
   sudo apt-get install -y oracle-java8-installer
   sudo apt-get install -y oracle-java8-set-default
 
