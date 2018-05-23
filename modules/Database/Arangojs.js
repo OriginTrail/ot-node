384a444b72edbdda3f01e6bb91c49a55e30537e5
Janko Simonovic <simonovic86@gmail.com>
2018-05-23 13:29:38 +0200

    fix imports for same data

--- modules/Database/Arangojs.js
+++ modules/Database/Arangojs.js
@@ -178,6 +178,53 @@ class ArangoJS {
     }
 
     /**
+     * Updates document imports by ID
+     * @param collectionName
+     * @param senderId
+     * @param uid
+     * @param importNumber
+     * @return {Promise<*>}
+     */
+    async updateDocumentImportsByUID(collectionName, senderId, uid, importNumber) {
+        const result = await this.findDocumentWithMaxVersion(collectionName, senderId, uid);
+        let new_imports = [];
+        if (result.imports !== undefined) {
+            new_imports = result.imports;
+
+            if (new_imports.includes(importNumber)) {
+                return result;
+            }
+        }
+
+        new_imports.push(importNumber);
+
+        result.imports = new_imports;
+        return this.updateDocument(collectionName, result);
+    }
+
+    /**
+     * Updates vertex imports by ID
+     * @param senderId
+     * @param uid
+     * @param importNumber
+     * @return {Promise<*>}
+     */
+    async updateVertexImportsByUID(senderId, uid, importNumber) {
+        return this.updateDocumentImportsByUID('ot_vertices', senderId, uid, importNumber);
+    }
+
+    /**
+     * Updates edge imports by ID
+     * @param senderId
+     * @param uid
+     * @param importNumber
+     * @return {Promise<*>}
+     */
+    async updateEdgeImportsByUID(senderId, uid, importNumber) {
+        return this.updateDocumentImportsByUID('ot_edges', senderId, uid, importNumber);
+    }
+
+    /**
      * Gets max version where uid is the same but not the _key
      * @param senderId  Sender ID
      * @param uid       Vertex uid
@@ -205,11 +252,32 @@ class ArangoJS {
      * @return {Promise<void>}
      */
     async findVertexWithMaxVersion(senderId, uid) {
-        const queryString = 'FOR v IN ot_vertices ' +
-                'FILTER v.identifiers.uid == @uid AND v.sender_id == @senderId ' +
-                'SORT v.version DESC ' +
-                'LIMIT 1 ' +
-                'RETURN v';
+        return this.findDocumentWithMaxVersion('ot_vertices', senderId, uid);
+    }
+
+    /**
+     * Gets max where uid is the same and has the max version
+     * @param senderId  Sender ID
+     * @param uid       Vertex uid
+     * @return {Promise<void>}
+     */
+    async findEdgeWithMaxVersion(senderId, uid) {
+        return this.findDocumentWithMaxVersion('ot_edges', senderId, uid);
+    }
+
+    /**
+     * Gets max where uid is the same and has the max version
+     * @param senderId   Sender ID
+     * @param uid        Vertex uid
+     * @param collection Collection name
+     * @return {Promise<void>}
+     */
+    async findDocumentWithMaxVersion(collection, senderId, uid) {
+        const queryString = `FOR v IN  ${collection} ` +
+            'FILTER v.identifiers.id == @uid AND v.sender_id == @senderId ' +
+            'SORT v.version DESC ' +
+            'LIMIT 1 ' +
+            'RETURN v';
         const params = {
             uid,
             senderId,
