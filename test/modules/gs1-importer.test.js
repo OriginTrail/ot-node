fb73b43c3d1ee269d7e7e992e7f987aff987ac7f
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-05-23 16:06:56 +0200

    Added test for group of imports

--- test/modules/gs1-importer.test.js
+++ test/modules/gs1-importer.test.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const {
-    describe, before, after, it,
+    describe, beforeEach, afterEach, it,
 } = require('mocha');
 const { expect } = require('chai');
 const gs1 = require('../../modules/gs1-importer')();
@@ -30,7 +30,7 @@ describe('GS1 Importer tests', () => {
         { args: [path.join(__dirname, '../../importers/GraphExample_3.xml')] },
     ];
 
-    before('Setup DB', async () => {
+    beforeEach('Setup DB', async () => {
         systemDb = new Database();
         systemDb.useBasicAuth(process.env.DB_USERNAME, process.env.DB_PASSWORD);
 
@@ -67,43 +67,64 @@ describe('GS1 Importer tests', () => {
     });
 
     describe('Graph validation', async () => {
+        function checkImportResults(import1Result, import2Result) {
+            expect(import1Result.root_hash).to.be
+                .equal(import2Result.root_hash);
+            expect(import1Result.total_documents).to.be
+                .equal(import2Result.total_documents);
+            expect(import1Result.vertices.length).to.be
+                .equal(import2Result.vertices.length);
+            expect(import1Result.edges.length).to.be
+                .equal(import2Result.edges.length);
+
+            import1Result.vertices.forEach((vertex) => {
+                const vertex2 = import2Result.vertices
+                    .find(element => vertex._key === element._key);
+                expect(vertex2).not.to.be.equal(undefined);
+
+                if (vertex.identifiers) {
+                    expect(vertex.identifiers).to.deep.equal(vertex2.identifiers);
+                }
+                if (vertex.data) {
+                    expect(vertex.data).to.deep.equal(vertex2.data);
+                }
+                if (vertex.vertex_type) {
+                    expect(vertex.vertex_type).to.deep.equal(vertex2.vertex_type);
+                }
+            });
+        }
+
         inputXmlFiles.forEach((test) => {
             it(
                 `should generate the same graph for subsequent ${path.basename(test.args[0])} imports`,
                 async () => {
                     const import1Result = await gs1.parseGS1(test.args[0]);
                     const import2Result = await gs1.parseGS1(test.args[0]);
-
-                    expect(import1Result.root_hash).to.be
-                        .equal(import2Result.root_hash);
-                    expect(import1Result.total_documents).to.be
-                        .equal(import2Result.total_documents);
-                    expect(import1Result.vertices.length).to.be
-                        .equal(import2Result.vertices.length);
-                    expect(import1Result.edges.length).to.be
-                        .equal(import2Result.edges.length);
-
-                    import1Result.vertices.forEach((vertex) => {
-                        const vertex2 = import2Result.vertices
-                            .find(element => vertex._key === element._key);
-                        expect(vertex2).not.to.be.equal(undefined);
-
-                        if (vertex.identifiers) {
-                            expect(vertex.identifiers).to.deep.equal(vertex2.identifiers);
-                        }
-                        if (vertex.data) {
-                            expect(vertex.data).to.deep.equal(vertex2.data);
-                        }
-                        if (vertex.vertex_type) {
-                            expect(vertex.vertex_type).to.deep.equal(vertex2.vertex_type);
-                        }
-                    });
+                    checkImportResults(import1Result, import2Result);
                 },
             );
         });
+
+        it('should correctly import all examples together', async () => {
+            const importResults = [];
+            const imports = [];
+
+            imports.push(...inputXmlFiles);
+            imports.push(...inputXmlFiles);
+
+            for (let i = 0; i < imports.length; i += 1) {
+                // eslint-disable-next-line no-await-in-loop
+                const result = await gs1.parseGS1(imports[i].args[0]);
+                importResults.push(result);
+            }
+
+            for (let i = 0; i < inputXmlFiles.length; i += 1) {
+                checkImportResults(importResults[i], importResults[i + inputXmlFiles.length]);
+            }
+        });
     });
 
-    after('Drop DB', async () => {
+    afterEach('Drop DB', async () => {
         if (systemDb) {
             const listOfDatabases = await systemDb.listDatabases();
             if (listOfDatabases.includes(databaseName)) {
