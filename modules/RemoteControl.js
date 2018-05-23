a8f38af0fbcb5e6a44617d8ddaed506238ee8c9b
v.lelicanin <v.lelicanin@sae.edu>
2018-05-23 07:23:42 +0200

    Vertex visualisation

--- modules/RemoteControl.js
+++ modules/RemoteControl.js
@@ -6,6 +6,7 @@ const Models = require('../models');
 const kadence = require('@kadenceproject/kadence');
 const pjson = require('../package.json');
 const Storage = require('./Storage');
+const GraphStorage = require('./GraphStorageInstance');
 
 class RemoteControl {
     static async connect() {
@@ -23,6 +24,8 @@ class RemoteControl {
                         });
                         socket.emit('config', config);
                     });
+            }).then((res) => {
+                RemoteControl.updateImports();
             });
 
             this.socket.on('config-update', (data) => {
@@ -46,11 +49,19 @@ class RemoteControl {
                     });
                 }
             });
+
+            this.socket.on('get-imports', () => {
+                RemoteControl.updateImports();
+            });
+
+            this.socket.on('get-visual-graph', (import_id) => {
+                RemoteControl.getImport(import_id);
+            });
         });
     }
 
     /**
-     * Returns basic informations about the running node
+     * Returns basic information about the running node
      * @param {Control~getProtocolInfoCallback} callback
      */
     static getProtocolInfo() {
@@ -70,6 +81,64 @@ class RemoteControl {
                 identity: this.node.identity.toString('hex'),
                 contact: this.node.contact,
                 peers,
+            });
+        });
+    }
+
+    /**
+     * Update imports table from data_info
+     */
+    static updateImports() {
+        return new Promise((resolve, reject) => {
+            Models.data_info.findAll()
+                .then((rows) => {
+                    this.socket.emit('imports', rows);
+                    resolve();
+                });
+        });
+    }
+
+    /**
+     * Get graph by import_id
+     * @import_id int
+     */
+    static getImport(import_id) {
+        return new Promise((resolve, reject) => {
+            const verticesPromise = GraphStorage.db.findVerticesByImportId(import_id);
+            const edgesPromise = GraphStorage.db.findEdgesByImportId(import_id);
+
+            Promise.all([verticesPromise, edgesPromise]).then((values) => {
+                var nodes = [];
+                var edges = [];
+                values[0].forEach((vertex) => {
+                    const isRoot = !!((vertex._id === 'ot_vertices/Transport'
+                        || vertex._id === 'ot_vertices/Transformation'
+                        || vertex._id === 'ot_vertices/Product'
+                        || vertex._id === 'ot_vertices/Ownership'
+                        || vertex._id === 'ot_vertices/Observation'
+                        || vertex._id === 'ot_vertices/Location'
+                        || vertex._id === 'ot_vertices/Actor'
+                    ));
+                    console.log(vertex);
+                    nodes.push({
+                        id: vertex._id,
+                        caption: vertex._id,
+                        root: isRoot,
+                        // category: vertex.data.category,
+                        // description: vertex.data.description,
+                        // object_class_id: vertex.data.object_class_id,
+                    });
+                });
+                values[1].forEach((edge) => {
+                    console.log(edge);
+                    edges.push({
+                        source: edge._from,
+                        target: edge._to,
+                    });
+                });
+
+                this.socket.emit('visualise', { nodes, edges });
+                resolve();
             });
         });
     }
