8d0fc118c84944c561b2491744368f162bde081f
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-05-24 21:53:28 +0200

    All OT objects in Avilix container declared as singleton

--- ot-node.js
+++ ot-node.js
@@ -140,17 +140,17 @@ class OTNode {
         });
 
         container.register({
-            emitter: awilix.asClass(EventEmitter),
-            network: awilix.asClass(Network),
-            graph: awilix.asClass(Graph),
-            product: awilix.asClass(Product),
-            dhService: awilix.asClass(DHService),
-            dcService: awilix.asClass(DCService),
+            emitter: awilix.asClass(EventEmitter).singleton(),
+            network: awilix.asClass(Network).singleton(),
+            graph: awilix.asClass(Graph).singleton(),
+            product: awilix.asClass(Product).singleton(),
+            dhService: awilix.asClass(DHService).singleton(),
+            dcService: awilix.asClass(DCService).singleton(),
             config: awilix.asValue(config),
-            importer: awilix.asClass(Importer),
-            blockchain: awilix.asClass(Blockchain),
-            dataReplication: awilix.asClass(DataReplication),
-            gs1Importer: awilix.asClass(GS1Importer),
+            importer: awilix.asClass(Importer).singleton(),
+            blockchain: awilix.asClass(Blockchain).singleton(),
+            dataReplication: awilix.asClass(DataReplication).singleton(),
+            gs1Importer: awilix.asClass(GS1Importer).singleton(),
             graphStorage: awilix.asValue(new GraphStorage(selectedDatabase)),
         });
         const emitter = container.resolve('emitter');
