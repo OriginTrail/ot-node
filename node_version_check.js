6d647c01b4420bca206012d4f98bffdf1984164f
Nikita Abrashkin <nikitaabrashkin@192.168.1.61>
2018-06-12 13:05:48 +0200

    solved-nodejs-version-onstart

--- node_version_check.js
+++ node_version_check.js
@@ -1,27 +1,8 @@
 var node_js_version = process.version.substring(0, 2);
 
 
-if(node_js_version == 'v9') {
-    var spawn = require('child_process').spawn;
-    var otNode = spawn(process.argv[0],  []);
-
-
-//noinspection JSUnresolvedFunction
-    otNode.stdout.setEncoding('utf8');
-    otNode.stdout.on('data', function (data) {
-        var str = data.toString()
-        var lines = str.split(/(\r?\n)/g);
-        console.log(lines.join(""));
-    });
-
-    otNode.on('close', function (code) {
-        console.log('process exit code ' + code);
-    });
-} else {
+if (node_js_version !== 'v8') {
     console.log('Make sure you have the 9.x version of Node.js installed. Some features will not work well on versions less or greater then 9.x');
+    process.exit(222);
 }
-
-
-
-
 
