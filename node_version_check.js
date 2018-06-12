23992e38b8b534b27522bad9b9be45c7698b48c9
Stevan Nesovic <stevannesovic22@gmail.com>
2018-06-12 13:15:34 +0200

    finished node.js version check

--- node_version_check.js
+++ node_version_check.js
@@ -1,7 +1,7 @@
 var node_js_version = process.version.substring(0, 2);
 
 
-if (node_js_version !== 'v8') {
+if (node_js_version !== 'v9') {
     console.log('Make sure you have the 9.x version of Node.js installed. Some features will not work well on versions less or greater then 9.x');
     process.exit(222);
 }
