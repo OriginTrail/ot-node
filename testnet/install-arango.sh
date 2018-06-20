f8500adfa61615fd47863103d2cf4c211247210a
Vladimir Lelicanin <vladimir.lelicanin@origin-trail.com>
2018-06-20 22:42:41 +0200

    Docker & Testnet Setup (#241)

---
+++ testnet/install-arango.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+wget https://www.arangodb.com/repositories/arangodb3/xUbuntu_16.04/Release.key
+apt-key add Release.key
+apt-add-repository 'deb https://www.arangodb.com/repositories/arangodb3/xUbuntu_16.04/ /'
+apt-get update -y
+echo arangodb3 arangodb3/password password root | debconf-set-selections
+echo arangodb3 arangodb3/password_again password root | debconf-set-selections
+apt-get install arangodb3 -y --allow-unauthenticated
+arangod --daemon --pid-file /var/tmp/arango.pid
