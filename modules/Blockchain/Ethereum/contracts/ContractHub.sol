e370bb8c3e1e47bf27ec5cb02fa7001165d13fb2
Uros Kukic <kukic.uros@yahoo.com>
2018-06-22 13:04:05 +0200

    Added ContractHub smart contract

---
+++ modules/Blockchain/Ethereum/contracts/ContractHub.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.4.21;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+ contract Ownable {
+ 	address public owner;
+
+ 	event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+	/**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+     function Ownable () public {
+     	owner = msg.sender;
+     }
+
+	/**
+     * @dev Throws if called by any account other than the owner.
+     */
+     modifier onlyOwner() {
+     	require(msg.sender == owner);
+     	_;
+     }
+
+	/**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+     function transferOwnership(address newOwner) public onlyOwner {
+     	require(newOwner != address(0));
+     	emit OwnershipTransferred(owner, newOwner);
+     	owner = newOwner;
+     }
+
+ }
+
+contract ContractHub is Ownable{
+	address public fingerprintAddress;
+	address public tokenAddress;
+	address public biddingAddress;
+	address public escrowAddress;
+     address public readingAddress;
+
+     function ContractHub(address _fingerprintAddress, address _tokenAddress, address _biddingAddress, address _escrowAddress, address _readingAddress)
+     public {
+          fingerprintAddress = _fingerprintAddress;
+          tokenAddress = _tokenAddress;
+          biddingAddress = _biddingAddress;
+          escrowAddress = _escrowAddress;
+          readingAddress = _readingAddress;
+     }
+	
+}
