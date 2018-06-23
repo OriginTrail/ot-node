9b260a0bc7dd420445e4b3ec9fdbab9d1165d95f
Vladimir Lelicanin <vladimir.lelicanin@origin-trail.com>
2018-06-24 00:02:31 +0200

    Register node update

--- testnet/start.sh
+++ testnet/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-wget https://raw.githubusercontent.com/origintrail/ot-node/docker/testnet/register-node.js
+wget https://raw.githubusercontent.com/origintrail/ot-node/docker/testnet/register-node.js -q
+rm register-node.js
 mv register-node.js /ot-node/register-node.js
 /usr/bin/supervisord
