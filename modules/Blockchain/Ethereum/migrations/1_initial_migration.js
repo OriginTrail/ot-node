33ed3045efc190b6f620ce7a70e8f29056cc445a
Uros Kukic <kukic.uros@yahoo.com>
2018-06-24 13:28:05 +0200

    Removes unnecessary console log

--- modules/Blockchain/Ethereum/migrations/1_initial_migration.js
+++ modules/Blockchain/Ethereum/migrations/1_initial_migration.js
@@ -1,6 +1,5 @@
 const Migrations = artifacts.require('Migrations'); // eslint-disable-line no-undef
 
 module.exports = (deployer, provider) => {
-        console.log(`\t chosen DH indexes: ${JSON.stringify(provider)}`);
     deployer.deploy(Migrations);
 };
