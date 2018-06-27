176d09e02dca641438bc93c7f22005920fc1dc03
Vuk Djoric <vuk.djoric@origin-trail.com>
2018-06-27 14:24:24 +0200

    test fix

--- test/modules/utilities.test.js
+++ test/modules/utilities.test.js
@@ -20,7 +20,7 @@ describe('Utilities module', () => {
         myConfig = await Utilities.loadConfig();
     });
 
-    it('node_config should contain certain entries', () => {
+    it.only('node_config should contain certain entries', () => {
         assert.hasAllKeys(
             myConfig, ['node_wallet', 'node_private_key', 'node_rpc_ip', 'node_port',
                 'node_kademlia_id', 'selected_graph_database', 'selected_blockchain', 'request_timeout', 'ssl_keypath',
@@ -29,7 +29,8 @@ describe('Utilities module', () => {
                 'control_port_enabled', 'control_port', 'control_sock_enabled', 'control_sock', 'onion_enabled', 'test_network',
                 'ssl_authority_paths', 'network_bootstrap_nodes', 'solve_hashes', 'remote_access_whitelist', 'node_rpc_port',
                 'dh_min_price', 'dh_max_price', 'dh_max_stake', 'remote_control_enabled', 'remote_control_port', 'probability_threshold',
-                'read_stake_factor', 'dh_max_time_mins', 'dh_price', 'dh_stake_factor', 'send_logs_to_origintrail'],
+                'read_stake_factor', 'dh_max_time_mins', 'dh_price', 'dh_stake_factor', 'send_logs_to_origintrail',
+                'dh_min_reputation', 'dh_min_stake_amount', 'max_token_amount_per_dh', 'total_escrow_time_in_minutes'],
             'Some config items are missing in node_config',
         );
     });
