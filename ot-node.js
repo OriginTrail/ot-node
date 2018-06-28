b21e8efebf7632e7626eaf8365b6b0d1072ec03a
Janko Simonovic <simonovic86@gmail.com>
2018-06-28 10:13:03 +0200

    fix api

--- ot-node.js
+++ ot-node.js
@@ -464,7 +464,7 @@ class OTNode {
         });
 
         server.get('/api/store', (req, res) => {
-            network.kademlia().iterativeFindValue(req.params.id, (err, res) => {
+            network.kademlia().iterativeFindValue(req.query.id, (err, res) => {
                 console.log(res);
             });
         });
