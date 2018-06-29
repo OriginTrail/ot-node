05fe2598c126eac7d17bd9d1c52347d9eb221a9e
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-06-29 16:11:30 +0200

    total escrow time in minutes

--- modules/Blockchain/Ethereum/index.js
+++ modules/Blockchain/Ethereum/index.js
@@ -406,7 +406,7 @@ class Ethereum {
             [
                 importId,
                 Utilities.normalizeHex(nodeId),
-                Math.round(totalEscrowTime / 1000 / 60), // In minutes
+                totalEscrowTime,
                 maxTokenAmount,
                 MinStakeAmount,
                 minReputation,
@@ -476,7 +476,7 @@ class Ethereum {
                     contract: contractName,
                     event: event.event,
                     data: JSON.stringify(event.returnValues),
-                    import_id: event.returnValues.import_id,
+                     import_id: event.returnValues.import_id,
                     block: event.blockNumber,
                     timestamp,
                     finished: 0,
