2a7004b79a9b944d4c3ed8262207356264e1fb93
Janko Simonovic <simonovic86@gmail.com>
2018-06-29 13:22:11 +0200

    fix test and lint

--- modules/Utilities.js
+++ modules/Utilities.js
@@ -908,7 +908,7 @@ class Utilities {
      * @param {Array} a items An array containing the items.
      */
     static shuffle(a) {
-        for (let i = a.length - 1; i > 0; i--) {
+        for (let i = a.length - 1; i > 0; i -= 1) {
             const j = Math.floor(Math.random() * (i + 1));
             [a[i], a[j]] = [a[j], a[i]];
         }
