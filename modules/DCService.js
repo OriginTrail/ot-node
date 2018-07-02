2ada2c69909f182d079260c2e974e3c9d0ed2e05
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-07-02 14:45:11 +0200

    Added missing file

--- modules/DCService.js
+++ modules/DCService.js
@@ -74,10 +74,10 @@ class DCService {
         const dhIds = [];
         const dhWallets = [];
 
-        let totalEscrowTime = config.total_escrow_time_in_minutes;
-        let maxTokenAmount = new BN(config.max_token_amount_per_dh, 10);
-        let minStakeAmount = new BN(config.dh_min_stake_amount, 10);
-        let minReputation = config.dh_min_reputation;
+        let totalEscrowTime = this.config.total_escrow_time_in_minutes;
+        let maxTokenAmount = new BN(this.config.max_token_amount_per_dh, 10);
+        let minStakeAmount = new BN(this.config.dh_min_stake_amount, 10);
+        let minReputation = this.config.dh_min_reputation;
 
         if (total_escrow_time) {
             totalEscrowTime = total_escrow_time;
@@ -145,7 +145,7 @@ class DCService {
 
                 this.blockchain.createOffer(
                     importId,
-                    config.identity,
+                    this.config.identity,
                     totalEscrowTime,
                     maxTokenAmount,
                     minStakeAmount,
