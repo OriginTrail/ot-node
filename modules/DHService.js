a8f7f614221d68035215067e79f2388a5a51d52c
Janko Simonovic <simonovic86@gmail.com>
2018-07-13 18:39:46 +0200

    send import ID

--- modules/DHService.js
+++ modules/DHService.js
@@ -772,6 +772,7 @@ class DHService {
                 r2,
                 sd: epkChecksum,
                 blockNumber: selectedBlockNumber,
+                import_id: importId,
             },
         };
         encryptedPaddedKeyObject.messageSignature = Utilities.generateRsvSignature(
