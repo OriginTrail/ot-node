7024b0b9605ceec3b80ccc32b2e8289d8780ee3b
radomir.sebek <rasho.ieee@gmail.com>
2018-07-15 21:53:46 +0200

    Minor: Logging params with which api endpoints were called

--- ot-node.js
+++ ot-node.js
@@ -418,7 +418,7 @@ class OTNode {
          * @param importtype - (GS1/WOT)
          */
         server.post('/api/import', (req, res) => {
-            log.api('POST Import request received.');
+            log.api('POST: Import of data request received.');
 
             if (!authorize(req, res)) {
                 return;
@@ -486,7 +486,7 @@ class OTNode {
         });
 
         server.post('/api/replication', (req, res) => {
-            log.api('POST Replication request received.');
+            log.api('POST: Replication of imported data request received.');
 
             if (!authorize(req, res)) {
                 return;
@@ -516,7 +516,7 @@ class OTNode {
         });
 
         server.get('/api/replication/:replication_id', (req, res) => {
-            log.api('GET Replication status request received');
+            log.api('GET: Replication status request received');
 
             if (!authorize(req, res)) {
                 return;
@@ -543,7 +543,7 @@ class OTNode {
          * @param QueryObject - ex. {uid: abc:123}
          */
         server.get('/api/trail', (req, res) => {
-            log.api('GET Trail request received.');
+            log.api('GET: Trail request received.');
             const queryObject = req.query;
             emitter.emit('api-trail', {
                 query: queryObject,
@@ -555,7 +555,7 @@ class OTNode {
          * @param Query params: dc_wallet, import_id
          */
         server.get('/api/fingerprint', (req, res) => {
-            log.api('GET Fingerprint request received.');
+            log.api('GET: Fingerprint request received.');
             const queryObject = req.query;
             emitter.emit('api-get_root_hash', {
                 query: queryObject,
@@ -564,7 +564,7 @@ class OTNode {
         });
 
         server.get('/api/query/network/:query_param', (req, res) => {
-            log.api('GET Query for status request received.');
+            log.api('GET: Query for status request received.');
             if (!req.params.query_param) {
                 res.status(400);
                 res.send({
@@ -579,7 +579,7 @@ class OTNode {
         });
 
         server.get('/api/query/:query_id/responses', (req, res) => {
-            log.api('GET Query responses');
+            log.api('GET: Local query responses request received.');
             if (!req.params.query_id) {
                 res.status(400);
                 res.send({
@@ -594,7 +594,7 @@ class OTNode {
         });
 
         server.post('/api/query/network', (req, res) => {
-            log.api('POST Query request received.');
+            log.api('POST: Network query request received.');
             if (!req.body) {
                 res.status(400);
                 res.send({
@@ -621,7 +621,7 @@ class OTNode {
          * @param queryObject
          */
         server.post('/api/query/local', (req, res) => {
-            log.api('GET Query request received.');
+            log.api('GET: Local query request received.');
 
             if (req.body == null || req.body.query == null) {
                 res.status(400);
@@ -639,7 +639,7 @@ class OTNode {
         });
 
         server.get('/api/query/local/import/:import_id', (req, res) => {
-            log.api('GET import request received.');
+            log.api('GET: Local import request received.');
 
             if (!req.params.import_id) {
                 res.status(400);
@@ -656,7 +656,7 @@ class OTNode {
         });
 
         server.post('/api/query/local/import', (req, res) => {
-            log.api('GET Query request received.');
+            log.api('GET: Local query import request received.');
 
             if (req.body == null || req.body.query == null) {
                 res.status(400);
@@ -673,7 +673,7 @@ class OTNode {
 
 
         server.post('/api/read/network', (req, res) => {
-            log.api('POST Read request received.');
+            log.api('POST: Network read request received.');
 
             if (req.body == null || req.body.query_id == null || req.body.reply_id == null
               || req.body.import_id == null) {
