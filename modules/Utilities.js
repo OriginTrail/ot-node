24bce77798f910c226431eee3b0120592ae872f8
Vladimir Lelicanin <vladimir.lelicanin@origin-trail.com>
2018-07-16 17:22:03 +0200

    Small utilities refactor (#357)

--- modules/Utilities.js
+++ modules/Utilities.js
@@ -445,8 +445,8 @@ class Utilities {
     /**
      * Makes a copy of object
      *
-     * @param object Obj
      * @return object
+     * @param Obj
      */
     static copyObject(Obj) {
         return JSON.parse(JSON.stringify(Obj));
@@ -661,8 +661,8 @@ class Utilities {
             throw new Error(`Given input "${num}" is not a number.`);
         }
 
-        var number = Utilities.toBN(num);
-        var result = number.toString(16);
+        const number = Utilities.toBN(num);
+        const result = number.toString(16);
 
         return number.lt(new BN(0)) ? `-0x${result.substr(1)}` : `0x${result}`;
     }
@@ -872,10 +872,7 @@ class Utilities {
      * @returns {boolean}
      */
     static validateNumberParameter(property) {
-        if (property == null || parseInt(property, 10) > 0) {
-            return true;
-        }
-        return false;
+        return property == null || parseInt(property, 10) > 0;
     }
 
     /**
@@ -884,10 +881,7 @@ class Utilities {
      * @returns {boolean}
      */
     static validateStringParameter(property) {
-        if (property == null || typeof property === 'string') {
-            return true;
-        }
-        return false;
+        return property == null || typeof property === 'string';
     }
 
     /**
