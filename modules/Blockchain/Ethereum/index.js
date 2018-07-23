d94a6987671b6ec36ee9ac08914f1abd7c6c2e52
radomir.sebek <rasho.ieee@gmail.com>
2018-07-23 09:17:50 +0200

    MINOR: Improving createOffer log

--- modules/Blockchain/Ethereum/index.js
+++ modules/Blockchain/Ethereum/index.js
@@ -420,7 +420,7 @@ class Ethereum {
             gasPrice: this.web3.utils.toHex(this.config.gas_price),
             to: this.biddingContractAddress,
         };
-        this.log.trace(`createOffer (${importId}, ${nodeId}, ${totalEscrowTime}, ${maxTokenAmount}, ${MinStakeAmount}, ${minReputation}, ${dataHash}, ${dataSize}, ${predeterminedDhWallets}, ${predeterminedDhNodeIds}`);
+        this.log.trace(`createOffer (${importId}, ${nodeId}, ${totalEscrowTime}, ${maxTokenAmount}, ${MinStakeAmount}, ${minReputation}, ${dataHash}, ${dataSize}, ${predeterminedDhWallets}, ${predeterminedDhNodeIds})`);
         return this.transactions.queueTransaction(
             this.biddingContractAbi, 'createOffer',
             [
