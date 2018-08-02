9543e0f1e8d27d3230303ab5df3ed4a4b146eb5d
radomir.sebek <rasho.ieee@gmail.com>
2018-08-02 12:49:36 +0200

    Separate test data from test logic

--- test/protocol/protocol.test.js
+++ test/protocol/protocol.test.js
@@ -44,6 +44,9 @@ const DCController = require('../../modules/controller/dc-controller');
 // This removes solc's overzealous uncaughtException event handler.
 // process.removeAllListeners('uncaughtException');
 
+// disable debug logs, comment out if you want to see debug logs in travis
+console.debug = function () {};
+
 describe('Protocol tests', () => {
 // Global functions.
     function recreateDatabase() {
