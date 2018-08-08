33308cff86bd1d822bd70feaa9e78df8661612ee
Uros Kukic <kukic.uros@yahoo.com>
2018-08-08 11:29:51 +0200

    Removes double modifier declaration

--- modules/Blockchain/Ethereum/contracts/Escrow.sol
+++ modules/Blockchain/Ethereum/contracts/Escrow.sol
@@ -102,12 +102,7 @@ library SafeMath {
           require(msg.sender != address(0), "Sender address cannot be 0");
           _;
      }
-
-     modifier senderNotZero() {
-          require(msg.sender != address(0), "Sender address cannot be 0");
-          _;
-     }
-
+     
      function EscrowHolder(address tokenAddress)
      public senderNotZero{
           require ( tokenAddress != address(0), "Token address cannot be 0");
