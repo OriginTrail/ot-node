fc657d5002b2d877da2d4ce8ee9b703c02a3ba85
David Aleksic <davidaleksic@Davids-MacBook-Pro.local>
2018-09-07 16:48:55 +0200

    20% done

--- test/modules/command/dc/dc-offer-create-database-command.test.js
+++ test/modules/command/dc/dc-offer-create-database-command.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-const Command = require('../.././../../modules/command/dc/dc-offer-create-database-command');
+const DCOfferCreateDatabaseCommand = require('../.././../../modules/command/dc/dc-offer-create-database-command');
 const models = require('../../../../models/index');
 const BN = require('bn.js');
 const {
@@ -9,8 +9,6 @@ const Utilities = require('../.././../../modules/Utilities');
 const GraphStorage = require('../.././../../modules/Database/GraphStorage');
 const Storage = require('../.././../../modules/Storage');
 const { Database } = require('arangojs');
-
-const ImportUtilities = require('../.././../../modules/ImportUtilities');
 const sequelizeConfig = require('../../../../config/config.json').development;
 const CommandResolver = require('../.././../../modules/command/command-resolver');
 const CommandExecutor = require('../.././../../modules/command/command-executor');
@@ -32,12 +30,18 @@ describe.only('Check for dc offer create database command', function () {
     this.timeout(5000);
     let graphStorage;
     let systemDb;
+    let myConfig;
+    let myGraphStorage;
+    let container;
+
+
     const databaseName = 'dc_offer_create_db';
+
     before('Setup models', async () => {
         Storage.models = (await models.sequelize.sync()).models;
     });
 
-    beforeEach('precondition', async () => {
+    before('precondition', async () => {
         systemDb = new Database();
         systemDb.useBasicAuth(process.env.DB_USERNAME, process.env.DB_PASSWORD);
 
@@ -53,18 +57,48 @@ describe.only('Check for dc offer create database command', function () {
         );
 
         // Create the container and set the injectionMode to PROXY (which is also the default).
-        const container = awilix.createContainer({
+        container = awilix.createContainer({
             injectionMode: awilix.InjectionMode.PROXY,
         });
         const logger = Utilities.getLogger();
         graphStorage = new GraphStorage(buildSelectedDatabaseParam(databaseName), logger);
+
+        container.register({
+            logger: awilix.asValue(Utilities.getLogger()),
+            graphStorage: awilix.asValue(graphStorage),
+            config: awilix.asValue(Utilities.loadConfig()),
+            commandResolver: awilix.asClass(CommandResolver),
+            dcOfferCreateDatabaseCommand: awilix.asClass(DCOfferCreateDatabaseCommand),
+
+        });
+        myGraphStorage = await graphStorage.connect();
+        myConfig = await container.resolve('config');
     });
 
-    it('should console log', () => {
-        console.log('Hello');
+    it('should console log', async () => {
+        const myCommand = {
+            data: {
+                importId: 100005,
+                replicationId: 2,
+                rootHash: 3,
+                total_escrow_time: 4,
+                max_token_amount: 5,
+                min_stake_amount: 6,
+                min_reputation: 7,
+            },
+        };
+
+        const dcOfferCreateDatabaseCommand = container.resolve('dcOfferCreateDatabaseCommand');
+
+        models.sequelize.transaction(async t => dcOfferCreateDatabaseCommand.execute(myCommand, t));
+
+
+        const listOfDatabases1 = await systemDb.listDatabases();
+        const offer =
+            await models.offers.findOne({ where: { import_id: myCommand.data.importId } });
     });
 
-    afterEach('Drop DB', async () => {
+    after('Drop DB', async () => {
         if (systemDb) {
             const listOfDatabases = await systemDb.listDatabases();
             if (listOfDatabases.includes(databaseName)) {
