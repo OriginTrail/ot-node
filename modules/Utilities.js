28fc2974a5c79c1e38a367c7919ce6e4022a778e
Janko Simonovic <simonovic86@gmail.com>
2018-09-11 09:42:56 +0200

    remove log skipping

--- modules/Utilities.js
+++ modules/Utilities.js
@@ -229,11 +229,7 @@ class Utilities {
                     args[1] = msg.stack;
                     origLog.apply(logger, args);
                 } else {
-                    const transformed = Utilities.transformLog(level, msg);
-                    if (!transformed) {
-                        return;
-                    }
-                    origLog.apply(logger, [transformed.level, transformed.msg]);
+                    origLog.apply(logger, [level, msg]);
                 }
             };
             return logger;
@@ -1036,53 +1032,6 @@ class Utilities {
      * @return {*}
      */
     static transformLog(level, msg) {
-        if (msg.startsWith('connection timed out')) {
-            return null; // skip logging
-        }
-        if (msg.startsWith('negotiation error')) {
-            return null; // skip logging
-        }
-        if (msg.includes('received late or invalid response')) {
-            return null; // skip logging
-        }
-        if (msg.includes('error with remote connection')) {
-            return null;
-        }
-        if (msg.includes('remote connection encountered error')) {
-            return null;
-        }
-        if (msg.startsWith('updating peer profile')) {
-            return null; // skip logging
-        }
-        if (msg.includes('client cannot service request at this time')) {
-            return null; // skip logging
-        }
-        if (msg.includes('KADemlia error') && msg.includes('Message previously routed')) {
-            return null; // skip logging
-        }
-        if (msg.includes('gateway timeout')) {
-            return null; // skip logging
-        }
-        if (msg.startsWith('connect econnrefused')) {
-            level = 'trace';
-            const address = msg.substr(21);
-            msg = `Peer ${address} timed out`;
-        }
-        if (msg.includes('HSDir')) {
-            return null;
-        }
-        if (msg.includes('servicesscrubbed.onion')) {
-            return null;
-        }
-        if (msg.includes('unable to route to tunnel')) {
-            return null;
-        }
-        if (msg.includes('socket hang up')) {
-            return null;
-        }
-        if (msg.includes('getaddrinfo')) {
-            return null;
-        }
         return {
             level,
             msg,
