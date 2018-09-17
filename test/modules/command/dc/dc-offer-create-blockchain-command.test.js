e1cfd486316d7e9bccf3213e1b8b823eb179c07f
radomir.sebek <rasho.ieee@gmail.com>
2018-09-18 00:18:33 +0200

    work in progress

--- test/modules/command/dc/dc-offer-create-blockchain-command.test.js
+++ test/modules/command/dc/dc-offer-create-blockchain-command.test.js
@@ -1,5 +1,6 @@
+/* eslint-disable max-len */
 const {
-    describe, before, after, it,
+    describe, before, beforeEach, after, it,
 } = require('mocha');
 const { assert } = require('chai');
 const DCOfferCreateBlockchainCommand = require('../../../../modules/command/dc/dc-offer-create-blockchain-command');
@@ -27,7 +28,7 @@ function buildSelectedDatabaseParam(databaseName) {
     };
 }
 
-describe.only('Checks DCOfferCreateBlockchainCommand', function () {
+describe.only('Checks DCOfferCreateBlockchainCommand execute() logic', function () {
     this.timeout(5000);
     let graphStorage;
     let systemDb;
@@ -35,76 +36,91 @@ describe.only('Checks DCOfferCreateBlockchainCommand', function () {
     let myGraphStorage;
     let container;
     let myCommand;
+    let insertedOfferId;
+    let initializingOfferCalled = false;
+    let getProfileCalled = false;
+    let getReplicationModifierCalled = false;
+    let depositTokenCalled = false;
+    let increaseBiddingApprovalCalled = false;
+    let createOfferCalled = false;
+    let biddingStartedCalled = false;
 
     const databaseName = 'dc_offer_create_db';
 
     class MockRemoteControl {
-        writingRootHash() {
-            console.log('Hi from writingRootHash()');
-            return 0;
+        initializingOffer(someInput) {
+            // console.log('Hi from initializingOffer()');
+            initializingOfferCalled = true;
+            return someInput;
         }
-        initializingOffer(input_arg_1) {
-            console.log('Hi from initializingOffer()');
-            return input_arg_1;
-        }
-        biddingStarted(input_arg_1) {
-            console.log('Hi from biddingStarted()');
-            return input_arg_1;
-        }
-        cancelingOffer() {
-            console.log('Hi from cancelingOffer()');
-            return 0;
-        }
-        biddingComplete() {
-            console.log('Hi from biddingComplete()');
-        }
-        choosingBids() {
-            console.log('Hi from choosingBids()');
-            return 0;
-        }
-        bidChosen() {
-            console.log('Hi from bidChosen()');
-            return 0;
-        }
-        offerFinalized() {
-            console.log('Hi from offerFinalized()');
-            return 0;
-        }
-        dcErrorHandling() {
-            console.log('Hi from dcErrorHandling()');
-            return 0;
-        }
-        bidNotTaken() {
-            console.log('Hi from bidNotTaken()');
-            return 0;
+        biddingStarted(someInput) {
+            // console.log('Hi from biddingStarted()');
+            biddingStartedCalled = true;
+            return someInput;
         }
     }
 
-    class MockBlockchain {
-        async getProfile(input_argument){
-            console.log('Hi from getProfile()');
-            return 10000;
+    class MockBlockchainWithLowProfileBalance {
+        async getProfile(someInput) {
+            // console.log('Hi from getProfile()');
+            getProfileCalled = true;
+            const profile = { balance: 2200000 };
+            return profile;
         }
-        async getReplicationModifier(input_argument){
-            console.log('Hi from getReplicationModifier()');
+        async getReplicationModifier(someInput) {
+            // console.log('Hi from getReplicationModifier()');
+            getReplicationModifierCalled = true;
             return 3;
         }
-        async increaseBiddingApproval(){
-            console.log('Hi from getReplicationModifier()');
-            return input_argument;  
+        async increaseBiddingApproval(someInput) {
+            console.log('Hi from increaseBiddingApproval()');
+            increaseBiddingApprovalCalled = true;
+            return someInput;
         }
-        async depositToken(input_argument){
+        async depositToken(someInput) {
             console.log('Hi from depositToken()');
-            return input_argument;  
+            depositTokenCalled = true;
+            return someInput;
         }
 
-        async createOffer(input_arg_1, input_arg_2, input_arg_3, input_arg_4, input_arg_5, input_arg_6, input_arg_7, input_arg_8, input_arg_9, input_arg_10 ){
-            console.log('Hi from depositToken()');
-            return 0;  
+        async createOffer(inputArg1, inputArg2, inputArg3, inputArg4, inputArg5, inputArg6, inputArg7, inputArg8, inputArg9, inputArg10) {
+            // console.log('Hi from createOffer()');
+            createOfferCalled = true;
+            return 0;
         }
     }
 
-    before('Setup preconditions and call DCOfferCreateBlockchainCommand execute function', async () => {
+    class MockBlockchainWithHighProfileBalance {
+        async getProfile(someInput) {
+            // console.log('Hi from getProfile()');
+            getProfileCalled = true;
+            const profile = { balance: 2895264000005 };
+            return profile;
+        }
+        async getReplicationModifier(someInput) {
+            // console.log('Hi from getReplicationModifier()');
+            getReplicationModifierCalled = true;
+            return 3;
+        }
+        async increaseBiddingApproval(someInput) {
+            console.log('Hi from increaseBiddingApproval()');
+            increaseBiddingApprovalCalled = true;
+            return someInput;
+        }
+        async depositToken(someInput) {
+            console.log('Hi from depositToken()');
+            depositTokenCalled = true;
+            return someInput;
+        }
+
+        async createOffer(inputArg1, inputArg2, inputArg3, inputArg4, inputArg5, inputArg6, inputArg7, inputArg8, inputArg9, inputArg10) {
+            // console.log('Hi from createOffer()');
+            createOfferCalled = true;
+            return 0;
+        }
+    }
+
+    beforeEach('Setup preconditions and call DCOfferCreateBlockchainCommand execute function', async () => {
         Storage.models = (await models.sequelize.sync()).models;
         Storage.db = models.sequelize;
 
@@ -113,6 +129,8 @@ describe.only('Checks DCOfferCreateBlockchainCommand', function () {
             where: {},
             truncate: true,
         });
+
+        await sleep.sleep(1000);
 
         systemDb = new Database();
         systemDb.useBasicAuth(process.env.DB_USERNAME, process.env.DB_PASSWORD);
@@ -133,44 +151,60 @@ describe.only('Checks DCOfferCreateBlockchainCommand', function () {
             injectionMode: awilix.InjectionMode.PROXY,
         });
 
+        // container2 = awilix.createContainer({
+        //     injectionMode: awilix.InjectionMode.PROXY,
+        // });
+
         graphStorage = new GraphStorage(buildSelectedDatabaseParam(databaseName), logger);
 
         container.register({
             logger: awilix.asValue(logger),
             graphStorage: awilix.asValue(graphStorage),
             config: awilix.asValue(Utilities.loadConfig()),
-            blockchain: awilix.asClass(MockBlockchain),
+            blockchain: awilix.asClass(MockBlockchainWithLowProfileBalance),
             remoteControl: awilix.asClass(MockRemoteControl),
             commandResolver: awilix.asClass(CommandResolver),
             dcOfferCreateBlockchainCommand: awilix.asClass(DCOfferCreateBlockchainCommand),
 
         });
+
+        // container2.register({
+        //     logger: awilix.asValue(logger),
+        //     graphStorage: awilix.asValue(graphStorage),
+        //     config: awilix.asValue(Utilities.loadConfig()),
+        //     blockchain: awilix.asClass(MockBlockchainWithHighProfileBalance),
+        //     remoteControl: awilix.asClass(MockRemoteControl),
+        //     commandResolver: awilix.asClass(CommandResolver),
+        //     dcOfferCreateBlockchainCommand: awilix.asClass(DCOfferCreateBlockchainCommand),
+
+        // });
+
         myGraphStorage = await graphStorage.connect();
         myConfig = await container.resolve('config');
 
         myCommand = {
             data: {
                 importId: 5,
-                minStakeAmount: 10e12,
-                maxTokenAmount: new BN(350000000, 10),
+                minStakeAmount: 10000,
+                maxTokenAmount: new BN(50000, 10),
                 minReputation: 0,
                 rootHash: '0xfe109af514aef462b86a02e032d1add2ce59a224cd095aa87716b1ad26aa08ca',
-                dhIds: ['123', '321'],
-                dhWallets: ['0xAC13a2D4cCD1d7Ba29517F96b4eD84D652be5EbD'],
-                importSizeInBytes: 300,
-                totalEscrowTime: 9640000,
-                offerId: 1,
+                dhIds: [],
+                dhWallets: [],
+                importSizeInBytes: new BN(13404, 10),
+                totalEscrowTime: new BN(1440, 10),
+                offerId: 0,
             },
         };
 
         let newOfferRow = {
             import_id: myCommand.data.importId,
-            total_escrow_time: myCommand.data.totalEscrowTime,
-            max_token_amount: myCommand.data.maxTokenAmount,
+            total_escrow_time: myCommand.data.totalEscrowTime.toString(),
+            max_token_amount: myCommand.data.maxTokenAmount.toString(),
             min_stake_amount: myCommand.data.minStakeAmount,
             min_reputation: myCommand.data.minReputation,
             data_hash: myCommand.data.rootHash,
-            data_size_bytes: myCommand.data.afterimportSizeInBytes,
+            data_size_bytes: myCommand.data.importSizeInBytes.toString(),
             dh_wallets: myCommand.data.dhWallets,
             dh_ids: myCommand.data.dhIds,
             message: 'Offer is pending',
@@ -178,27 +212,38 @@ describe.only('Checks DCOfferCreateBlockchainCommand', function () {
             start_tender_time: Date.now(), // TODO: Problem. Actual start time is returned by SC.
             status: 'PENDING',
         };
-        newOfferRow = await models.offers.create(newOfferRow, { });
 
-        let offerId = newOfferRow.id;
+        // mimic task done by DCOfferCreateDatabaseCommand
+        newOfferRow = await models.offers.create(newOfferRow, {});
+        console.log(newOfferRow.id);
+        myCommand.data.offerId = newOfferRow.id;
+        insertedOfferId = newOfferRow.id;
 
         // allow some time for offer to be written to system.db
         await sleep.sleep(1000);
 
-        const dcOfferCreateBlockchainCommand = container.resolve('dcOfferCreateBlockchainCommand');
+        const dcOfferCreateBlockchainCommand = await container.resolve('dcOfferCreateBlockchainCommand');
+        // const dcOfferCreateBlockchainCommand2 = await container2.resolve('dcOfferCreateBlockchainCommand');
 
         // call command's execute function
-        // eslint-disable-next-line max-len
-        // models.sequelize.transaction(async t => dcOfferCreateBlockchainCommand.execute(myCommand.data, t));
-
         dcOfferCreateBlockchainCommand.execute(myCommand);
+        // allow some time for offer to be updated in system.db
+        await sleep.sleep(1000);
     });
 
 
-    it('Check to be created later', async () => {
-        assert.isTrue(2 > 1);
+    it('Check that right methods have been called and status is updated', async () => {
+        assert.isTrue(initializingOfferCalled, 'remoteControl.initializingOffer() should be called');
+        assert.isTrue(getProfileCalled, 'blockchain.getProfile() should be called');
+        assert.isTrue(getReplicationModifierCalled, 'blockchain.getReplicationModifier() should be called');
+        // in case profile satisfies condition
+        // assert.isTrue(depositTokenCalled, "blockchain.depositToken() should be called");
+        // assert.isTrue(increaseBiddingApprovalCalled, "blockchain.increaseBiddingApproval() should be called");
+        assert.isTrue(createOfferCalled, 'blockchain.createOfferCalled() should be called');
+        assert.isTrue(biddingStartedCalled, 'blockchain.biddingStartedCalled() should be called');
 
-        // check that offer status has been updated to STARTED
+        const updatedOffer = await models.offers.findOne({ where: { id: insertedOfferId } });
+        assert.equal(updatedOffer.status, 'STARTED', 'offer.status should be in STARTED state');
     });
 
     after('Drop DB', async () => {
