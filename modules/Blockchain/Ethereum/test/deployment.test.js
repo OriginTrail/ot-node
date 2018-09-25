056fbba0fb6d82a7c591f03e186e9ae23d94c67c
Uros Kukic <kukic.uros@yahoo.com>
2018-09-25 15:50:01 +0200

    Add proper deployment tests and temporarily disable old tests

---
+++ modules/Blockchain/Ethereum/test/deployment.test.js
@@ -0,0 +1,109 @@
+const { assert, expect } = require('chai');
+
+var TestingUtilities = artifacts.require('TestingUtilities'); // eslint-disable-line no-undef
+var TracToken = artifacts.require('TracToken'); // eslint-disable-line no-undef
+
+var Hub = artifacts.require('Hub'); // eslint-disable-line no-undef
+
+var Profile = artifacts.require('Profile'); // eslint-disable-line no-undef
+var Holding = artifacts.require('Holding'); // eslint-disable-line no-undef
+
+var ProfileStorage = artifacts.require('ProfileStorage'); // eslint-disable-line no-undef
+var HoldingStorage = artifacts.require('HoldingStorage'); // eslint-disable-line no-undef
+var Reading = artifacts.require('Reading'); // eslint-disable-line no-undef
+
+var Web3 = require('web3');
+
+// eslint-disable-next-line no-undef
+contract('Deployment tests', async () => {
+    // eslint-disable-next-line no-undef
+    it('Should get Hub contract', async () => {
+        await Hub.deployed()
+        .catch((err) => {
+            assert(false, 'Hub contract is not deployed!');
+        });
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should get TracToken contract', async () => {
+        const hub = await Hub.deployed();
+        const res = await hub.tokenAddress.call();
+        assert.notEqual(
+            res,
+            '0x0000000000000000000000000000000000000000',
+            'TracToken contract address in Hub is not set!',
+        );
+        await TracToken.deployed()
+        .catch((err) => {
+            assert(false, 'TracToken contract is not deployed!');
+        });
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should get Profile contract', async () => {
+        const hub = await Hub.deployed();
+        const res = await hub.profileAddress.call();
+        assert.notEqual(
+            res,
+            '0x0000000000000000000000000000000000000000',
+            'Profile contract address in Hub is not set!',
+        );
+        await Profile.deployed()
+        .catch((err) => {
+            assert(false, 'Profile contract is not deployed!');
+        });
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should get Holding contract', async () => {
+        const hub = await Hub.deployed();
+        const res = await hub.holdingAddress.call();
+        assert.notEqual(
+            res,
+            '0x0000000000000000000000000000000000000000',
+            'Holding contract address in Hub is not set!',
+        );
+        await Holding.deployed()
+        .catch((err) => {
+            assert(false, 'Holding contract is not deployed!');
+        });
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should get ProfileStorage contract', async () => {
+        const hub = await Hub.deployed();
+        const res = await hub.profileStorageAddress.call();
+        assert.notEqual(
+            res,
+            '0x0000000000000000000000000000000000000000',
+            'ProfileStorage contract address in Hub is not set!',
+        );
+        await ProfileStorage.deployed()
+        .catch((err) => {
+            assert(false, 'ProfileStorage contract is not deployed!');
+        });
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should get HoldingStorage contract', async () => {
+        const hub = await Hub.deployed();
+        const res = await hub.holdingStorageAddress.call();
+        assert.notEqual(
+            res,
+            '0x0000000000000000000000000000000000000000',
+            'HoldingStorage contract address in Hub is not set!',
+        );
+        await HoldingStorage.deployed()
+        .catch((err) => {
+            assert(false, 'HoldingStorage contract is not deployed!');
+        });
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should get TestingUtilities contract', async () => {
+        await TestingUtilities.deployed()
+        .catch((err) => {
+            assert(false, 'TestingUtilities contract is not deployed!');
+        }) 
+    });
+});
