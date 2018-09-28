9f8cdf60d8d996911f7ae34ba96e1ad5c0579b32
radomir-sebek <rasho.ieee@gmail.com>
2018-09-28 10:31:32 +0200

    Handle AssertionError in modules/EventEmitter.js:428 [#160786344] (#575)
    * Handle AssertionError in modules/EventEmitter.js:428 [#160786344]
    
    * proper fix

--- modules/EventEmitter.js
+++ modules/EventEmitter.js
@@ -428,7 +428,12 @@ class EventEmitter {
 
         const processImport = async (response, error, data) => {
             if (response === null) {
-                data.response.status(error.status);
+                if (typeof (error.status) !== 'number') {
+                    // TODO investigate why we get non numeric error.status
+                    data.response.status(500);
+                } else {
+                    data.response.status(error.status);
+                }
                 data.response.send({
                     message: error.message,
                 });
