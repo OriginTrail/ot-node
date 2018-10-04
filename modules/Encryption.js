695ce117a247920a1ee135f30ae10787ca963c87
Aleksandar Veljković <35335520+alexveljkovic@users.noreply.github.com>
2018-10-04 15:12:29 +0200

    Feature/message signing [#160815848] (#588)
    - Multiple data signing function

--- modules/Encryption.js
+++ modules/Encryption.js
@@ -4,6 +4,7 @@ const BN = require('bn.js');
 const xor = require('buffer-xor');
 const abi = require('ethereumjs-abi');
 const Utilities = require('./Utilities');
+const utils = require('ethereumjs-util');
 
 class Encryption {
     /**
@@ -295,6 +296,55 @@ class Encryption {
         }
 
         return true;
+    }
+
+    /**
+     * Hash multiple data
+     * @param dataArgs
+     * @returns {string}
+     */
+    static generateHash(dataArgs) {
+        const types = [];
+        const args = [];
+
+        for (const value of dataArgs) {
+            types.push('uint256');
+            args.push(new BN(value, 16));
+        }
+        return `0x${abi.soliditySHA3(types, args).toString('hex')}`;
+    }
+
+    /**
+     * Sign message arguments
+     * @param dataArgs
+     * @param privateKey (padded with 0x)
+     * @returns {Signature object}
+     */
+    static signMessage(web3, dataArgs, privateKey) {
+        const hash = this.generateHash(dataArgs);
+        const signature = web3.eth.accounts.sign(hash, privateKey);
+        return signature;
+    }
+
+    /**
+     * Extract signer's address from signature for given data
+     * @param dataArgs array
+     * @param signature
+     * @returns {string}
+     */
+    static extractSignerAddress(dataArgs, signature) {
+        const r = utils.toBuffer(signature.slice(0, 66));
+        const s = utils.toBuffer(`0x${signature.slice(66, 130)}`);
+        const v = Utilities.hexToNumber(utils.toBuffer(`0x${signature.slice(130, 132)}`).toString('hex'));
+
+        const dataHash = this.generateHash(dataArgs).slice(2);
+
+        const msg = `0x${abi.soliditySHA3(['bytes', 'bytes32'], [Buffer.from('\x19Ethereum Signed Message:\n32', 'utf-8'), new BN(dataHash, 16)]).toString('hex')}`;
+
+        const m = utils.toBuffer(msg);
+        const pub = utils.ecrecover(m, v, r, s);
+        const adr = `0x${utils.pubToAddress(pub).toString('hex')}`;
+        return adr;
     }
 
     /**
