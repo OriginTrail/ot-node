783004463a4cd5eac63ab47f7f812e898ec95104
Uros Kukic <kukic.uros@yahoo.com>
2018-10-05 16:07:55 +0200

    Increase number of runs for solidity compiler optimizer

--- modules/Blockchain/Ethereum/truffle.js
+++ modules/Blockchain/Ethereum/truffle.js
@@ -9,7 +9,7 @@ module.exports = {
             settings: {
                 optimizer: {
                     enabled: true,
-                    runs: 1,
+                    runs: 200000,
                 },
             },
         },
