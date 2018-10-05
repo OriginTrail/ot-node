9c6417d8d79519a6f30407b76c0b7fcef30a78d8
Uros Kukic <kukic.uros@yahoo.com>
2018-10-05 16:10:02 +0200

    Increase gas when deploying Profile contract

--- modules/Blockchain/Ethereum/migrations/2_total_migration.js
+++ modules/Blockchain/Ethereum/migrations/2_total_migration.js
@@ -56,7 +56,7 @@ module.exports = async (deployer, network, accounts) => {
         token = await deployer.deploy(TracToken, accounts[0], accounts[1], accounts[2]);
         await hub.setTokenAddress(token.address);
 
-        profile = await deployer.deploy(Profile, hub.address, { gas: 6000000, from: accounts[0] });
+        profile = await deployer.deploy(Profile, hub.address, { gas: 9000000, from: accounts[0] });
         await hub.setProfileAddress(profile.address);
 
         holding = await deployer.deploy(Holding, hub.address, { gas: 6000000, from: accounts[0] });
