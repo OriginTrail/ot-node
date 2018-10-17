f72be6ac7bf888deb9d1c24b63a4d15ab243ad85
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-10-17 14:12:26 +0200

    added missing file

---
+++ modules/Database/graph-converter.js
@@ -0,0 +1,31 @@
+
+function denormalizeGraph(importId, vertices, edges) {
+    const denormalizedVertices = [];
+    const denormalizedEdges = [];
+
+    vertices.forEach((vertex) => {
+        const denormalizedVertex = {
+            [importId]: {},
+            // TODO: some R here.
+        };
+
+        denormalizedVertex[importId].data = vertex.data;
+        denormalizedVertex[importId].identifiers = vertex.identifiers;
+        denormalizedVertex[importId].sender_id = vertex.sender_id;
+        denormalizedVertex[importId].private = vertex.private; // TODO: ?
+        denormalizedVertex[importId].randomness = vertex.randomness; // TODO: ?
+
+        denormalizedVertex._key = vertex.identifiers.id;
+        denormalizedVertex.vertex_type = vertex.vertex_type;
+        denormalizedVertices.push(denormalizedVertex);
+    });
+
+    return {
+        vertices: denormalizedVertices,
+        edges: denormalizedEdges,
+    };
+}
+
+module.exports =  {
+    denormalizeGraph,
+};
