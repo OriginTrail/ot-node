9316c0201aae58647fe0c4291b67b80da4662c67
Uros Kukic <kukic.uros@yahoo.com>
2018-10-29 14:40:45 +0100

    Update approval servive to use new Approval contract

--- modules/service/approval-service.js
+++ modules/service/approval-service.js
@@ -18,24 +18,74 @@ class ApprovalService {
      * Load all nodes currently approved for functioning on the network
      */
     async initialize() {
-        this.approvedNodes = await this.blockchain.getApprovedNodes();
+        const allNodes = await this.blockchain.getAddedNodes();
+        var approvedNodes = [];
+
+        var promises;
+        for (var i = allNodes.length - 1; i >= 0; i -= 1) {
+            promises[i] = this.blockchain.nodeHasApproval(allNodes[i]);
+        }
+        const nodeApproved = await Promise.all(promises);
+
+        for (i = 0; i < allNodes.length; i += 1) {
+            if (nodeApproved[i] === true) {
+                allNodes[i] = allNodes[i].toLowerCase();
+                allNodes[i] = Utilities.normalizeHex(allNodes[i]);
+                if (allNodes[i].length > 42) {
+                    allNodes[i] = allNodes[i].substr(-40, 40);
+                    allNodes[i] = Utilities.normalizeHex(allNodes[i]);
+                }
+                approvedNodes.push(allNodes[i]);
+            }
+        }
+        this.approvedNodes = approvedNodes;
     }
 
     /**
      * Return all nodes currently approved for functioning on the network
      */
-    async getApprovedNodes() {
+    getApprovedNodes() {
         return this.approvedNodes;
     }
 
     /**
-     * Return all nodes currently approved for functioning on the network
+     * Add a single node to the approved nodes
+     * @param nodeId
      */
-    async addApprovedNode(nodeId) {
+    addApprovedNode(nodeId) {
+        nodeId = nodeId.toLowerCase();
+        nodeId = Utilities.normalizeHex(nodeId);
+        if (nodeId.length > 42) {
+            nodeId = nodeId.substr(-40, 40);
+            nodeId = Utilities.normalizeHex(nodeId);
+        }
         this.approvedNodes.push(nodeId);
     }
 
+    /**
+     * Remove a signle node from the approved nodes
+     * @param nodeId
+     */
+    removeApprovedNode(nodeId) {
+        nodeId = nodeId.toLowerCase();
+        nodeId = Utilities.normalizeHex(nodeId);
+        if (nodeId.length > 42) {
+            nodeId = nodeId.substr(-40, 40);
+            nodeId = Utilities.normalizeHex(nodeId);
+        }
+        const index = this.approvedNodes.indexOf(nodeId);
+        if (index > -1) {
+            this.approvedNodes.splice(index, 1);
+        }
+    }
+
     isApproved(nodeId) {
+        nodeId = nodeId.toLowerCase();
+        nodeId = Utilities.normalizeHex(nodeId);
+        if (nodeId.length > 42) {
+            nodeId = nodeId.substr(-40, 40);
+            nodeId = Utilities.normalizeHex(nodeId);
+        }
         return this.approvedNodes.contains(nodeId);
     }
 }
