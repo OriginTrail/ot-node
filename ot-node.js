c39192fe14a4b978521d9bfc2fc7d616e3cb3c86
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2018-11-07 16:03:01 +0100

    Support mariner environment

--- ot-node.js
+++ ot-node.js
@@ -58,7 +58,7 @@ global.__basedir = __dirname;
 let context;
 const defaultConfig = configjson[
     process.env.NODE_ENV &&
-    ['development', 'staging', 'stable', 'production'].indexOf(process.env.NODE_ENV) >= 0 ?
+    ['development', 'staging', 'stable', 'mariner', 'production'].indexOf(process.env.NODE_ENV) >= 0 ?
         process.env.NODE_ENV : 'development'];
 
 let config;
