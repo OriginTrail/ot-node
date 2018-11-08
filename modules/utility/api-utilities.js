bb4267965df9a71ad6796f376b9b1054773e407d
Vuk Djoric <vuk.djoric@origin-trail.com>
2018-11-08 14:26:04 +0100

    fix authorize

--- modules/utility/api-utilities.js
+++ modules/utility/api-utilities.js
@@ -15,11 +15,6 @@ class APIUtilities {
         const request_ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         const remote_access = this.config.remote_access_whitelist;
 
-        // TODO: Temp solution for local network. Ignore whitelist.
-        if (ip.isLoopback(this.config.node_rpc_ip)) {
-            return true;
-        }
-
         if (remote_access.length > 0 && !remote_access.includes(request_ip)) {
             res.status(403);
             res.send({
