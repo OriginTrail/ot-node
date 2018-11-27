29bdb537743f09c93b00a46727e7a1656e37b973
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-11-27 14:52:36 +0100

    fix

--- autoupdater.js
+++ autoupdater.js
@@ -67,7 +67,7 @@ process.once('message', async ([options]) => {
                     console.log('npm modules have been installed');
                     console.log('Migrating node configuration');
 
-                    console.log(`cp -r ${options.appDataPath} /ot-node/${options.version}/`);
+                    console.log(`cp -r ${options.appDataPath}/ /ot-node/${options.version}/`);
                     execSync(`cp -r ${options.appDataPath} /ot-node/${options.version}/`);
                     console.log('Configuration migration complete');
 
