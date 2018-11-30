e51a9f6aa4ccb3ea129541091109605a61bac9db
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-11-30 11:45:53 +0100

    lint fix

--- autoupdater.js
+++ autoupdater.js
@@ -23,73 +23,72 @@ process.once('message', async ([options]) => {
                 console.log(err);
             },
 
-            try {
-                console.log('Update extraction complete');
-        console.log(`Moving update to directory ${options.version}...`);
+            success() {
+                try {
+                    console.log('Update extraction complete');
+                    console.log(`Moving update to directory ${options.version}...`);
 
-        console.log('Cleaning update destination directory');
-        execSync(`rm -rf ../${options.version}`);
+                    console.log('Cleaning update destination directory');
+                    execSync(`rm -rf ../${options.version}`);
 
-        let extractedFileName = options.autoUpdater.branch;
-        console.log(extractedFileName);
-        extractedFileName = `ot-node-${extractedFileName.replace('/', '-')}`;
-        execSync(`mv ../${extractedFileName} ../${options.version}`);
+                    let extractedFileName = options.autoUpdater.branch;
+                    console.log(extractedFileName);
+                    extractedFileName = `ot-node-${extractedFileName.replace('/', '-')}`;
+                    execSync(`mv ../${extractedFileName} ../${options.version}`);
 
-        console.log(`Update has been moved to directory ${options.version}`);
-        console.log('Migrating node modules...');
+                    console.log(`Update has been moved to directory ${options.version}`);
+                    console.log('Migrating node modules...');
 
-        execSync(`cp -r ./node_modules ../${options.version}/`);
-        console.log('Node modules migrated');
-        console.log('Installing new node modules');
+                    execSync(`cp -r ./node_modules ../${options.version}/`);
+                    console.log('Node modules migrated');
+                    console.log('Installing new node modules');
 
-        execSync(`cd ../${options.version} && npm install`);
-        console.log('npm modules have been installed');
-        console.log('Migrating node configuration');
+                    execSync(`cd ../${options.version} && npm install`);
+                    console.log('npm modules have been installed');
+                    console.log('Migrating node configuration');
 
-        execSync(`cp -r ${options.appDataPath} /ot-node/${options.version}/`);
-        console.log('Configuration migration complete');
+                    execSync(`cp -r ${options.appDataPath} /ot-node/${options.version}/`);
+                    console.log('Configuration migration complete');
 
-        const SEQUELIZEDB_OLD = process.env.SEQUELIZEDB;
-        process.env.SEQUELIZEDB = `/ot-node/${options.version}/data/system.db`;
+                    const SEQUELIZEDB_OLD = process.env.SEQUELIZEDB;
+                    process.env.SEQUELIZEDB = `/ot-node/${options.version}/data/system.db`;
 
-        // eslint-disable-next-line
-        const Models = require(`../${options.version}/models`);
+                    // eslint-disable-next-line
+                    const Models = require(`../${options.version}/models`);
 
-        const umzug_migrations = new Umzug({
+                    const umzug_migrations = new Umzug({
 
-            storage: 'sequelize',
+                        storage: 'sequelize',
 
-            storageOptions: {
-                sequelize: Models.sequelize,
-            },
+                        storageOptions: {
+                            sequelize: Models.sequelize,
+                        },
 
-            migrations: {
-                params: [Models.sequelize.getQueryInterface(),
-                    Models.sequelize.constructor, () => {
-                        throw new Error('Migration tried to use old style "done" callback. Please upgrade to "umzug" and return a promise instead.');
-                    }],
-                path: `../${options.version}/migrations`,
-                pattern: /\.js$/,
-            },
-        });
+                        migrations: {
+                            params: [Models.sequelize.getQueryInterface(),
+                                Models.sequelize.constructor, () => {
+                                    throw new Error('Migration tried to use old style "done" callback. Please upgrade to "umzug" and return a promise instead.');
+                                }],
+                            path: `../${options.version}/migrations`,
+                            pattern: /\.js$/,
+                        },
+                    });
 
-        umzug_migrations.up().then((migrations) => {
-            console.log('Database migrated.');
-            console.log('Switching node version');
-            process.send('complete');
-            execSync(`ln -sfn /ot-node/${options.version}/ /ot-node/current`);
-            process.exit(0);
-        }).catch((err) => {
-            console.log('Update failed');
-            process.env.SEQUELIZEDB = SEQUELIZEDB_OLD;
-            console.log(err);
-        });
-
-    } catch (err) {
-            // TODO: Rollback
-            console.log(err);
-        }
-        success() {
+                    umzug_migrations.up().then((migrations) => {
+                        console.log('Database migrated.');
+                        console.log('Switching node version');
+                        process.send('complete');
+                        execSync(`ln -sfn /ot-node/${options.version}/ /ot-node/current`);
+                        process.exit(0);
+                    }).catch((err) => {
+                        console.log('Update failed');
+                        process.env.SEQUELIZEDB = SEQUELIZEDB_OLD;
+                        console.log(err);
+                    });
+                } catch (err) {
+                    // TODO: Rollback
+                    console.log(err);
+                }
             },
         });
     });
