e9367cb664083cb1b6320e80677ccb3aadf1efdc
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-12-03 09:58:51 +0100

    new directions

--- modules/Utilities.js
+++ modules/Utilities.js
@@ -171,6 +171,100 @@ class Utilities {
             });
         });
     }
+    checkForUpdates(options) {
+        return new Promise(async (resolve, reject) => {
+                const filename = `https://github.com/${options.autoUpdater.repo}/archive/${options.autoUpdater.branch}.zip`;
+                logger.info(`Downloading update: ${filename} ...`);
+                const response = await request(filename);
+                const filestream = fs.createWriteStream('update.zip');
+                response.pipe(filestream);
+
+                filestream.on('finish', () => {
+                    logger.info('Download complete');
+                    logger.info('Extracting update...');
+
+                    Zip.unzip({
+                        source: 'update.zip',
+                        destination: '..',
+                    }).exec({
+                        error(err) {
+                            logger.error(err);
+                        },
+
+                        success() {
+                            try {
+                                logger.info('Update extraction complete');
+                                logger.info(`Moving update to directory ${options.version}...`);
+
+                                logger.info('Cleaning update destination directory');
+                                execSync(`rm -rf ../${options.version}`);
+
+                                let extractedFileName = options.autoUpdater.branch;
+                                logger.info(extractedFileName);
+                                extractedFileName = `ot-node-${extractedFileName.replace('/', '-')}`;
+                                execSync(`mv ../${extractedFileName} ../${options.version}`);
+
+                                logger.info(`Update has been moved to directory ${options.version}`);
+                                logger.info('Migrating node modules...');
+
+                                execSync(`cp -r ./node_modules ../${options.version}/`);
+                                logger.info('Node modules migrated');
+                                logger.info('Installing new node modules');
+
+                                execSync(`cd ../${options.version} && npm install`);
+                                logger.info('npm modules have been installed');
+                                logger.info('Migrating node configuration');
+
+                                execSync(`cp -r ${options.appDataPath} /ot-node/${options.version}/`);
+                                logger.info('Configuration migration complete');
+
+                                const SEQUELIZEDB_OLD = process.env.SEQUELIZEDB;
+                                process.env.SEQUELIZEDB = `/ot-node/${options.version}/data/system.db`;
+
+                                // eslint-disable-next-line
+                                const Models = require(`../${options.version}/models`);
+
+                                const umzug_migrations = new Umzug({
+
+                                    storage: 'sequelize',
+
+                                    storageOptions: {
+                                        sequelize: Models.sequelize,
+                                    },
+
+                                    migrations: {
+                                        params: [Models.sequelize.getQueryInterface(),
+                                            Models.sequelize.constructor, () => {
+                                                throw new Error('Migration tried to use old style "done" callback. Please upgrade to "umzug" and return a promise instead.');
+                                            }],
+                                        path: `../${options.version}/migrations`,
+                                        pattern: /\.js$/,
+                                    },
+                                });
+
+                                umzug_migrations.up().then((migrations) => {
+                                    logger.info('Database migrated.');
+                                    logger.info('Switching node version');
+                                    process.send('complete');
+                                    execSync(`ln -sfn /ot-node/${options.version}/ /ot-node/current`);
+                                    process.exit(0);
+                                }).catch((err) => {
+                                    logger.error('Update failed');
+                                    process.env.SEQUELIZEDB = SEQUELIZEDB_OLD;
+                                    logger.error('Database migrations failed');
+                                    logger.error(err);
+                                });
+                            } catch (err) {
+                                // TODO: Rollback
+                                logger.error('Update failed');
+                                logger.error(err);
+                            }
+                        },
+                    });
+                });
+            });
+        }
+    }
 
     /**
      * Returns solidity keccak256 hash of given data
