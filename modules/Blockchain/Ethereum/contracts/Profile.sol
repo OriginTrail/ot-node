ac99fc8914ebaa1387cd3c904aceb916286423bf
Uros Kukic <kukic.uros@yahoo.com>
2018-12-05 11:31:34 +0100

    Require a node to submit a nodeId when creating a profile

--- modules/Blockchain/Ethereum/contracts/Profile.sol
+++ modules/Blockchain/Ethereum/contracts/Profile.sol
@@ -6,6 +6,7 @@ import {Ownable, Hub} from './Hub.sol';
 import {Identity, ERC725} from './Identity.sol';
 import {ERC20} from './TracToken.sol';
 
+
 contract Profile {
     using SafeMath for uint256;
     Hub public hub;
@@ -44,6 +45,7 @@ contract Profile {
         ERC20 tokenContract = ERC20(hub.tokenAddress());
         require(tokenContract.allowance(msg.sender, this) >= initialBalance, "Sender allowance must be equal to or higher than initial balance");
         require(tokenContract.balanceOf(msg.sender) >= initialBalance, "Sender balance must be equal to or higher than initial balance!");
+        require(uint256(profileNodeId) != 0, "Cannot create a profile without a nodeId submitted");
 
         tokenContract.transferFrom(msg.sender, address(profileStorage), initialBalance);
 
@@ -147,7 +149,7 @@ contract Profile {
             profileStorage.getStake(identity).sub(profileStorage.getWithdrawalAmount(identity))
         );
     }
-    
+
     function reserveTokens(address payer, address identity1, address identity2, address identity3, uint256 amount)
     public onlyHolding {
         if(profileStorage.getWithdrawalPending(payer)) {
