804a1b4845bbf55bc73e1c160f415c9c670d70a3
Uros Kukic <kukic.uros@yahoo.com>
2018-12-06 13:16:51 +0100

    Simplify token withdrawal logic

--- modules/Blockchain/Ethereum/contracts/Profile.sol
+++ modules/Blockchain/Ethereum/contracts/Profile.sol
@@ -92,21 +92,7 @@ contract Profile {
         // Verify sender
         require(ERC725(identity).keyHasPurpose(keccak256(abi.encodePacked(msg.sender)), 2));
 
-        if(profileStorage.getWithdrawalPending(identity)){
-            if(block.timestamp < profileStorage.getWithdrawalTimestamp(identity)){
-                // Transfer already reserved tokens to user identity
-                profileStorage.transferTokens(msg.sender, profileStorage.getWithdrawalAmount(identity));
-                
-                uint256 balance = profileStorage.getStake(identity);
-                balance = balance.sub(profileStorage.getWithdrawalAmount(identity));
-                profileStorage.setStake(identity, balance);
-                
-                emit TokensWithdrawn(identity, profileStorage.getWithdrawalAmount(identity), balance);
-            }
-            else {
-                require(false, "Withrdrawal process already pending!");
-            }
-        }
+        require(profileStorage.getWithdrawalPending(identity) == false, "Withrdrawal process already pending!");
 
         uint256 availableBalance = profileStorage.getStake(identity).sub(profileStorage.getStakeReserved(identity));
 
