1efe3ecd0f747717890048faffe815117e1e2e08
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2018-12-07 18:43:46 +0100

    fixes

--- modules/GS1Importer.js
+++ modules/GS1Importer.js
@@ -788,9 +788,6 @@ class GS1Importer {
             );
 
             const objectClasses = await this.db.findObjectClassVertices();
-            if (objectClasses.length === 0) {
-                throw Error('Missing class vertices');
-            }
 
             const dataSetId = ImportUtilities.importHash(
                 importId,
