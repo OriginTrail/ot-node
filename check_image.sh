6c8b28a599cabe0f145c2547cccaa97debccc8f4
radomir.sebek <rasho.ieee@gmail.com>
2018-12-12 12:26:48 +0100

    fix 7

--- check_image.sh
+++ check_image.sh
@@ -11,5 +11,4 @@ docker exec mynode /bin/sh -c "ls logs/"
 docker exec mynode /bin/sh -c "cat logs/*.log"
 docker exec mynode /bin/sh -c "curl -X POST http://127.0.0.1:8900/api/import -F importtype=GS1 -F importfile=@/ot-node/importers/xml_examples/Retail/03_Pink_to_Orange_shipment.xml -F replicate=true" > importResult.json
 cat importResult.json
-docker stop mynode
-docker rm mynode
+# TODO assert that import result has two keys, data_set_is and replication_id
