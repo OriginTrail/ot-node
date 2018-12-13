c4a7dc7c589d9aaba31b610362e84080dd8e1fc6
radomir.sebek <rasho.ieee@gmail.com>
2018-12-13 08:21:03 +0100

    fix hub_contract_address

--- check_image.sh
+++ check_image.sh
@@ -6,10 +6,13 @@ docker ps -a
 docker build --file ./Dockerfile.development -t myimage:latest .
 docker images
 docker run -d --name=mynode -p 8900:8900 -p 5278:5278 -p 3000:3000 --network host -v $PWD/.origintrail_noderc.image:/ot-node/.origintrail_noderc myimage:latest
+# TODO make sure that following hub contract address is in .origintrail_noderc.image
+cat $ARTIFACTS_DIR/truffle-migrate.log | grep "Hub contract address:"
 sleep 540
 # docker exec mynode /bin/sh -c "ls logs/"
 # docker exec mynode /bin/sh -c "cat logs/*.log"
 docker container inspect mynode
+docker network inspect host
 docker exec mynode /bin/sh -c "curl -X POST http://127.0.0.1:8900/api/import -F importtype=GS1 -F importfile=@/ot-node/importers/xml_examples/Retail/03_Pink_to_Orange_shipment.xml -F replicate=true" > importResult.json
 cat importResult.json
 # TODO assert that import result has two keys, data_set_is and replication_id
