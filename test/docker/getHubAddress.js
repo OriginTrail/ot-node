8ad16c79a4e8e284a21b7eb460cb3cc85843db7a
radomir.sebek <rasho.ieee@gmail.com>
2018-12-27 10:23:07 +0100

    incorporating next round of review comments

--- test/docker/getHubAddress.js
+++ test/docker/getHubAddress.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
+
 const fs = require('fs');
 
-const fileToRead = './artifacts/truffle-migrate.log';
+const fileToRead = `${ARTIFACTS_DIR}/truffle-migrate.log`;
 
 fs.readFile(fileToRead, (err, data) => {
     if (err) throw err;
@@ -9,9 +11,9 @@ fs.readFile(fileToRead, (err, data) => {
         const begining = data.toString().indexOf(hint) + hint.length;
         const end = begining + 42;
         const hubContractAddress = data.toString().substring(begining, end);
-        const jsonData = JSON.parse(fs.readFileSync('.origintrail_noderc.image'));
+        const jsonData = JSON.parse(fs.readFileSync(`${TRAVIS_BUILD_DIR}/.origintrail_noderc.image`));
         jsonData.blockchain.hub_contract_address = hubContractAddress;
-        fs.writeFileSync('.origintrail_noderc.image', JSON.stringify(jsonData));
+        fs.writeFileSync(`${TRAVIS_BUILD_DIR}/.origintrail_noderc.image`, JSON.stringify(jsonData));
     } else {
         console.log('Hub not found, something is wrong!');
         process.exit(-1);
