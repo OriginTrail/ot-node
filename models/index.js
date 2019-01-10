794211e2e7c11110cdcad30392966990559b8c09
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-01-10 14:22:23 +0100

    Revert "debug"
    This reverts commit dabf9ff5d6940cd8e027c4ac154b5d41caa4257e.

--- models/index.js
+++ models/index.js
@@ -20,7 +20,7 @@ if (config.use_env_variable) {
 } else {
     sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-console.log('DEBUG idex opt storage', sequelize.options.storage);
+
 fs
     .readdirSync(__dirname)
     .filter(file => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
