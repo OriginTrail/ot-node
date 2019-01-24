778381e4e5d4c05f2288c1716b45a34ed8562724
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-01-24 16:46:38 +0100

    test

--- setup_arangodb.sh
+++ setup_arangodb.sh
@@ -12,9 +12,8 @@ sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install arangodb3=3.4.1-1
 sudo sed -i 's/authentication = true/authentication = false/g' /etc/arangodb3/arangod.conf
-cat /etc/arangodb3/arangod.conf
-#sudo systemctl start arangodb3
-sudo /etc/init.d/arangodb3 start
+sudo arango-secure-installation
+sudo systemctl start arangodb3
 
 echo "Waiting until ArangoDB is ready on port 8529"
 
