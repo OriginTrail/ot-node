48fadb07ca38ec5f3b5ae47064434c95f10ffd3b
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-01-24 12:40:56 +0100

    Fix

--- setup_arangodb.sh
+++ setup_arangodb.sh
@@ -1,50 +1,16 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-
-VERSION=3.2.2
-NAME=ArangoDB-$VERSION
-
-if [ ! -d "$DIR/$NAME" ]; then
-  # download ArangoDB
-  echo "curl -L -o $NAME.tar.gz https://www.arangodb.com/repositories/travisCI/$NAME.tar.gz"
-  curl -L -o $NAME.tar.gz https://www.arangodb.com/repositories/travisCI/$NAME.tar.gz
-  echo "tar zxf $NAME.tar.gz"
-  tar zxf $NAME.tar.gz
-fi
-
-ARCH=$(arch)
-PID=$(echo $PPID)
-TMP_DIR="/tmp/arangodb.$PID"
-PID_FILE="/tmp/arangodb.$PID.pid"
-ARANGODB_DIR="$DIR/$NAME"
-ARANGOD="${ARANGODB_DIR}/bin/arangod_x86_64"
-
-# create database directory
-mkdir ${TMP_DIR}
-
-echo "Starting ArangoDB '${ARANGOD}'"
-
-${ARANGOD} \
-    --database.directory ${TMP_DIR} \
-    --configuration none \
-    --server.endpoint tcp://127.0.0.1:8529 \
-    --javascript.app-path ${ARANGODB_DIR}/js/apps \
-    --javascript.startup-directory ${ARANGODB_DIR}/js \
-    --database.maximal-journal-size 1048576 \
-    --server.authentication false &
-
-sleep 2
-
-echo "Check for arangod process"
-process=$(ps auxww | grep "bin/arangod" | grep -v grep)
-
-if [ "x$process" == "x" ]; then
-  echo "no 'arangod' process found"
-  echo "ARCH = $ARCH"
-  exit 1
-fi
+echo arangodb3 arangodb/password password root | sudo debconf-set-selections  # set username 'root'
+echo arangodb3 arangodb/password_again password root | sudo debconf-set-selections  # set password 'root'
+curl -OL https://download.arangodb.com/arangodb34/DEBIAN/Release.key
+sudo apt-key add - < Release.key
+echo 'deb https://download.arangodb.com/arangodb34/DEBIAN/ /' | sudo tee /etc/apt/sources.list.d/arangodb.list
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install arangodb3=3.4.1-1
+sudo sed -i 's/authentication = true/authentication = false/g' /etc/arangodb3/arangod.conf
+cat /etc/arangodb3/arangod.conf
+sudo systemctl start arangodb3
 
 echo "Waiting until ArangoDB is ready on port 8529"
 
@@ -62,6 +28,5 @@ then
     echo "Could not start ArangoDB. Timeout reached."
     exit 1
 fi
-
 
 echo "ArangoDB is up"
