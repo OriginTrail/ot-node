ed01e2b1cc30b5e8f1adeb74202c304b0fe9f48c
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-01-25 13:11:52 +0100

    arango3

--- setup_arangodb.sh
+++ setup_arangodb.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-export ARANGODB_DEFAULT_ROOT_PASSWORD=root
+curl -OL https://download.arangodb.com/arangodb33/xUbuntu_16.04/Release.key
+apt-key add - < Release.key
+echo 'deb https://download.arangodb.com/arangodb33/xUbuntu_16.04/ /' | tee /etc/apt/sources.list.d/arangodb.list
+apt-get install apt-transport-https -y
+apt-get update -y
+echo arangodb3 arangodb3/backup boolean false | debconf-set-selections
+echo arangodb3 arangodb3/upgrade boolean true | debconf-set-selections
+echo arangodb3 arangodb3/storage_engine select mmfiles | debconf-set-selections
+echo arangodb3 arangodb3/password password root | debconf-set-selections
+echo arangodb3 arangodb3/password_again password root | debconf-set-selections
+apt-get install arangodb3=3.3.12 -y
 
-curl -OL https://download.arangodb.com/arangodb34/DEBIAN/Release.key
-sudo apt-key add - < Release.key
-echo 'deb https://download.arangodb.com/arangodb34/DEBIAN/ /' | sudo tee /etc/apt/sources.list.d/arangodb.list
-sudo apt-get install apt-transport-https
-sudo apt-get update
-echo arangodb3 arangodb3/password password root | sudo debconf-set-selections  # set username 'root'
-echo arangodb3 arangodb3/password_again password root | sudo debconf-set-selections  # set password 'root'
-sudo apt-get install arangodb3=3.4.1-1
-sudo sed -i 's/authentication = true/authentication = false/g' /etc/arangodb3/arangod.conf
-sudo systemctl start arangodb3
+sed -i 's/authentication = true/authentication = false/g' /etc/arangodb3/arangod.conf
+systemctl start arangodb3
 
 echo "Waiting until ArangoDB is ready on port 8529"
 
