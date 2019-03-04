1d99e9ed3b6b93c7f8daf910bdaa8d1942591bbf
Aleksandar Veljkovic <aleksandar.veljkovic@origin-trail.com>
2019-03-04 11:39:46 +0100

    Fixed miner no solution message

--- modules/EventEmitter.js
+++ modules/EventEmitter.js
@@ -1005,7 +1005,7 @@ class EventEmitter {
 
         this._on('int-miner-solution', async (err, data) => {
             if (err) {
-                await dcService.miningFailed(data.offerId);
+                await dcService.miningFailed(err);
             } else {
                 await dcService.miningSucceed(data);
             }
