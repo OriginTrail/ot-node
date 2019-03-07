129912f51499db6d3e64a8a961d8f513d2da8288
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-03-07 10:13:43 +0100

    Fix BDD tests

--- test/bdd/steps/network.js
+++ test/bdd/steps/network.js
@@ -123,7 +123,7 @@ Given(/^I setup (\d+) node[s]*$/, { timeout: 120000 }, function (nodeCount, done
             },
             blockchain: {
                 hub_contract_address: this.state.localBlockchain.hubContractAddress,
-                rpc_node_host: 'http://localhost:7545/', // TODO use from instance
+                rpc_server_url: 'http://localhost:7545/', // TODO use from instance
             },
             local_network_only: true,
             dc_choose_time: 60000, // 1 minute
@@ -567,7 +567,7 @@ Given(/^I additionally setup (\d+) node[s]*$/, { timeout: 30000 }, function (nod
                 },
                 blockchain: {
                     hub_contract_address: this.state.localBlockchain.hubContractAddress,
-                    rpc_node_host: 'http://localhost:7545/', // TODO use from instance
+                    rpc_server_url: 'http://localhost:7545/', // TODO use from instance
                 },
                 local_network_only: true,
                 initial_deposit_amount: '10000000000000000000000',
