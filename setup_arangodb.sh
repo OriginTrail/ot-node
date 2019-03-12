0c59f8c038f5f95e1fe5d2f288a348f77e14afd8
Janko Simonovic <simonovic86@gmail.com>
2019-03-12 15:58:40 +0100

    Revert "merge develop changes into this patch"
    This reverts commit e52a56cd0d121fb56f0d66de11a62ac394882589.

--- setup_arangodb.sh
+++ setup_arangodb.sh
@@ -1,19 +1,50 @@
 #!/bin/bash
 
-curl -OL https://download.arangodb.com/arangodb33/xUbuntu_16.04/Release.key
-apt-key add - < Release.key
-echo 'deb https://download.arangodb.com/arangodb33/xUbuntu_16.04/ /' | tee /etc/apt/sources.list.d/arangodb.list
-apt-get install apt-transport-https -y
-apt-get update -y
-echo arangodb3 arangodb3/backup boolean false | debconf-set-selections
-echo arangodb3 arangodb3/upgrade boolean true | debconf-set-selections
-echo arangodb3 arangodb3/storage_engine select mmfiles | debconf-set-selections
-echo arangodb3 arangodb3/password password root | debconf-set-selections
-echo arangodb3 arangodb3/password_again password root | debconf-set-selections
-apt-get install arangodb3=3.3.12 -y
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
 
-sed -i 's/authentication = true/authentication = false/g' /etc/arangodb3/arangod.conf
-systemctl start arangodb3
+VERSION=3.2.2
+NAME=ArangoDB-$VERSION
+
+if [ ! -d "$DIR/$NAME" ]; then
+  # download ArangoDB
+  echo "curl -L -o $NAME.tar.gz https://www.arangodb.com/repositories/travisCI/$NAME.tar.gz"
+  curl -L -o $NAME.tar.gz https://www.arangodb.com/repositories/travisCI/$NAME.tar.gz
+  echo "tar zxf $NAME.tar.gz"
+  tar zxf $NAME.tar.gz
+fi
+
+ARCH=$(arch)
+PID=$(echo $PPID)
+TMP_DIR="/tmp/arangodb.$PID"
+PID_FILE="/tmp/arangodb.$PID.pid"
+ARANGODB_DIR="$DIR/$NAME"
+ARANGOD="${ARANGODB_DIR}/bin/arangod_x86_64"
+
+# create database directory
+mkdir ${TMP_DIR}
+
+echo "Starting ArangoDB '${ARANGOD}'"
+
+${ARANGOD} \
+    --database.directory ${TMP_DIR} \
+    --configuration none \
+    --server.endpoint tcp://127.0.0.1:8529 \
+    --javascript.app-path ${ARANGODB_DIR}/js/apps \
+    --javascript.startup-directory ${ARANGODB_DIR}/js \
+    --database.maximal-journal-size 1048576 \
+    --server.authentication false &
+
+sleep 2
+
+echo "Check for arangod process"
+process=$(ps auxww | grep "bin/arangod" | grep -v grep)
+
+if [ "x$process" == "x" ]; then
+  echo "no 'arangod' process found"
+  echo "ARCH = $ARCH"
+  exit 1
+fi
 
 echo "Waiting until ArangoDB is ready on port 8529"
 
@@ -31,5 +62,6 @@ then
     echo "Could not start ArangoDB. Timeout reached."
     exit 1
 fi
+
 
 echo "ArangoDB is up"
