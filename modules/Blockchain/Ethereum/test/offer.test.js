e0bedee3c5532e3e1934850cba7ff0ce7d50cd07
Uros Kukic <kukic.uros@yahoo.com>
2019-03-18 14:32:37 +0100

    Improve payOut test coverage

--- modules/Blockchain/Ethereum/test/offer.test.js
+++ modules/Blockchain/Ethereum/test/offer.test.js
@@ -53,6 +53,104 @@ var holdingStorage;
 var profileStorage;
 var util;
 
+async function createOffer(accounts) {
+    await holdingStorage.setDifficultyOverride(new BN(1));
+
+    let res = await holding.createOffer(
+        DC_identity,
+        dataSetId,
+        dataRootHash,
+        redLitigationHash,
+        greenLitigationHash,
+        blueLitigationHash,
+        dcNodeId,
+        holdingTimeInMinutes,
+        tokenAmountPerHolder,
+        dataSetSizeInBytes,
+        litigationIntervalInMinutes,
+        { from: DC_wallet },
+    );
+    const firstOfferGasUsage = res.receipt.gasUsed;
+
+    // eslint-disable-next-line prefer-destructuring
+    offerId = res.logs[0].args.offerId;
+
+    const task = await holdingStorage.getOfferTask.call(offerId);
+
+    const hash1 = await util.keccakAddressBytes(identities[0], task);
+    const hash2 = await util.keccakAddressBytes(identities[1], task);
+    const hash3 = await util.keccakAddressBytes(identities[2], task);
+
+    const sortedIdentities = [
+        {
+            identity: identities[0],
+            privateKey: privateKeys[0],
+            hash: hash1,
+        },
+        {
+            identity: identities[1],
+            privateKey: privateKeys[1],
+            hash: hash2,
+        },
+        {
+            identity: identities[2],
+            privateKey: privateKeys[2],
+            hash: hash3,
+        },
+    ].sort((x, y) => x.hash.localeCompare(y.hash));
+
+    const solution = await util.keccakBytesBytesBytes.call(
+        sortedIdentities[0].hash,
+        sortedIdentities[1].hash,
+        sortedIdentities[2].hash,
+    );
+
+    for (var i = 65; i >= 2; i -= 1) {
+        if (task.charAt(task.length - 1) === solution.charAt(i)) break;
+    }
+    if (i === 2) {
+        assert(false, 'Could not find solution for offer challenge!');
+    }
+    const shift = 65 - i;
+
+    // Getting hashes
+    var hashes = [];
+    let promises = [];
+    for (i = 0; i < 3; i += 1) {
+        promises[i] = util.keccakBytesAddress.call(offerId, sortedIdentities[i].identity);
+    }
+    hashes = await Promise.all(promises);
+
+    // Getting confirmations
+    var confimations = [];
+    promises = [];
+    for (i = 0; i < 3; i += 1) {
+        promises[i] = web3.eth.accounts.sign(
+            hashes[i],
+            sortedIdentities[i].privateKey,
+        );
+    }
+    confimations = await Promise.all(promises);
+
+    res = await holding.finalizeOffer(
+        DC_identity,
+        offerId,
+        shift,
+        confimations[0].signature,
+        confimations[1].signature,
+        confimations[2].signature,
+        [new BN(0), new BN(1), new BN(2)],
+        [
+            sortedIdentities[0].identity,
+            sortedIdentities[1].identity,
+            sortedIdentities[2].identity,
+        ],
+        { from: DC_wallet },
+    );
+
+    await holdingStorage.setDifficultyOverride(new BN(0));
+}
+
 // eslint-disable-next-line no-undef
 contract('Offer testing', async (accounts) => {
     // eslint-disable-next-line no-undef
@@ -297,50 +395,180 @@ contract('Offer testing', async (accounts) => {
         console.log(`Total gas used for creating a second offer: ${secondOfferGasUsage + finalizeOfferGasUsage}`);
 
         await holdingStorage.setDifficultyOverride(new BN(0));
-
-        errored = false;
     });
 
     // eslint-disable-next-line no-undef
     it('Should test token transfers for holding', async () => {
-        // wait for holding job to expire
-        if (errored) assert(false, 'Test cannot run without previous test succeeding');
+        await createOffer(accounts);
 
-        let timestamp = await holdingStorage.getOfferStartTime.call(offerId);
-        timestamp = timestamp.sub(new BN(800));
-        await holdingStorage.setOfferStartTime(offerId, timestamp);
+        // Move the payment timestamp halfway through the offer
+        let timestamp;
+        let promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = holdingStorage.getHolderPaymentTimestamp(offerId, identities[i]);
+        }
+        let res = await Promise.all(promises);
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            timestamp = res[i];
+            timestamp = timestamp.sub(holdingTimeInMinutes.muln(60));
+            promises[i] = holdingStorage.setHolderPaymentTimestamp(
+                offerId,
+                identities[i],
+                timestamp,
+            );
+        }
+        await Promise.all(promises);
 
-        const promises = [];
         for (var i = 0; i < 3; i += 1) {
             promises[i] =
                 holdingStorage.setHolderPaymentTimestamp(offerId, identities[i], timestamp);
         }
         await Promise.all(promises);
 
-        var initialStake = [];
-        for (i = 0; i < 3; i += 1) {
-            // eslint-disable-next-line no-await-in-loop
-            var res = await profileStorage.profile.call(identities[i]);
-            initialStake[i] = res.stake;
+        const initialStake = [];
+        const initialStakeReserved = [];
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = profileStorage.profile.call(identities[i]);
+        }
+        res = await Promise.all(promises);
+        for (let i = 0; i < 3; i += 1) {
+            initialStake[i] = res[i].stake;
+            initialStakeReserved[i] = res[i].stakeReserved;
         }
         res = await profileStorage.profile.call(DC_identity);
         const initialStakeDC = res.stake;
+        const initialStakeReservedDC = res.stakeReserved;
 
         for (i = 0; i < 3; i += 1) {
             // eslint-disable-next-line no-await-in-loop
             await holding.payOut(identities[i], offerId, { from: accounts[i] });
         }
 
-        for (i = 0; i < 3; i += 1) {
-            // eslint-disable-next-line no-await-in-loop
-            res = await profileStorage.profile.call(identities[i]);
-            assert(initialStake[i].add(tokenAmountPerHolder).eq(res.stake), `Stake amount incorrect for account ${i}! `
-                + `Expected ${initialStake[i].add(tokenAmountPerHolder).toString()}, but got ${res.stake.toString()}!`);
-            assert((new BN(0)).eq(res.stakeReserved), `Stake amout incorrect for account ${i}`);
+        const finalStake = [];
+        const finalStakeReserved = [];
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = profileStorage.profile.call(identities[i]);
+        }
+        res = await Promise.all(promises);
+        for (let i = 0; i < 3; i += 1) {
+            finalStake[i] = res[i].stake;
+            finalStakeReserved[i] = res[i].stakeReserved;
         }
         res = await profileStorage.profile.call(DC_identity);
-        assert(initialStakeDC.sub(tokenAmountPerHolder.mul(new BN(3))).eq(res.stake), 'Stake amount incorrect for DC');
-        assert((new BN(0)).eq(res.stakeReserved), 'Reserved stake amount incorrect for DC');
+        const finalStakeDC = res.stake;
+        const finalStakeReservedDC = res.stakeReserved;
+
+        for (let i = 0; i < 3; i += 1) {
+            assert(initialStake[i].add(tokenAmountPerHolder).eq(finalStake[i]), `Stake amount incorrect for account ${i}! `
+                + `Expected ${initialStake[i].add(tokenAmountPerHolder).toString()}, but got ${finalStake[i].toString()}!`);
+            assert(initialStakeReserved[i].sub(tokenAmountPerHolder).eq(finalStakeReserved[i]), `Reserved stake amount incorrect for account ${i}! `
+                + `Expected ${initialStakeReserved[i].sub(tokenAmountPerHolder).toString()}, but got ${finalStakeReserved[i].toString()}!`);
+        }
+        assert(initialStakeDC.sub(tokenAmountPerHolder.muln(3)).eq(finalStakeDC), 'Stake amount incorrect for DC account! '
+            + `Expected ${initialStakeDC.sub(tokenAmountPerHolder.muln(3)).toString()}, but got ${finalStakeDC.toString()}!`);
+        assert(initialStakeReservedDC.sub(tokenAmountPerHolder.muln(3)).eq(finalStakeReservedDC), 'Reserved stake amount incorrect for DC account! '
+            + `Expected ${initialStakeReservedDC.sub(tokenAmountPerHolder.muln(3)).toString()}, but got ${finalStakeReservedDC.toString()}!`);
+    });
+
+    // eslint-disable-next-line no-undef
+    it('Should test multiple calls of payOut function', async () => {
+        // Create a new job for a holder
+
+        await createOffer(accounts);
+
+        // Move the payment timestamp halfway through the offer
+        let timestamp;
+        let promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = holdingStorage.getHolderPaymentTimestamp(offerId, identities[i]);
+        }
+        let res = await Promise.all(promises);
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            timestamp = res[i];
+            timestamp = timestamp.sub(holdingTimeInMinutes.muln(60).divn(2));
+            promises[i] = holdingStorage.setHolderPaymentTimestamp(
+                offerId,
+                identities[i],
+                timestamp,
+            );
+        }
+        await Promise.all(promises);
+
+        const initialStake = [];
+        const initialStakeReserved = [];
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = profileStorage.profile.call(identities[i]);
+        }
+        res = await Promise.all(promises);
+        for (let i = 0; i < 3; i += 1) {
+            initialStake[i] = res[i].stake;
+            initialStakeReserved[i] = res[i].stakeReserved;
+        }
+        res = await profileStorage.profile.call(DC_identity);
+        const initialStakeDC = res.stake;
+        const initialStakeReservedDC = res.stakeReserved;
+
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = holding.payOut(identities[i], offerId, { from: accounts[i] });
+        }
+        await Promise.all(promises);
+
+        // Move the payment timestamp even further - some time after the offer has finished
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = holdingStorage.getHolderPaymentTimestamp(offerId, identities[i]);
+        }
+        res = await Promise.all(promises);
+        promises = [];
+        for (let i = 0; i < res.length; i += 1) {
+            timestamp = res[i];
+            timestamp = timestamp.sub(holdingTimeInMinutes.muln(60));
+            promises[i] = holdingStorage.setHolderPaymentTimestamp(
+                offerId,
+                identities[i],
+                timestamp,
+            );
+        }
+        await Promise.all(promises);
+
+        // Call payout again
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = holding.payOut(identities[i], offerId, { from: accounts[i] });
+        }
+        await Promise.all(promises);
+
+        const finalStake = [];
+        const finalStakeReserved = [];
+        promises = [];
+        for (let i = 0; i < 3; i += 1) {
+            promises[i] = profileStorage.profile.call(identities[i]);
+        }
+        res = await Promise.all(promises);
+        for (let i = 0; i < 3; i += 1) {
+            finalStake[i] = res[i].stake;
+            finalStakeReserved[i] = res[i].stakeReserved;
+        }
+        res = await profileStorage.profile.call(DC_identity);
+        const finalStakeDC = res.stake;
+        const finalStakeReservedDC = res.stakeReserved;
+
+        for (let i = 0; i < 3; i += 1) {
+            assert(initialStake[i].add(tokenAmountPerHolder).eq(finalStake[i]), `Stake amount incorrect for account ${i}! `
+                + `Expected ${initialStake[i].add(tokenAmountPerHolder).toString()}, but got ${finalStake[i].toString()}!`);
+            assert(initialStakeReserved[i].sub(tokenAmountPerHolder).eq(finalStakeReserved[i]), `Reserved stake amount incorrect for account ${i}! `
+                + `Expected ${initialStakeReserved[i].sub(tokenAmountPerHolder).toString()}, but got ${finalStakeReserved[i].toString()}!`);
+        }
+        assert(initialStakeDC.sub(tokenAmountPerHolder.muln(3)).eq(finalStakeDC), 'Stake amount incorrect for DC account! '
+            + `Expected ${initialStakeDC.sub(tokenAmountPerHolder.muln(3)).toString()}, but got ${finalStakeDC.toString()}!`);
+        assert(initialStakeReservedDC.sub(tokenAmountPerHolder.muln(3)).eq(finalStakeReservedDC), 'Reserved stake amount incorrect for DC account! '
+            + `Expected ${initialStakeReservedDC.sub(tokenAmountPerHolder.muln(3)).toString()}, but got ${finalStakeReservedDC.toString()}!`);
     });
 
     // eslint-disable-next-line no-undef
