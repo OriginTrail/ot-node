c26b88a06a1073d06c8cfdba60a929694e9dff25
Uros Kukic <kukic.uros@yahoo.com>
2019-03-27 12:42:32 +0100

    Add CreditorHandler contract for delegated payments

---
+++ modules/Blockchain/Ethereum/contracts/CreditorHandler.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.4.25;
+
+import './Hub.sol';
+import {ERC725} from './ERC725.sol';
+import {Profile} from './Profile.sol';
+import {ProfileStorage} from './ProfileStorage.sol';
+import {HoldingStorage} from './HoldingStorage.sol';
+import {SafeMath} from './SafeMath.sol';
+
+/**
+ * The CreditorHandler contract handles funds and requirements when an offer is finalized using
+ * creditor funds
+ */
+contract CreditorHandler {
+	using SafeMath for uint256;
+
+    Hub public hub;
+	constructor(address hubAddress) public{
+		require(hubAddress!=address(0));
+        hub = Hub(hubAddress);
+	}
+
+	function setHubAddress(address newHubAddress) public{
+        require(hub.isContract(msg.sender), "This function can only be called by contracts or their creator!");
+
+        hub = Hub(newHubAddress);
+    }
+
+    modifier onlyContracts() {
+        require(hub.isContract(msg.sender),
+        "Function can only be called by contracts!");
+        _;
+    }
+
+    function finalizeOffer(uint256 offerId, address identity, address parentIdentity)
+    public onlyContracts {
+        ProfileStorage profileStorage = ProfileStorage(hub.getContractAddress("ProfileStorage"));
+
+    	require(ERC725(parentIdentity).keyHasPurpose(keccak256(abi.encodePacked(identity)), 237), "Sender identity is not a sub-identity of the provided parent identity!");
+
+        uint256 amount = HoldingStorage(hub.getContractAddress("HoldingStorage")).getOfferTokenAmountPerHolder(bytes32(offerId));
+        uint256 minimalStake = Profile(hub.getContractAddress("Profile")).minimalStake();
+
+    	if(profileStorage.getWithdrawalPending(parentIdentity) && profileStorage.getWithdrawalAmount(parentIdentity).add(amount.mul(3)) > profileStorage.getStake(parentIdentity) - profileStorage.getStakeReserved(parentIdentity)) {
+            profileStorage.setWithdrawalPending(parentIdentity,false);
+        }
+
+    	require(minimalStake <= profileStorage.getStake(parentIdentity).sub(profileStorage.getStakeReserved(parentIdentity)),
+            "Parent identity does not have enough stake to create new jobs!");
+
+    	// Transferring funds
+    	require(profileStorage.getStake(parentIdentity).sub(profileStorage.getStakeReserved(parentIdentity)) >= amount.mul(3),
+            "Parent identity does not have enough stake for reserving!");
+
+    	uint256 currentStake = profileStorage.getStake(parentIdentity);
+        profileStorage.setStake(parentIdentity, currentStake.sub(amount.mul(3)));
+        currentStake = profileStorage.getStake(identity);
+        profileStorage.setStake(identity, currentStake.add(amount.mul(3)));
+    }
+    
+        
+
+}
