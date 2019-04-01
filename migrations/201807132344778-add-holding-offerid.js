a98900d914844f81e0b697fa5371a6eb1d847b66
Janko Simonovic <simonovic86@gmail.com>
2019-04-01 14:52:02 +0200

    change table name

--- migrations/201807132344778-add-holding-offerid.js
+++ migrations/201807132344778-add-holding-offerid.js
@@ -2,11 +2,11 @@
 module.exports = {
     up: async (queryInterface, Sequelize) =>
         queryInterface.addColumn(
-            'holding',
+            'holding_data',
             'offer_id',
             {
                 type: Sequelize.STRING,
             },
         ),
-    down: queryInterface => queryInterface.removeColumn('holding', 'offer_id'),
+    down: queryInterface => queryInterface.removeColumn('holding_data', 'offer_id'),
 };
