afeb5a35119df0d8130b5951223327176ac1f1f5
Janko Simonovic <simonovic86@gmail.com>
2019-04-04 13:43:02 +0200

    add offer_id

--- modules/RemoteControl.js
+++ modules/RemoteControl.js
@@ -374,6 +374,7 @@ class RemoteControl {
                     paid_amount: paidAmount,
                     staked_amount: stakedAmount,
                     status: bid.status,
+                    offer_id: bid.offer_id,
                 };
             },
         );
