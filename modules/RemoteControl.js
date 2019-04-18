b0e4d6db1956ca16dc5c49b0e765c618762e282e
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-04-18 14:29:01 +0200

    Prevent remote control to crash finalize command

--- modules/RemoteControl.js
+++ modules/RemoteControl.js
@@ -367,6 +367,11 @@ class RemoteControl {
             async (bid) => {
                 const holding = await this._findHoldingByBid(bid);
 
+                if (holding == null || holding.data_set_id == null) {
+                    this.log.debug('Failed to get holding data for for bid %s.', bid);
+                    return;
+                }
+
                 const dataInfo = await Models.data_info.findOne({
                     where: {
                         data_set_id: holding.data_set_id,
