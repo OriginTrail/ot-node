fc851c7c5476411e161f775c50a2023f3efdb7b1
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-05-23 14:21:07 +0200

    Fix

--- modules/transpiler/epcis/epcis-otjson-transpiler.js
+++ modules/transpiler/epcis/epcis-otjson-transpiler.js
@@ -768,11 +768,11 @@ class EpcisOtJsonTranspiler {
 
 module.exports = EpcisOtJsonTranspiler;
 
-const fs = require('fs');
-
-const xml = fs.readFileSync('./datasetA.xml').toString('UTF-8');
-const converter = new EpcisOtJsonTranspiler(null);
-const otJson = converter.convertToOTJson(xml);
-console.log(JSON.stringify(otJson));
-const xmlFromOtJson = converter.convertFromOTJson(otJson);
-console.log(xmlFromOtJson);
+// const fs = require('fs');
+//
+// const xml = fs.readFileSync('./datasetA.xml').toString('UTF-8');
+// const converter = new EpcisOtJsonTranspiler(null);
+// const otJson = converter.convertToOTJson(xml);
+// console.log(JSON.stringify(otJson));
+// const xmlFromOtJson = converter.convertFromOTJson(otJson);
+// console.log(xmlFromOtJson);
