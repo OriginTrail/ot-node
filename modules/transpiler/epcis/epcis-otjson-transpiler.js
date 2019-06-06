1b936a309e4972b79b48ec4ccb410f2d2c1c009d
Janko Simonovic <simonovic86@gmail.com>
2019-06-06 13:05:36 +0200

    add children

--- modules/transpiler/epcis/epcis-otjson-transpiler.js
+++ modules/transpiler/epcis/epcis-otjson-transpiler.js
@@ -107,7 +107,6 @@ class EpcisOtJsonTranspiler {
 
     /**
      * Remove comments from raw json
-     * @param jsonRaw
      */
     _removeCommentsAndTrimTexts(obj) {
         if (typeof obj === 'object' || Array.isArray((obj))) {
@@ -321,6 +320,20 @@ class EpcisOtJsonTranspiler {
                 otVocabulary['@id'] = vocabularyElement._attributes.id;
                 otVocabulary.properties = properties;
                 otVocabulary['@type'] = 'otObject';
+
+                if (vocabularyElement.children) {
+                    const compressedChildren = this._compressText(vocabularyElement.children);
+                    otVocabulary.properties.children = compressedChildren.id;
+                    otVocabulary.relations = [];
+                    otVocabulary.properties.children.forEach(() => otVocabulary.relations.push({
+                        '@type': 'otRelation',
+                        direction: 'direct', // think about direction
+                        linkedObject: {
+                            '@id': otVocabulary['@id'],
+                        },
+                        relationType: 'HAS_CHILD',
+                    }));
+                }
                 result.push(otVocabulary);
             }
         }
@@ -348,6 +361,13 @@ class EpcisOtJsonTranspiler {
             const vocabularyElement = metadata;
             if (elementsByType[type] == null) {
                 elementsByType[type] = [];
+            }
+
+            const { children: otChildren } = otVocabularyElement.properties;
+            if (otChildren) {
+                vocabularyElement.children = {
+                    id: this._decompressText(otChildren),
+                };
             }
             elementsByType[type].push(vocabularyElement);
         }
