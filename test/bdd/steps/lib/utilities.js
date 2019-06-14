061158e29dff8389522076057d7c07faa60276d9
Nebojsa Obradovic <nebojsa.obradovic@origin-trail.com>
2019-06-14 13:11:02 +0200

    Fix replication test

--- test/bdd/steps/lib/utilities.js
+++ test/bdd/steps/lib/utilities.js
@@ -3,10 +3,99 @@ const sortedStringify = require('sorted-json-stringify');
 const { sha3_256 } = require('js-sha3');
 const _ = require('lodash');
 const BN = require('bn.js');
+const Web3 = require('web3');
 
+// TODO: use 3rd party.
+const MerkleTree = require('../../../../modules/Merkle');
 
-function calculateImportHash(data) {
-    return `0x${sha3_256(sortedStringify(data, null, 0))}`;
+// Private functions.
+
+function _sortedStringify(obj) {
+    if (obj == null) {
+        return 'null';
+    }
+    if (typeof obj === 'object') {
+        const stringified = [];
+        for (const key of Object.keys(obj)) {
+            if (!Array.isArray(obj)) {
+                stringified.push(`"${key}":${_sortedStringify(obj[key])}`);
+            } else {
+                stringified.push(_sortedStringify(obj[key]));
+            }
+        }
+        if (!Array.isArray(obj)) {
+            stringified.sort();
+            return `{${stringified.join(',')}}`;
+        }
+        return `[${stringified.join(',')}]`;
+    }
+    return JSON.stringify(obj);
+}
+
+/**
+ *
+ * @param graph
+ * @return {string|*|undefined}
+ * @private
+ */
+function _sortGraphRecursively(graph) {
+    graph.forEach((el) => {
+        if (el.relations) {
+            el.relations.sort(
+                (r1, r2) =>
+                    sha3_256(_sortedStringify(r1)).localeCompare(sha3_256(_sortedStringify(r2))));
+        }
+
+        if (el.identifiers) {
+            el.identifiers.sort(
+                (r1, r2) =>
+                    sha3_256(_sortedStringify(r1)).localeCompare(sha3_256(_sortedStringify(r2))));
+        }
+    });
+    graph.sort((e1, e2) => e1['@id'].localeCompare(e2['@id']));
+    return _sortedStringify(graph);
+}
+
+function _sortDataset(dataset) {
+    dataset['@graph'].forEach((el) => {
+        if (el.relations) {
+            el.relations.sort((r1, r2) => sha3_256(_sortedStringify(r1))
+                .localeCompare(sha3_256(_sortedStringify(r2))));
+        }
+
+        if (el.identifiers) {
+            el.identifiers.sort((r1, r2) => sha3_256(_sortedStringify(r1))
+                .localeCompare(sha3_256(_sortedStringify(r2))));
+        }
+    });
+    dataset['@graph'].sort((e1, e2) => e1['@id'].localeCompare(e2['@id']));
+    return _sortedStringify(dataset);
+}
+
+function _generateDatasetSummary(dataset) {
+    return {
+        datasetId: dataset['@id'],
+        datasetCreator: dataset.datasetHeader.dataCreator,
+        objects: dataset['@graph'].map(vertex => ({
+            '@id': vertex['@id'],
+            identifiers: vertex.identifiers != null ? vertex.identifiers : [],
+        })),
+        numRelations: dataset['@graph']
+            .filter(vertex => vertex.relations != null)
+            .reduce((acc, value) => acc + value.relations.length, 0),
+    };
+}
+
+// Public functions.
+
+/**
+ * Calculate dataset ID from a given graph.
+ * @param graph
+ * @return {string}
+ */
+function calculateImportHash(graph) {
+    const sorted = _sortGraphRecursively(graph);
+    return `0x${sha3_256(sorted, null, 0)}`;
 }
 
 /**
@@ -65,10 +154,67 @@ function findVertexUid(verticesArray, vertex_type, sender_id, uid, data) {
      */
 function isZeroHash(hash) {
     const num = new BN(this.denormalizeHex(hash));
-
     return num.eqn(0);
 }
 
+function verifySignature(otJson, wallet) {
+    const { signature } = otJson;
+    const { accounts } = new Web3().eth;
+    const strippedOtjson = Object.assign({}, otJson);
+    delete strippedOtjson.signature;
+
+    const stringifiedOtJson = _sortDataset(strippedOtjson);
+    return (wallet.toLowerCase() === accounts.recover(stringifiedOtJson, signature.value).toLowerCase());
+}
+
+/**
+ * Calculate root-hash of OT-JSON document
+ * @return {string}
+ * @param otJson
+ */
+function calculateRootHash(otJson) {
+    if (otJson == null) {
+        throw Error('Invalid OT JSON');
+    }
+
+    const { datasetHeader } = otJson;
+    if (datasetHeader == null) {
+        throw Error('Invalid OT JSON');
+    }
+
+    const graph = otJson['@graph'];
+
+    if (!Array.isArray(graph)) {
+        throw Error('Invalid graph');
+    }
+    if (graph.filter(v => v['@id'] == null).length > 0) {
+        throw Error('Invalid graph');
+    }
+
+    const datasetSummary = _generateDatasetSummary(otJson);
+
+    graph.forEach((el) => {
+        if (el.relations) {
+            el.relations.sort((r1, r2) => sha3_256(_sortedStringify(r1))
+                .localeCompare(sha3_256(_sortedStringify(r2))));
+        }
+
+        if (el.identifiers) {
+            el.identifiers.sort((r1, r2) => sha3_256(_sortedStringify(r1))
+                .localeCompare(sha3_256(_sortedStringify(r2))));
+        }
+    });
+
+    const stringifiedGraph = [];
+    graph.forEach(obj => stringifiedGraph.push(_sortedStringify(obj)));
+
+    const merkle = new MerkleTree(
+        [_sortedStringify(datasetSummary), ...stringifiedGraph],
+        'sha3',
+    );
+
+    return merkle.getRoot();
+}
 
 module.exports = {
     calculateImportHash,
@@ -77,4 +223,6 @@ module.exports = {
     findVertexIdValue,
     findVertexUid,
     isZeroHash,
+    verifySignature,
+    calculateRootHash,
 };
