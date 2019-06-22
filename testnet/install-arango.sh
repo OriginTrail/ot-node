7b8d4e24579082c79a4ee2bde2d1c4137c515787
Vuk Djoric <djoric.vuk@gmail.com>
2019-06-22 13:12:58 +0200

    Expand parameters (#942)
    * expand parameters
    
    * temporary fix arango installation
    
    * bump version

--- testnet/install-arango.sh
+++ testnet/install-arango.sh
@@ -9,4 +9,4 @@ echo arangodb3 arangodb3/upgrade boolean true | debconf-set-selections
 echo arangodb3 arangodb3/storage_engine select mmfiles | debconf-set-selections
 echo arangodb3 arangodb3/password password root | debconf-set-selections
 echo arangodb3 arangodb3/password_again password root | debconf-set-selections
-apt-get install arangodb3=3.3.12 -y
+apt-get install arangodb3=3.3.12 -y --allow-unauthenticated
