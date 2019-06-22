bd197ff07f658214dc3a0f571a4f750685532c32
Janko Simonovic <simonovic86@gmail.com>
2019-06-22 15:41:42 +0200

    Expand parameters (#942) (#943)
    * expand parameters

--- modules/command/dh/dh-replication-import-command.js
+++ modules/command/dh/dh-replication-import-command.js
@@ -53,6 +53,10 @@ class DhReplicationImportCommand extends Command {
         const calculatedDataSetId =
             await ImportUtilities.importHash(dataSetId, decryptedVertices, edges);
 
+        if (bytes(JSON.stringify(litigationVertices)) > this.config.dh_max_data_set_size) {
+            throw new Error(`Data set size is bigger than given limit of ${this.config.dh_max_data_set_size} bytes`);
+        }
+
         if (dataSetId !== calculatedDataSetId) {
             throw new Error(`Calculated data set ID ${calculatedDataSetId} differs from DC data set ID ${dataSetId}`);
         }
