18f6173822a3dd44b77433587d6006474349f202
antic11d <dimitrije.misa@gmail.com>
2019-08-16 13:27:42 +0200

    Add _getSignerAddress function

--- modules/validator/schema-validator.js
+++ modules/validator/schema-validator.js
@@ -1,9 +1,11 @@
 /**
  * Methods used for various data schema validations
  */
-
+const ethutils = require('ethereumjs-util');
 const importUtilities = require('../ImportUtilities');
-
+const Utilities = require('../Utilities');
+const web3 = require('web3');
+// const Buffer = require('buffer');
 /**
  * Constants used to select various schemas.
  * @type {{
@@ -20,42 +22,49 @@ const importUtilities = require('../ImportUtilities');
 
 
 class SchemaValidator {
-    static supportedSchemas = [
-        {
-            '/schemas/erc725-main': SchemaValidator._validateERC725Schema,
-        },
-    ];
+    constructor() {
+        this.supportedSchemas = { '/schemas/erc725-main': SchemaValidator._validateERC725Schema };
+    }
 
     /**
      * Validate a particular schema
      * @param document OT-JSON dataset
      * @param schemaName the name of the schema to be validated
      */
-    static validateSchema(document, schemaName) {
+    validateSchema(document, schemaName) {
         return this.supportedSchemas[schemaName](document);
     }
 
-    static _getSignerAddress(document) {
-
+    _getSignerAddress(document) {
         const merkleRoot = importUtilities.calculateDatasetRootHash(document['@graph'], document['@id'], document.datasetHeader.dataCreator);
+        const { signature } = document;
 
-        const signature = document.signature;
+        const { value } = signature;
+        const normValue = Utilities.denormalizeHex(value);
 
-        // TODO Ecrecovery na node-u;
+        const p = {};
+        p.r = `0x${normValue.slice(0, 64)}`;
+        p.s = `0x${normValue.slice(64, 128)}`;
+        const v = `0x${normValue.slice(128, 130)}`;
+        p.v_decimal = web3.utils.hexToNumber(v);
+
+        const key = ethutils.ecrecover(Buffer.from(Utilities.denormalizeHex(merkleRoot), 'hex'), p.v_decimal, p.r, p.s);
+
+        const addr = key.toString('hex').slice(key.length - 40, key.length);
+
+        return Utilities.normalizeHex(addr);
     }
 
-    static _validateERC725Schema(document) {
-
-        const signer = _getSignerAddress(document);
-
+    _validateERC725Schema(document) {
+        const signer = this._getSignerAddress(document);
 
 
-        otjson.datasetHeader.dataIntegrity.proofs[0].proofValue = merkleRoot;
+        // otjson.datasetHeader.dataIntegrity.proofs[0].proofValue = merkleRoot;
 
-        const signedOtjson = importUtilities.signDataset(otjson, this.config, this.web3);
+        // const signedOtjson = importUtilities.signDataset(otjson, this.config, this.web3);
 
-        return signedOtjson;
-        return null;
+        // return signedOtjson;
+        // return null;
     }
 }
 
