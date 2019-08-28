3acb0801052128ea0b7a5397efc071678842770a
akinovak <akinovak@gmail.com>
2019-08-28 10:21:26 +0200

    created table for import_handles

---
+++ models/import_handles.js
@@ -0,0 +1,18 @@
+const uuidv4 = require('uuid/v4');
+
+module.exports = (sequelize, DataTypes) => {
+    const import_handles = sequelize.define('import_handles', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: () => uuidv4(),
+        },
+        import_handle_id: DataTypes.STRING,
+        data: DataTypes.JSON,
+        status: DataTypes.STRING,
+    }, {});
+    import_handles.associate = (models) => {
+        // associations can be defined here
+    };
+    return import_handles;
+};
