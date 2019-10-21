283a43705d570b835d5db0516a2b287f3f845b3f
Milos Kotlar <milos.kotlar@origin-trail.com>
2019-10-21 14:31:20 +0200

    docker fix

--- test/docker/check_image.sh
+++ test/docker/check_image.sh
@@ -15,7 +15,7 @@ sleep 180
 # docker container inspect mynode
 # docker network inspect host
 docker exec mynode /bin/sh -c "ls"
-docker exec mynode /bin/sh -c "curl -X POST http://127.0.0.1:8900/api/latest/import -F standard_id=GS1-EPCIS -F file=@/importers/xml_examples/Retail/03_Pink_to_orange_shipment.xml" > $TRAVIS_BUILD_DIR/importResult.json
+docker exec mynode /bin/sh -c "curl -X POST http://127.0.0.1:8900/api/latest/import -F standard_id=GS1-EPCIS -F file=/importers/xml_examples/Retail/03_Pink_to_orange_shipment.xml" > $TRAVIS_BUILD_DIR/importResult.json
 cat $TRAVIS_BUILD_DIR/importResult.json
 # TODO better asserts that import response has one key, handler_id
 grep -q 'handler_id' $TRAVIS_BUILD_DIR/importResult.json
