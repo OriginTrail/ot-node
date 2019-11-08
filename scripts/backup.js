ebb779ae83c36c74a823dd6969d2cbbe5ac25b20
akinovak <akinovak@gmail.com>
2019-11-08 10:43:20 +0100

    both backup of folder and file are handled

--- scripts/backup.js
+++ scripts/backup.js
@@ -6,46 +6,79 @@ const path = require('path');
 const backupPath = '../Backup';
 const configPath = '../config/config.json';
 
-async function checkConfigInfo() {
-    const configInfo = await Models.node_data.find({ where: { key: 'node-config' } });
+const keynameMap = new Map();
+keynameMap.set('kademlia-identity', ['../../config/DCG/identity-test.json', '../../config/DCG/kademlia-test.crt', '../../config/DCG/kademlia-test.key']);
+keynameMap.set('node-config', ['../config/config-test.json']);
+
+function extractNameFromPath(path) {
+    const n = path.lastIndexOf('/');
+    return path.slice(n + 1);
+}
+
+async function checkFileInfo(filename) {
+    const configInfo = await Models.node_data.find({ where: { key: filename } });
     return configInfo;
 }
 
-async function updateConfigInfo(timestamp) {
+async function updateFileInfo(timestamp, filename) {
     await Models.node_data.update(
         {
             value: timestamp,
         },
         {
             where: {
-                key: 'node-config',
+                key: filename,
             },
         },
     );
 }
 
-function createNewBackup(timestamp) {
-    fs.mkdir(`${backupPath}/${timestamp}`, (err) => {
+function createNewBackup(timestamp, keyname) {
+    fs.mkdirSync(`${backupPath}/${keyname}/${timestamp}`, (err) => {
         if (err) throw err;
     });
-
-    fs.copyFile(configPath, `${backupPath}/${timestamp}/config.json`, (err) => {
-        if (err) throw err;
-    });
+    for (const path of keynameMap.get(keyname)) {
+        const filename = extractNameFromPath(path);
+        fs.copyFile(path, `${backupPath}/${keyname}/${timestamp}/${filename}`, (err) => {
+            if (err) throw err;
+        });
+    }
 }
 
-async function handleModification(configInfo) {
-    const stat = fs.statSync(configPath);
+async function checkForModification(keyname) {
+    let max_timestamp = -1;
+    for (const path of keynameMap.get(keyname)) {
+        const filename = extractNameFromPath(path);
+        const stat = fs.statSync(path);
+        // eslint-disable-next-line no-await-in-loop
+        const configInfo = await checkFileInfo(filename);
+        // console.log(filename);
 
-    const modificationTime = new Date(stat.mtime).getTime();
-    const previousModificationTime = configInfo.value;
+        const modificationTime = new Date(stat.mtime).getTime();
+        const previousModificationTime = configInfo.value;
 
-    if (modificationTime > previousModificationTime) {
-        await updateConfigInfo(stat.mtime.getTime().toString());
-        createNewBackup(stat.mtime.toString());
-        console.log('Modification have occurred');
+        if (modificationTime > previousModificationTime) {
+            if (modificationTime > max_timestamp) {
+                max_timestamp = modificationTime;
+            }
+        }
+    }
+    return max_timestamp;
+}
+
+async function handleModification(keyname) {
+    const timestamp = await checkForModification(keyname);
+    console.log(timestamp);
+    if (timestamp > -1) {
+        for (const path of keynameMap.get(keyname)) {
+            const filename = extractNameFromPath(path);
+            // eslint-disable-next-line no-await-in-loop
+            await updateFileInfo(timestamp, filename);
+        }
+        createNewBackup(timestamp, keyname);
+        console.log(`Modification have occurred in ${keyname}`);
     } else {
-        console.log('Ther was no modification');
+        console.log(`There was no modification in ${keyname}`);
     }
 }
 
@@ -53,8 +86,11 @@ async function main() {
     if (argv.configDir) {
         Models.sequelize.options.storage = path.join(argv.configDir, 'system.db');
     }
-    const configInfo = await checkConfigInfo();
-    await handleModification(configInfo);
+    const promises = [];
+    keynameMap.forEach((value, key) => {
+        promises.push(handleModification(key));
+    });
+    await Promise.all(promises);
 }
 
 main();
