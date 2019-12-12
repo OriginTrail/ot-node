f3ee063f9fb08cd20bbd2df7f241f8b59f1efa79
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2019-12-12 09:15:18 +0100

    Added sudo when starting upgrade-arango script

--- ot-node.js
+++ ot-node.js
@@ -319,7 +319,7 @@ class OTNode {
                     && semver.lt(version, '3.5.0')
                     && config.autoUpdater.enabled) {
                     log.info('Your arangodb version is lower than required. Starting upgrade...');
-                    execSync(`./upgrade-arango.sh ${config.database.password} ${config.database.host} ${config.database.port}`, { stdio: 'inherit' });
+                    execSync(`sudo ./upgrade-arango.sh ${config.database.password} ${config.database.host} ${config.database.port}`, { stdio: 'inherit' });
 
                     const { version } = await Utilities.getArangoDbVersion(config);
                     log.info(`Arango server is updated to version ${version}.`);
