6b9f23e8ae7231639a916fd3c64d6f2f7cea230c
Uros Kukic <33048701+Kuki145@users.noreply.github.com>
2019-12-20 15:23:19 +0100

    Add smart contract migration script (#1058)

--- modules/Blockchain/Ethereum/migrations/2_total_migration.js
+++ modules/Blockchain/Ethereum/migrations/2_total_migration.js
@@ -3,6 +3,7 @@ var BN = require('bn.js');
 var TracToken = artifacts.require('TracToken'); // eslint-disable-line no-undef
 
 var Hub = artifacts.require('Hub'); // eslint-disable-line no-undef
+var OldHub = artifacts.require('OldHub'); // eslint-disable-line no-undef
 var Profile = artifacts.require('Profile'); // eslint-disable-line no-undef
 var Holding = artifacts.require('Holding'); // eslint-disable-line no-undef
 var CreditorHandler = artifacts.require('CreditorHandler'); // eslint-disable-line no-undef
@@ -24,6 +25,7 @@ const amountToMint = (new BN(5)).mul((new BN(10)).pow(new BN(30)));
 
 module.exports = async (deployer, network, accounts) => {
     let hub;
+    let oldHub;
     let token;
 
     let profile;
@@ -245,31 +247,76 @@ module.exports = async (deployer, network, accounts) => {
         console.log(`\t Token contract address: \t${token.address}`);
         console.log(`\t Escrow contract address: \t${holding.address}`);
         break;
-    case 'update':
-        hub = await Hub.at('0x54985ef4EF2d3d04df7B026DA98d9f356b418626');
+    case 'updateRinkeby':
+        await deployer.deploy(Hub)
+            .then((result) => {
+                hub = result;
+            });
+        await hub.setContractAddress('Owner', accounts[0]);
 
-        // token = await deployer.deploy(TracToken, accounts[0], accounts[1], accounts[2]);
-        // await hub.setContractAddress('Token', token.address);
+        oldHub = await OldHub.at('');
 
-        profile = await deployer.deploy(Profile, hub.address, { gas: 6500000, from: accounts[0] });
+        temp = await oldHub.profileStorageAddress.call();
+        profileStorage = await ProfileStorage.at(temp);
+        await hub.setContractAddress('ProfileStorage', profileStorage.address);
+
+        holdingStorage = await deployer.deploy(
+            HoldingStorage,
+            hub.address,
+        );
+        await hub.setContractAddress('HoldingStorage', holdingStorage.address);
+
+        litigationStorage = await deployer.deploy(
+            LitigationStorage,
+            hub.address,
+        );
+        await hub.setContractAddress('LitigationStorage', litigationStorage.address);
+
+        approval = await deployer.deploy(Approval);
+        await hub.setContractAddress('Approval', approval.address);
+
+        await hub.setContractAddress('Token', '0x98d9a611ad1b5761bdc1daac42c48e4d54cf5882');
+
+        profile = await deployer.deploy(Profile, hub.address);
         await hub.setContractAddress('Profile', profile.address);
 
-        holding = await deployer.deploy(Holding, hub.address, { gas: 6000000, from: accounts[0] });
+        holding = await deployer.deploy(Holding, hub.address);
         await hub.setContractAddress('Holding', holding.address);
 
-        // for (let i = 0; i < 10; i += 1) {
-        //     amounts.push(amountToMint);
-        //     recepients.push(accounts[i]);
-        // }
-        // await token.mintMany(recepients, amounts, { from: accounts[0] });
-        // await token.finishMinting({ from: accounts[0] });
+        litigation = await deployer.deploy(
+            Litigation,
+            hub.address,
+        );
+        await hub.setContractAddress('Litigation', litigation.address);
 
-        console.log('\n\n \t Contract adressess on ganache:');
+        replacement = await deployer.deploy(
+            Replacement,
+            hub.address,
+        );
+        await hub.setContractAddress('Replacement', replacement.address);
+
+        temp = await oldHub.profileAddress.call();
+        await hub.setContractAddress('OldProfile', temp);
+        temp2 = await oldHub.holdingAddress.call();
+        await hub.setContractAddress('OldHolding', temp2);
+
+        await profileStorage.setHubAddress(hub.address);
+
+        console.log('\n\n \t Contract adressess on rinkeby:');
         console.log(`\t Hub contract address: \t\t\t${hub.address}`);
-        // console.log(`\t Approval contract address: \t\t${approval.address}`);
-        // console.log(`\t Token contract address: \t\t${token.address}`);
+        console.log(`\t Approval contract address: \t\t${approval.address}`);
         console.log(`\t Profile contract address: \t\t${profile.address}`);
         console.log(`\t Holding contract address: \t\t${holding.address}`);
+        console.log(`\t Litigation contract address: \t\t${litigation.address}`);
+        console.log(`\t Replacement contract address: \t\t${replacement.address}`);
+
+        console.log(`\t ProfileStorage contract address: \t${profileStorage.address}`);
+        console.log(`\t HoldingStorage contract address: \t${holdingStorage.address}`);
+        console.log(`\t LitigationStorage contract address: \t${litigationStorage.address}`);
+
+
+        console.log(`\t OldProfile contract address: \t${temp}`);
+        console.log(`\t OldHolding contract address: \t${temp2}`);
         break;
     case 'rinkeby':
         await deployer.deploy(Hub)
