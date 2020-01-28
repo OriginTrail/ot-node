b1495adefbade74bcb32463ba3563071ae0bd20c
Milos Kotlar <kotlarmilos@gmail.com>
2020-01-28 14:51:12 +0100

    increase kademlia parallel network calls

--- modules/network/kademlia/kademlia.js
+++ modules/network/kademlia/kademlia.js
@@ -39,6 +39,7 @@ class Kademlia {
         kadence.constants.T_RESPONSETIMEOUT = this.config.request_timeout;
         kadence.constants.SOLUTION_DIFFICULTY = this.config.network.solutionDifficulty;
         kadence.constants.IDENTITY_DIFFICULTY = this.config.network.identityDifficulty;
+        kadence.constants.ALPHA = kadence.constants.ALPHA * 2;
         this.log.info(`Network solution difficulty ${kadence.constants.SOLUTION_DIFFICULTY}.`);
         this.log.info(`Network identity difficulty ${kadence.constants.IDENTITY_DIFFICULTY}.`);
     }
