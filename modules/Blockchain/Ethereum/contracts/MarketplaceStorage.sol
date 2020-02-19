3df8691bda7f20c924625655f3a28f2d7f8294f5
Milos Kotlar <kotlarmilos@gmail.com>
2020-02-19 13:55:20 +0100

    marketplace storage contract update

--- modules/Blockchain/Ethereum/contracts/MarketplaceStorage.sol
+++ modules/Blockchain/Ethereum/contracts/MarketplaceStorage.sol
@@ -24,8 +24,8 @@ contract MarketplaceStorage {
     struct PurchaseDefinition{
         address seller;
         address buyer;
-        uint price;
         uint keyCommit;
+        uint price;
         uint key;
         bytes32 root;
 
@@ -37,11 +37,56 @@ contract MarketplaceStorage {
 //        uint256 withdrawalAmount;
 //        bytes32 nodeId;
     }
-    mapping(bytes32 => SaleDefinition) public purchase;
+    mapping(bytes32 => PurchaseDefinition) public purchase;
 
 
     function setSeller (bytes32 purchaseId, address sellerAddress)
     public onlyContracts {
         purchase[purchaseId].seller = sellerAddress;
+    }
+
+    function getSeller (bytes32 purchaseId)
+    public view returns(address seller){
+        return purchase[purchaseId].seller;
+    }
+
+    function setBuyer (bytes32 purchaseId, address buyerAddress)
+    public onlyContracts {
+        purchase[purchaseId].buyer = buyerAddress;
+    }
+
+    function getBuyer (bytes32 purchaseId)
+    public view returns(address buyer){
+        return purchase[purchaseId].buyer;
+    }
+
+    function setKeyCommit (bytes32 purchaseId, uint keyCommit)
+    public onlyContracts {
+        purchase[purchaseId].keyCommit = keyCommit;
+    }
+
+    function getKeyCommit (bytes32 purchaseId)
+    public view returns(uint keyCommit){
+        return purchase[purchaseId].keyCommit;
+    }
+
+    function setPrice (bytes32 purchaseId, uint price)
+    public onlyContracts {
+        purchase[purchaseId].price = price;
+    }
+
+    function getPrice (bytes32 purchaseId)
+    public view returns(uint price){
+        return purchase[purchaseId].price;
+    }
+
+    function setKey (bytes32 purchaseId, uint key)
+    public onlyContracts {
+        purchase[purchaseId].key = key;
+    }
+
+    function getKey (bytes32 purchaseId)
+    public view returns(uint key){
+        return purchase[purchaseId].key;
     }
 }
