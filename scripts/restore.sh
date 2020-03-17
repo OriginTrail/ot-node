44c170ae7e499ff801374f152e88261b8fb8e72e
Uros Kukic <kukic.uros@yahoo.com>
2020-03-17 09:42:25 +0100

    Update script for restoring a node

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -96,48 +96,89 @@ fi
 
 configFiles=(erc725_identity.json houston.txt identity.json kademlia.crt kademlia.key system.db)
 
+temp_folder=temp_ot_node_files_8092
+mkdir $temp_folder
+
 for file in ${configFiles[@]}; do
   sourcePath="${BACKUPDIR}/${file}"
   destinationPath="otnode:${CONFIGDIR}/"
 
+  echo "cp ${sourcePath} ${temp_folder}"
+  cp ${sourcePath} ${temp_folder}/
+
+  sourcePath=./${temp_folder}/${file}
   echo "docker cp ${sourcePath} ${destinationPath}"
+  docker cp ${sourcePath} ${destinationPath}
 done
+
 
 sourcePath="${BACKUPDIR}/.origintrail_noderc"
 destinationPath="otnode:/ot-node/current/"
+
+echo "cp ${sourcePath} ${temp_folder}"
+cp ${sourcePath} ${temp_folder}/
+sourcePath=./${temp_folder}/.origintrail_noderc
+
 echo "docker cp ${sourcePath} ${destinationPath}"
+docker cp ${sourcePath} ${destinationPath}
 
 certFiles=(fullchain.pub privkey.pem)
 
-if [ -e fullchain.pub ] && [ -e privkey.pem ]
+if [ -e "${BACKUPDIR}/fullchain.pem" ] && [ -e "${BACKUPDIR}/privkey.pem" ]
 then
-	echo "mkdir ${BACKUPDIR}/certs"
-	echo "docker cp ${BACKUPDIR}/certs otnode:/ot-node/"
+	echo "mkdir ${temp_folder}/certs"
+	mkdir ${temp_folder}/certs
+
+	echo "cp ${BACKUPDIR}/fullchain.pem ./${temp_folder}/certs/"
+	cp ${BACKUPDIR}/fullchain.pem ./${temp_folder}/certs
+
+	echo "cp ${BACKUPDIR}/privkey.pem ./${temp_folder}/certs/"
+	cp ${BACKUPDIR}/privkey.pem ./${temp_folder}/certs
+	
+	echo "docker cp ${temp_folder}/certs otnode:/ot-node/"
+	docker cp ${temp_folder}/certs otnode:/ot-node/
+else
+	echo "Cert files do not exits, skipping..."
 fi
 
+echo docker cp otnode:/ot-node/current/config/config.json ./
 docker cp otnode:/ot-node/current/config/config.json ./
 # cp ~/ot-node/config/config.json ./
 
 databaseName=($(cat config.json | grep "\"database\": \"" | sed -r "s_([[:blank:]]+)\"database\":[[:blank:]]\"__" | sed "s/\",$//"))
 databaseName=${databaseName[@]:${environmentIndex}:1}
-
 echo "database name ${databaseName}"
 
 databaseUsername=($(cat config.json | grep "\"username\": \"" | sed -r "s_([[:blank:]]+)\"username\":[[:blank:]]\"__" | sed "s/\",$//"))
 databaseUsername=${databaseUsername[@]:${environmentIndex}:1}
-
 echo "database username ${databaseUsername}"
 
 databasePassword=($(cat config.json | grep "\"password\": \"" | sed -r "s_([[:blank:]]+)\"password\":[[:blank:]]\"__" | sed "s/\",$//"))
 databasePassword=${databasePassword[@]:${environmentIndex}:1}
-
 echo "database password ${databasePassword}"
 
 rm config.json
 
-echo "docker cp ${BACKUPDIR}/arangodb otnode:${CONFIGDIR}/"
+echo "cp -r ${BACKUPDIR}/arangodb ${temp_folder}/"
+cp -r ${BACKUPDIR}/arangodb ${temp_folder}/
+
+echo "docker cp ${temp_folder}/arangodb otnode:${CONFIGDIR}/"
+docker cp "${temp_folder}/arangodb" otnode:${CONFIGDIR}/
+
+
+echo rm -rf ${temp_folder}
+rm -rf ${temp_folder}
+
 echo docker start otnode
+docker start otnode
+
+echo sleep 5
 sleep 5
+
 echo "docker exec otnode arangorestore --server.database ${databaseName} --server.username ${databaseUsername} --server.password ${databasePassword} --input-directory ${CONFIGDIR}/arangodb/ --overwrite true"
+docker exec otnode arangorestore --server.database ${databaseName} --server.username ${databaseUsername} --server.password ${databasePassword} --input-directory ${CONFIGDIR}/arangodb/ --overwrite true
+
 echo docker restart otnode
+docker restart otnode
+
 
