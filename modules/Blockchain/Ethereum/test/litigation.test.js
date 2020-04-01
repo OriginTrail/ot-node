be93d155e268c699d97d28f4234fe6d136885898
Uros Kukic <kukic.uros@yahoo.com>
2020-04-01 15:33:23 +0200

    Fix truffle tests

--- modules/Blockchain/Ethereum/test/litigation.test.js
+++ modules/Blockchain/Ethereum/test/litigation.test.js
@@ -77,6 +77,114 @@ var util;
 // eslint-disable-next-line no-undef
 contract('Litigation testing', async (accounts) => {
     // eslint-disable-next-line no-undef
+    before(async () => {
+        // Get contracts used in hook
+        trac = await TracToken.deployed();
+        profile = await Profile.deployed();
+        holding = await Holding.deployed();
+        litigation = await Litigation.deployed();
+        replacement = await Replacement.deployed();
+        holdingStorage = await HoldingStorage.deployed();
+        profileStorage = await ProfileStorage.deployed();
+        litigationStorage = await LitigationStorage.deployed();
+        util = await TestingUtilities.deployed();
+
+        privateKeys = [
+            '0x02b39cac1532bef9dba3e36ec32d3de1e9a88f1dda597d3ac6e2130aed9adc4e',
+            '0xb1c53fd90d0172ff60f14f61f7a09555a9b18aa3c371991d77209cfe524e71e6',
+            '0x8ab3477bf3a1e0af66ab468fafd6cf982df99a59fee405d99861e7faf4db1f7b',
+            '0xc80796c049af64d07c76ab4cfb00655895368c60e50499e56cdc3c38d09aa88e',
+            '0x239d785cea7e22f23d1fa0f22a7cb46c04d81498ce4f2de07a9d2a7ceee45004',
+            '0x021336479aa1553e42bfcd3b928dee791db84a227906cb7cec5982d382ecf106',
+            '0x217479bee25ed6d28302caec069c7297d0c3aefdda81cf91ed754c4d660862ae',
+            '0xa050f7b3a0479a55e9ddd074d218fbfea302f061e9f21a117a2ec1f0b986a363',
+            '0x0dbaee2066aacd16d43a9e23649f232913bca244369463320610ffe6ffb0d69d',
+            '0x63b854ff0d973dbd4808a6def4c6a7f65bebcaec07520fbf1c0056331af65a7b',
+        ];
+
+
+        // Generate web3 and set provider
+        web3 = new Web3('HTTP://127.0.0.1:7545');
+        web3.setProvider(Ganache.provider());
+
+        // Generate eth_account, identities, and profiles
+
+        // Increase approval for depositing tokens
+        let promises = [];
+        for (let i = 0; i < accounts.length; i += 1) {
+            promises[i] = trac.increaseApproval(
+                profile.address,
+                tokensToDeposit,
+                { from: accounts[i] },
+            );
+        }
+        await Promise.all(promises);
+
+        let res;
+        // Generate profiles
+        for (let i = 0; i < accounts.length; i += 1) {
+            // eslint-disable-next-line no-await-in-loop
+            res = await profile.createProfile(
+                accounts[i],
+                accounts[i],
+                tokensToDeposit,
+                false,
+                '0x7e9f99b7971cb3de779690a82fec5e2ceec74dd0',
+                { from: accounts[i] },
+            );
+            identities[i] = res.logs[0].args.newIdentity;
+        }
+
+        DC_wallet = accounts[accounts.length - 1];
+        DC_identity = identities[identities.length - 1];
+
+        // eslint-disable-next-line prefer-destructuring
+        DH_wallet = accounts[0];
+        // eslint-disable-next-line prefer-destructuring
+        DH_identity = identities[0];
+
+        // Calculate litigation root hash
+        // Merkle tree structure
+        /*      / \
+               /   \
+              /     \
+             /       \
+            /         \
+           / \       / \
+          /   \     /   \
+         /\   /\   /\   /\
+        A  B C  D E  F G  H  */
+
+        // Calculating hashes of requested data
+        promises = [];
+        for (let i = 0; i < 8; i += 1) {
+            promises[i] = util.keccakString.call(letters[i]);
+        }
+        requested_data = await Promise.all(promises);
+
+        // Calculating indexed hashes of requested data
+        promises = [];
+        for (let i = 0; i < 8; i += 1) {
+            promises[i] = util.keccakIndex.call(requested_data[i], i);
+        }
+        hashes = await Promise.all(promises);
+
+        // Creating merkle tree
+        hash_AB = await util.keccak2hashes.call(hashes[0], hashes[1]);
+        hash_CD = await util.keccak2hashes.call(hashes[2], hashes[3]);
+        hash_EF = await util.keccak2hashes.call(hashes[4], hashes[5]);
+        hash_GH = await util.keccak2hashes.call(hashes[6], hashes[7]);
+        hash_ABCD = await util.keccak2hashes.call(hash_AB, hash_CD);
+        hash_EFGH = await util.keccak2hashes.call(hash_EF, hash_GH);
+        root_hash = await util.keccak2hashes.call(hash_ABCD, hash_EFGH);
+
+        dataRootHash = root_hash;
+        redLitigationHash = root_hash;
+        greenLitigationHash = root_hash;
+        blueLitigationHash = root_hash;
+    });
+
+    // eslint-disable-next-line no-undef
     beforeEach(async () => {
         await holdingStorage.setDifficultyOverride(new BN(1));
         // Creating offer used for litigation
@@ -173,102 +281,6 @@ contract('Litigation testing', async (accounts) => {
         );
 
         await holdingStorage.setDifficultyOverride(new BN(0));
-    });
-
-    // eslint-disable-next-line no-undef
-    beforeEach(async () => {
-        // Creating offer used for litigation
-        const res = await holding.createOffer(
-            DC_identity,
-            dataSetId,
-            dataRootHash,
-            redLitigationHash,
-            greenLitigationHash,
-            blueLitigationHash,
-            dcNodeId,
-            holdingTimeInMinutes,
-            tokenAmountPerHolder,
-            dataSetSizeInBytes,
-            litigationIntervalInMinutes,
-            { from: DC_wallet },
-        );
-        // eslint-disable-next-line prefer-destructuring
-        offerId = res.logs[0].args.offerId;
-        const task = await holdingStorage.getOfferTask.call(offerId);
-
-        const hash1 = await util.keccakAddressBytes(identities[0], task);
-        const hash2 = await util.keccakAddressBytes(identities[1], task);
-        const hash3 = await util.keccakAddressBytes(identities[2], task);
-
-        const sortedIdentities = [
-            {
-                identity: identities[0],
-                privateKey: privateKeys[0],
-                hash: hash1,
-            },
-            {
-                identity: identities[1],
-                privateKey: privateKeys[1],
-                hash: hash2,
-            },
-            {
-                identity: identities[2],
-                privateKey: privateKeys[2],
-                hash: hash3,
-            },
-        ].sort((x, y) => x.hash.localeCompare(y.hash));
-
-        const solution = await util.keccakBytesBytesBytes.call(
-            sortedIdentities[0].hash,
-            sortedIdentities[1].hash,
-            sortedIdentities[2].hash,
-        );
-
-        // Calculate task solution
-        for (var i = 65; i >= 2; i -= 1) {
-            if (task.charAt(task.length - 1) === solution.charAt(i)) break;
-        }
-        if (i === 2) {
-            assert(false, 'Could not find solution for offer challenge!');
-        }
-        const shift = 65 - i;
-
-        // Calculating confirmations to be signed by DH's
-        var confirmations = [];
-        let promises = [];
-        for (let i = 0; i < 3; i += 1) {
-            // eslint-disable-next-line no-await-in-loop
-            promises[i] = await util.keccakBytesAddress.call(offerId, sortedIdentities[i].identity);
-        }
-        confirmations = await Promise.all(promises);
-
-        // Signing calculated confirmations
-        promises = [];
-        for (let i = 0; i < 3; i += 1) {
-            // eslint-disable-next-line no-await-in-loop
-            promises[i] = web3.eth.accounts.sign(
-                confirmations[i],
-                sortedIdentities[i].privateKey,
-            );
-        }
-        const signedConfirmations = await Promise.all(promises);
-
-        await holding.finalizeOffer(
-            DC_identity,
-            offerId,
-            shift,
-            signedConfirmations[0].signature,
-            signedConfirmations[1].signature,
-            signedConfirmations[2].signature,
-            [new BN(2), new BN(2), new BN(2)],
-            [
-                sortedIdentities[0].identity,
-                sortedIdentities[1].identity,
-                sortedIdentities[2].identity,
-            ],
-            emptyAddress,
-            { from: DC_wallet },
-        );
     });
 
 
