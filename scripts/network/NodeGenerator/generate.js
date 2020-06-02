9da50b3af1296ce0803b437d8c685e77425e371a
Milos Kotlar <kotlarmilos@gmail.com>
2020-06-02 10:53:05 +0200

    test script

--- scripts/network/NodeGenerator/generate.js
+++ scripts/network/NodeGenerator/generate.js
@@ -13,7 +13,7 @@ if (process.argv.length > 2) {
 
 try {
     execSync('rm -rf ../../../../config-files');
-} catch (e) {}
+} catch (e) {console.log(e);}
 execSync('mkdir ../../../../config-files');
 
 for (let i = 1; i <= n; i += 1) {
@@ -46,7 +46,7 @@ for (let i = 1; i <= n; i += 1) {
     configDir = `../config-files/${nodeType}-config`;
     filename = `../config-files/${nodeType}.json`;
 
-    execSync(`cd ../../../../ot-node && npm run setup:hard -- --configDir=${configDir} --config=${filename}`);
+    // execSync(`cd ../../../../ot-node && npm run setup:hard -- --configDir=${configDir} --config=${filename}`);
     if (nodeType === 'DCG') {
         configDir = `../../../../config-files/${nodeType}-config`;
 
--- scripts/network/NodeGenerator/setup-environment.sh
+++ scripts/network/NodeGenerator/setup-environment.sh
@@ -11,11 +11,11 @@ echo ===============================
 echo ===== Deploying contracts =====
 echo ===============================
 
-screen -d -m -S ganache bash -c 'ganache-cli -i 5777 -a 1000 -p 7545 -l 10000000 -m "aspect ask story desert profit engage tuition leave fade giraffe exclude brief"'
+#screen -d -m -S ganache bash -c 'ganache-cli -i 5777 -a 1000 -p 7545 -l 10000000 -m "aspect ask story desert profit engage tuition leave fade giraffe exclude brief"'
 
-cd ../../../modules/Blockchain/Ethereum
-../../../node_modules/.bin/truffle migrate --network ganache
-cd ../../../scripts/network/NodeGenerator
+#cd ../../../modules/Blockchain/Ethereum
+#../../../node_modules/.bin/truffle migrate --network ganache
+#cd ../../../scripts/network/NodeGenerator
 
 echo ================================
 echo ======= Setting up nodes =======
