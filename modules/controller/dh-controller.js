1d6cdd1c79fb6856b33c8cb8a228c70e93de2d9f
Uros Kukic <kukic.uros@yahoo.com>
2020-07-17 14:50:26 +0200

    Remove trail body log

--- modules/controller/dh-controller.js
+++ modules/controller/dh-controller.js
@@ -101,8 +101,6 @@ class DHController {
             return;
         }
 
-        this.logger.notify(`Trail request body ${JSON.stringify(req.body, null, 4)}`);
-
         const { identifier_types, identifier_values } = req.body;
 
         if (Utilities.arrayze(identifier_types).length !==
