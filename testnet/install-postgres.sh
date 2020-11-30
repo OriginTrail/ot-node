3fb2a1949cc2f465937d8e3744589179821d0294
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2020-11-30 16:06:39 +0100

    Updated install postgres script

--- testnet/install-postgres.sh
+++ testnet/install-postgres.sh
@@ -2,4 +2,4 @@
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
-apt-get -y install postgresql-12
+apt-get -y install postgresql
