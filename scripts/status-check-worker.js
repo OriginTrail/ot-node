89410f8ba99e49cd2d644d9d1779e67f9455266a
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2020-12-02 10:44:00 +0100

    Updated worker process

--- scripts/status-check-worker.js
+++ scripts/status-check-worker.js
@@ -9,10 +9,10 @@ process.on('message', async (data) => {
     let isLive = true;
     try {
         do {
-            // const supervisorStatus = execSync('supervisorctl status
-            // | awk \'{print $1, $2, $6, $7}\'').toString().split('\n');
-            const supervisorStatus = 'arango RUNNING 6 days,\notnode RUNNING 6 days,\notnodelistener RUNNING 6 days,\nremote_syslog RUNNING 6 days,'.split('\n');
-
+            const supervisorStatus = execSync('supervisorctl status | awk \'{print $1, $2, $6, $7}\'').toString().split('\n');
+            // const supervisorStatus = 'arango RUNNING 6 days,\notnode RUNNING 6 days,
+            // \notnodelistener RUNNING 6 days,\nremote_syslog RUNNING 6 days,'.split('\n');
+            console.log(`Reveived supervisor status ${JSON.stringify(supervisorStatus)}`);
             for (let i = 1; i < supervisorStatus.length; i += 1) {
                 const status = supervisorStatus[i].split(' ');
                 if (['otnode', 'arango'].includes(status[0])) {
@@ -48,8 +48,12 @@ process.on('message', async (data) => {
                 setTimeout(() => resolve('done!'), 5000);
             });
         } while (isLive);
+        process.exit(0);
     } catch (e) {
-        console.log(e);
-        throw Error(e);
+        process.send({ error: `${e.message}\n${e.stack}` }, () => {
+            process.exit(0);
+        });
     }
 });
+
+process.once('SIGTERM', () => process.exit(0));
