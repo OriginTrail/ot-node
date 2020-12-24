81f3edbbbcd915fd2977134c333f263dec747f36
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2020-12-24 14:18:56 +0100

    Reverted changes in migration files

--- migrations/20180407124103-create-interaction-log.js
+++ migrations/20180407124103-create-interaction-log.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    up: (queryInterface, Sequelize) => queryInterface.createTable('interaction_log', {
+    up: (queryInterface, Sequelize) => queryInterface.createTable('interaction_logs', {
         id: {
             allowNull: false,
             autoIncrement: true,
@@ -20,5 +20,5 @@ module.exports = {
             type: Sequelize.STRING,
         },
     }),
-    down: (queryInterface, Sequelize) => queryInterface.dropTable('interaction_log'),
+    down: (queryInterface, Sequelize) => queryInterface.dropTable('interaction_logs'),
 };
