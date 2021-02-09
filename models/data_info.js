624231c6e594b4cc20b82849cdb97c31ea5c0024
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2021-02-09 13:56:10 +0100

    Added data provider wallets

--- models/data_info.js
+++ models/data_info.js
@@ -2,7 +2,6 @@
 module.exports = (sequelize, DataTypes) => {
     var data_info = sequelize.define('data_info', {
         data_set_id: DataTypes.STRING,
-        data_provider_wallets: DataTypes.TEXT,
         total_documents: DataTypes.INTEGER,
         root_hash: DataTypes.STRING(40),
         import_timestamp: DataTypes.DATE,
