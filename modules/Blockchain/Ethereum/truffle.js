7e4bc6c14be7bd5a733e3002c2a8e32096d2d6bc
Milos Kotlar <kotlarmilos@gmail.com>
2021-02-10 09:26:37 +0100

    Add solc version

--- modules/Blockchain/Ethereum/truffle.js
+++ modules/Blockchain/Ethereum/truffle.js
@@ -6,6 +6,7 @@ var mnemonic = process.env.TRUFFLE_MNEMONIC;
 module.exports = {
     compilers: {
         solc: {
+            version: '^0.4.24',
             settings: {
                 optimizer: {
                     enabled: true,
