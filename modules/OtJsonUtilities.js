3dc51086e73a36aaacfb80c6862c99e0613e4df5
Uros Kukic <kukic.uros@yahoo.com>
2021-02-11 14:59:19 +0100

    Overwrite ambiguous blockchain_id values

--- modules/OtJsonUtilities.js
+++ modules/OtJsonUtilities.js
@@ -1,5 +1,7 @@
 const Utilities = require('./Utilities');
 const { sha3_256 } = require('js-sha3');
+const defaultConfig = require('../config/config')[process.env.NODE_ENV];
+
 
 class OtJsonUtilities {
     /**
@@ -72,8 +74,18 @@ class OtJsonUtilities {
         const result = [];
         for (const creatorId of datasetHeader.dataCreator.identifiers) {
             const identity = creatorId.identifierValue;
-            const blockchain_id =
-                datasetHeader.validationSchemas[creatorId.validationSchema].networkId;
+
+            // Added to overwrite the previous ambiguous blockchain_id of Ethereum
+            let blockchain_id =
+                datasetHeader.validationSchemas[creatorId.validationSchema].networkId === 'mainnet'
+                    ? defaultConfig.blockchain.implementations[0].networkId
+                    : datasetHeader.validationSchemas[creatorId.validationSchema].networkId;
+
+            // Added for testing the fix locally, remove when tested
+            blockchain_id =
+                datasetHeader.validationSchemas[creatorId.validationSchema].networkId === 'ganache'
+                    ? defaultConfig.blockchain.implementations[0].networkId
+                    : datasetHeader.validationSchemas[creatorId.validationSchema].networkId;
 
             const schemaPostfix = creatorId.validationSchema.split('erc725-main').pop();
             const schemaEndpoint = `merkleRoot${schemaPostfix}`;
