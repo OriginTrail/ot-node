823383b4b890b28d02bac3687ac0829e35f2ac7c
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2021-05-13 15:42:22 +0200

    Removed recreate dataset from offer create database test

--- test/modules/command/dc/dc-offer-create-database-command.test.js
+++ test/modules/command/dc/dc-offer-create-database-command.test.js
@@ -38,7 +38,6 @@ describe('Checks DCOfferCreateDatabaseCommand execute() logic', function () {
 
     before('Setup preconditions and call DCOfferCreateDatabaseCommand execute function', async () => {
         config = rc(pjson.name, defaultConfig);
-        await testUtilities.recreateDatabase();
 
         // make sure offers table is cleaned up
 
