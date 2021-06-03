1c80ccecc88eaa1586e28547f3e49295d45ea3a0
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2021-06-03 09:58:10 +0200

    Added revert methods in migration

--- migrations/202106011054103-update-column-types.js
+++ migrations/202106011054103-update-column-types.js
@@ -20,5 +20,23 @@ module.exports = {
                 type: Sequelize.STRING,
             });
     },
-    down: async (queryInterface) => { },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface
+            .changeColumn('public_keys', 'public_key', {
+                type: Sequelize.STRING,
+            });
+        await queryInterface
+            .changeColumn('public_keys', 'timestamp', {
+                type: Sequelize.INTEGER,
+            });
+        await queryInterface
+            .changeColumn('offers', 'data_set_id', {
+                type: Sequelize.STRING,
+                unique: true,
+            });
+        await queryInterface
+            .changeColumn('offers', 'price_factor_used_for_price_calculation', {
+                type: Sequelize.INTEGER,
+            });
+    },
 };
