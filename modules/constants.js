5062d3f4c61b874e85e1d4c1955742d27a4de142
Kuki145 <kukic.uros@yahoo.com>
2021-06-16 15:41:28 +0200

    Add Blockchain title costant map

--- modules/constants.js
+++ modules/constants.js
@@ -213,3 +213,14 @@ exports.IDENTITY_PERMISSION = {
     action: '2',
     encryption: '4',
 };
+
+/**
+ * @constant {Object: string} BLOCKCHAIN_TITLE
+ * Possible types of blockchain implementations available on ot-node
+ * @type {{Starfleet: string, Ethereum: string, XDai: string}}
+ */
+exports.BLOCKCHAIN_TITLE = {
+    Starfleet: 'Starfleet',
+    Ethereum: 'Ethereum',
+    XDai: 'XDai',
+};
