b264762ff271694d4fda3e8e6e2dee0d14c2a734
Milos Kotlar <kotlarmilos@gmail.com>
2021-08-18 16:21:41 +0200

    Patch mariner environment as mainnet

--- modules/Blockchain/Polygon/index.js
+++ modules/Blockchain/Polygon/index.js
@@ -1,3 +1,5 @@
+const environment = process.env.NODE_ENV === 'mariner' ? 'mainnet' : process.env.NODE_ENV;
+
 const Web3Implementation = require('../Web3Implementation');
 const path = require('path');
 const axios = require('axios');
@@ -46,7 +48,7 @@ class Polygon extends Web3Implementation {
     async calculateGasPrice() {
         const now = new Date().getTime();
 
-        if (process.env.NODE_ENV === 'development' || (this.config.gas_price_last_update_timestamp
+        if (environment === 'development' || (this.config.gas_price_last_update_timestamp
             + constants.GAS_PRICE_VALIDITY_TIME_IN_MILLS > now)) {
             this.logger.trace(`[${this.getBlockchainId()}] Using default gas price from `
                 + `configuration: ${this.config.gas_price}`);
@@ -72,7 +74,7 @@ class Polygon extends Web3Implementation {
     }
 
     async getGasStationPrice() {
-        const response = await axios.get(gasStationLinks[process.env.NODE_ENV])
+        const response = await axios.get(gasStationLinks[environment])
             .catch((err) => {
                 this.logger.warn(err);
                 return undefined;
