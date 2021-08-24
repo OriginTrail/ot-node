780fb7454b5c11c8ac6c6dbe5ecd696bd983bd0d
Milos Kotlar <kotlarmilos@gmail.com>
2021-08-24 15:59:58 +0200

    Update ganache migration script for Polygon mainnet

--- modules/Blockchain/Polygon/migrations/2_total_migration.js
+++ modules/Blockchain/Polygon/migrations/2_total_migration.js
@@ -162,6 +162,101 @@ module.exports = async (deployer, network, accounts) => {
             console.log('Updated contract in Hub contract map!');
         }
         break;
+    case 'mainnet':
+        await deployer.deploy(Hub, { gas: 6000000, from: accounts[0] })
+            .then((result) => {
+                hub = result;
+            });
+
+        await deployer.deploy(Hub, { gas: 6000000, from: accounts[0] })
+            .then((result) => {
+                hub = result;
+            });
+        await hub.setContractAddress('Owner', accounts[0]);
+
+        profileStorage = await deployer.deploy(
+            ProfileStorage,
+            hub.address, { gas: 6000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('ProfileStorage', profileStorage.address);
+
+        holdingStorage = await deployer.deploy(
+            HoldingStorage,
+            hub.address,
+            { gas: 6000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('HoldingStorage', holdingStorage.address);
+
+        marketplaceStorage = await deployer.deploy(
+            MarketplaceStorage,
+            hub.address,
+            { gas: 6000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('MarketplaceStorage', marketplaceStorage.address);
+
+        litigationStorage = await deployer.deploy(
+            LitigationStorage,
+            hub.address,
+            { gas: 6000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('LitigationStorage', litigationStorage.address);
+
+        approval = await deployer.deploy(Approval);
+        await hub.setContractAddress('Approval', approval.address);
+
+        await hub.setContractAddress('Token', '0x4287F07CBE6954f9F0DecD91d0705C926d8d03A4');
+
+        profile = await deployer.deploy(Profile, hub.address, { gas: 9000000, from: accounts[0] });
+        await hub.setContractAddress('Profile', profile.address);
+
+        holding = await deployer.deploy(Holding, hub.address, { gas: 8000000, from: accounts[0] });
+        await hub.setContractAddress('Holding', holding.address);
+
+        creditorHandler = await deployer.deploy(
+            CreditorHandler,
+            hub.address,
+            { gas: 7000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('CreditorHandler', creditorHandler.address);
+
+        litigation = await deployer.deploy(
+            Litigation,
+            hub.address,
+            { gas: 6000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('Litigation', litigation.address);
+
+        marketplace = await deployer.deploy(
+            Marketplace,
+            hub.address,
+            { gas: 7000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('Marketplace', marketplace.address);
+
+        replacement = await deployer.deploy(
+            Replacement,
+            hub.address,
+            { gas: 7000000, from: accounts[0] },
+        );
+        await hub.setContractAddress('Replacement', replacement.address);
+
+
+        console.log('\n\n \t Contract adressess on polygon mainnet:');
+        console.log(`\t Hub contract address: \t\t\t${hub.address}`);
+        console.log(`\t Approval contract address: \t\t${approval.address}`);
+        console.log(`\t Token contract address: \t\t0x4287F07CBE6954f9F0DecD91d0705C926d8d03A4`);
+        console.log(`\t Profile contract address: \t\t${profile.address}`);
+        console.log(`\t Holding contract address: \t\t${holding.address}`);
+        console.log(`\t Litigation contract address: \t\t${litigation.address}`);
+        console.log(`\t Marketplace contract address: \t\t${marketplace.address}`);
+        console.log(`\t Replacement contract address: \t\t${replacement.address}`);
+
+        console.log(`\t ProfileStorage contract address: \t${profileStorage.address}`);
+        console.log(`\t HoldingStorage contract address: \t${holdingStorage.address}`);
+        console.log(`\t LitigationStorage contract address: \t${litigationStorage.address}`);
+        console.log(`\t MarketplaceStorage contract address: \t${marketplaceStorage.address}`);
+
+        break;
     default:
         console.warn('Please use one of the following network identifiers: ganache, mock, test, or rinkeby');
         break;
