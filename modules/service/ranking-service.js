dab5972223da4449d32de8ffac0671be4dda179f
NZT48 <nikolaztodorovic26@gmail.com>
2022-01-20 14:25:23 +0100

    Fix loading params from config

--- modules/service/ranking-service.js
+++ modules/service/ranking-service.js
@@ -8,13 +8,18 @@ class RankingService {
     initialize() {
         this.implementation = new KadIdentityRanking();
         this.implementation.initialize(this.logger);
+        // Object.keys(implementations).forEach((key, index) => {
+        //     implementations[key].initialize(this.logger);
+        // });
     }
 
     async rank(nodes, topic, rankingTypes) {
         let rankedNodes = [];
-        for (const type of rankingTypes) {
-            rankedNodes = await this.implementation[type].execute(nodes, topic);
-        }
+        // console.log(this.implementation);
+        // for (const type of rankingTypes) {
+        //     rankedNodes = await this.implementation[type].execute(nodes, topic);
+        // }
+        rankedNodes = await this.implementation.execute(nodes, topic);
         return rankedNodes;
     }
 }
