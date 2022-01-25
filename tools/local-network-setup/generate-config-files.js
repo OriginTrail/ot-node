c44962d7a472f2bafbe5dcdfdaf883724e310fcc
NZT48 <nikolaztodorovic26@gmail.com>
2022-01-25 10:27:33 +0100

    Update file paths and rename variables in local network tool

--- tools/local-network-setup/generate-config-files.js
+++ tools/local-network-setup/generate-config-files.js
@@ -2,25 +2,24 @@ const path = require('path');
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-const template = JSON.parse(fs.readFileSync('./.dh_origintrail_noderc'));
+const template = JSON.parse(fs.readFileSync('./tools/local-network-setup/.dh_origintrail_noderc'));
 
-const path_to_node = path.join(__dirname, '../');
-const number_of_nodes = process.argv.length === 3 ? parseInt(process.argv[2], 10) : 4;
+const numberOfNodes = process.argv.length === 3 ? parseInt(process.argv[2], 10) : 4;
 
-console.log(`Generating ${number_of_nodes} total nodes`);
+console.log(`Generating ${numberOfNodes} total nodes`);
 
-for (let i = 0; i < number_of_nodes; i += 1) {
-    let node_name;
+for (let i = 0; i < numberOfNodes; i += 1) {
+    let nodeName;
     if (i === 0) {
         console.log('Using the preexisting identity for the first node (bootstrap)');
-        node_name = 'bootstrap';
+        nodeName = 'bootstrap';
         continue;
     } else {
-        node_name = `DH${i}`;
+        nodeName = `DH${i}`;
     }
-    console.log(`Configuring node ${node_name}`);
+    console.log(`Configuring node ${nodeName}`);
 
-    const configPath = path.join(`./.dh${i}_origintrail_noderc`);
+    const configPath = path.join(`./tools/local-network-setup/.dh${i}_origintrail_noderc`);
     execSync(`touch ${configPath}`);
 
     const parsedTemplate = JSON.parse(JSON.stringify(template));
