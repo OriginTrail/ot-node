6546aec3e27d4822290ec2e26ab7f16089993c2a
NZT48 <nikolaztodorovic26@gmail.com>
2022-01-25 10:52:20 +0100

    Load blockchain keys from .env

--- tools/local-network-setup/generate-config-files.js
+++ tools/local-network-setup/generate-config-files.js
@@ -1,10 +1,29 @@
 const path = require('path');
 const fs = require('fs');
 const { execSync } = require('child_process');
-
-const template = JSON.parse(fs.readFileSync('./tools/local-network-setup/.dh_origintrail_noderc'));
+require('dotenv').config('.env');
 
 const numberOfNodes = process.argv.length === 3 ? parseInt(process.argv[2], 10) : 4;
+
+const templatePath = './tools/local-network-setup/.dh_origintrail_noderc';
+const bootstrapTemplatePath = './tools/local-network-setup/.bootstrap_origintrail_noderc';
+
+const template = JSON.parse(fs.readFileSync(templatePath));
+const bootstrapTemplate = JSON.parse(fs.readFileSync(bootstrapTemplatePath));
+
+console.log('Preparing keys for blockchain');
+
+if (!process.env.PRIVATE_KEY || !process.env.PUBLIC_KEY) {
+    console.log('Missing blockchain keys in .env file');
+    process.exit(1);
+}
+
+template.blockchain[0].publicKey = process.env.PUBLIC_KEY;
+template.blockchain[0].privateKey = process.env.PRIVATE_KEY;
+bootstrapTemplate.blockchain[0].publicKey = process.env.PUBLIC_KEY;
+bootstrapTemplate.blockchain[0].privateKey = process.env.PRIVATE_KEY;
+
+fs.writeFileSync(bootstrapTemplatePath, JSON.stringify(bootstrapTemplate, null, 2));
 
 console.log(`Generating ${numberOfNodes} total nodes`);
 
