1ff794e657e16e6e2b564401f9de7d48204fb518
NZT48 <nikolaztodorovic26@gmail.com>
2022-02-09 12:33:56 +0100

    Blazegraph support implemented

--- external/blazegraph-service.js
+++ external/blazegraph-service.js
@@ -1,120 +1,98 @@
-const { ServerClientConfig, GraphDBServerClient } = require('graphdb').server;
-const { RepositoryClientConfig, RepositoryConfig, RepositoryType } = require('graphdb').repository;
-const { SparqlXmlResultParser } = require('graphdb').parser;
-const { GetQueryPayload, QueryType } = require('graphdb').query;
-const { RDFMimeType } = require('graphdb').http;
 const axios = require('axios');
-const { execSync } = require('child_process');
 const jsonld = require('jsonld');
 const N3 = require('n3');
-const { BufferList } = require('bl');
+const qs = require('qs');
 const constants = require('../modules/constants');
 
-class GraphdbService {
+class BlazegraphService {
     constructor(config) {
         this.config = config;
     }
 
     async initialize(logger) {
         this.logger = logger;
-        this.logger.info(`Data repository name: ${this.config.repositoryName}`);
-        const serverConfig = new ServerClientConfig('http://localhost:7200/')
-            .setTimeout(40000)
-            .setHeaders({
-                Accept: RDFMimeType.N_QUADS,
-            })
-            .setKeepAlive(true);
-        this.server = new GraphDBServerClient(serverConfig);
-
-        const exists = await this.server.hasRepository(this.config.repositoryName);
-        if (!exists) {
-            const newConfig = new RepositoryConfig(this.config.repositoryName, '', new Map(), '', 'Repo title', RepositoryType.FREE);
-            // Use the configuration to create new repository
-            await this.server.createRepository(newConfig);
-        }
-
-        const readTimeout = 30000;
-        const writeTimeout = 30000;
-        const repositoryServerConfig = new RepositoryClientConfig('http://localhost:7200/')
-            .setEndpoints([`http://localhost:7200/repositories/${this.config.repositoryName}`])
-            .setHeaders({
-                Accept: RDFMimeType.N_QUADS,
-            })
-            .setReadTimeout(readTimeout)
-            .setWriteTimeout(writeTimeout);
-
-        this.repository = await this.server.getRepository(this.config.repositoryName, repositoryServerConfig);
-        this.repository.registerParser(new SparqlXmlResultParser());
-        this.logger.info('GraphDB module initialized successfully');
+        this.logger.info('Blazegraph module initialized successfully');
     }
 
     async insert(triples, rootHash) {
-        const contentType = RDFMimeType.N_QUADS;
-        await this.repository.overwrite(triples, contentType, rootHash);
+        const askQuery = `ASK WHERE { GRAPH <${rootHash}> { ?s ?p ?o } }`;
+        const exists = await this.ask(askQuery);
+        if (!exists) {
+            const config = {
+                method: 'post',
+                url: `http://localhost:9999/blazegraph/sparql?context-uri=${rootHash}`,
+                headers: {
+                    'Content-Type': 'text/x-nquads',
+                },
+                data: triples,
+            };
+
+            await axios(config).then((response) => true)
+                .catch((error) => {
+                    this.logger.error(`Failed to write into Blazegraph: ${error} - ${error.stack}`);
+                    return false;
+                });
+        }
+        // TODO: else -> Should log if we already have data
     }
 
     async execute(query) {
         return new Promise(async (accept, reject) => {
-            const payload = new GetQueryPayload()
-                .setQuery(query)
-                .setQueryType(QueryType.SELECT)
-                .setResponseType(RDFMimeType.SPARQL_RESULTS_JSON);
-
-            try {
-                const stream = await this.repository.query(payload);
-                const bl = new BufferList();
-                stream.on('data', (bindings) => {
-                    bl.append(bindings);
-                });
-                stream.on('end', () => {
-                    accept(bl);
-                });
-            } catch (e) {
-                reject(e);
-            }
+            const data = qs.stringify({
+                query,
+            });
+            const config = {
+                method: 'post',
+                url: 'http://localhost:9999/blazegraph/sparql',
+                headers: {
+                    Accept: 'application/sparql-results+json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                data,
+            };
+            axios(config).then((response) => {
+                accept(response.data);
+            }).catch((e) => reject(e));
         });
     }
 
     async construct(query) {
         return new Promise(async (accept, reject) => {
-            const payload = new GetQueryPayload()
-                .setQuery(query)
-                .setQueryType(QueryType.CONSTRUCT)
-                .setResponseType(RDFMimeType.N_QUADS);
-
-            try {
-                const stream = await this.repository.query(payload);
-                const bl = new BufferList();
-                stream.on('data', (bindings) => {
-                    bl.append(bindings);
-                });
-                stream.on('end', () => {
-                    accept(bl);
-                });
-            } catch (e) {
-                reject(e);
-            }
+            const data = qs.stringify({
+                query,
+            });
+            const config = {
+                method: 'post',
+                url: 'http://localhost:9999/blazegraph/sparql',
+                headers: {
+                    Accept: 'text/x-nquads',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                data,
+            };
+            axios(config).then((response) => {
+                accept(response.data);
+            }).catch((e) => reject(e));
         });
     }
 
     async ask(query) {
         return new Promise(async (accept, reject) => {
-            const payload = new GetQueryPayload()
-                .setQuery(query)
-                .setQueryType(QueryType.ASK)
-                .setResponseType(RDFMimeType.BOOLEAN_RESULT);
-            try {
-                const stream = await this.repository.query(payload);
-                const bl = new BufferList();
-                stream.on('data', (bindings) => {
-                    bl.append(bindings);
-                });
-                stream.on('end', () => {
-                    accept(bl);
-                });
-            } catch (e) {
-                reject(e);
-            }
+            const data = qs.stringify({
+                query,
+            });
+            const config = {
+                method: 'post',
+                url: 'http://localhost:9999/blazegraph/sparql',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                data,
+            };
+            axios(config).then((response) => {
+                accept(response.data.boolean);
+            }).catch((e) => reject(e));
         });
     }
 
@@ -274,7 +252,7 @@ class GraphdbService {
     async findAssertions(nquads) {
         const query = `SELECT ?g
                        WHERE {
-                            GRAPH ?g { 
+                            GRAPH ?g {
                             ${nquads}
                             }
                        }`;
@@ -303,7 +281,7 @@ class GraphdbService {
                         group by ?outerAssetId
                         ${options.limit ? `LIMIT ${options.limit}`: ''}`;
         let result = await this.execute(sparqlQuery);
-        result = JSON.parse(result).results.bindings;
+        result = result.results.bindings;
         return result;
     }
 
@@ -322,45 +300,25 @@ class GraphdbService {
                             LIMIT ${options.limit}`;
 
         let result = await this.execute(sparqlQuery);
-        result = JSON.parse(result).results.bindings;
+        result = result.results.bindings;
         return result;
     }
 
     async healthCheck() {
         try {
-            const response = await axios.get(`http://localhost:7200/repositories/${this.config.repositoryName}/health`, {},
-                {
-                    auth: {
-                        username: this.config.username,
-                        password: this.config.password,
-                    },
-                });
-            if (response.data.status === 'green') {
+            const response = await axios.get('http://localhost:9999/blazegraph/status', {});
+            if (response.data !== null) {
                 return true;
             }
             return false;
         } catch (e) {
-            if (e.response && e.response.status === 404) {
-                // Expected error: GraphDB is up but has not created node0 repository
-                // Ot-node will create repo in initialization
-                return true;
-            }
             return false;
         }
     }
 
-    async restartService() {
-        // TODO: check env if development or production
-        const port = execSync('ps -aux | grep graphdb | cut -d\' \' -f7 | head -n 1').toString();
-        if (port) {
-            execSync(`kill -9 ${port}`);
-        }
-        execSync('nohup ../graphdb-free-9.9.0/bin/graphdb &');
-    }
-
     getName() {
-        return 'GraphDB';
+        return 'Blazegraph';
     }
 }
 
-module.exports = GraphdbService;
+module.exports = BlazegraphService;
