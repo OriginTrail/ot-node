c5ae730f2954d47345d1dfb2e59c851a83032d84
zeroxbt <original.swazi@protonmail.com>
2022-02-10 16:52:04 +0100

    removed os check

--- installer/installer.sh
+++ installer/installer.sh
@@ -10,15 +10,7 @@ NC='\033[0m' # No Color
 
 clear
 
-echo -n "${N1}Checking that the OS is Ubuntu 20.04 or newer: "
-
-if [[ $OS_VERSION != 20.04 ]] || [[ $OS_VERSION != 21.04 ]] || [[ $OS_VERSION != 21.10 ]]; then
-    echo -e "${RED}FAILED${NC}"
-    echo "This installer requires Ubuntu 20.04 or newer. Destroy this VPS and remake using Ubuntu 20.04 or newer."
-    exit 1
-else
-    echo -e "${GREEN}SUCCESS${NC}"
-fi
+cd /root/ot-node
 
 echo -n "Checking that we are in /root directory: "
 
