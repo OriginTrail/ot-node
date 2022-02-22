14d4d935e65930cd38a3190c98c4b46eb1013c91
zeroxbt <original.swazi@protonmail.com>
2022-02-22 15:31:25 +0100

    add buffer before sending received data

--- external/libp2p-service.js
+++ external/libp2p-service.js
@@ -173,9 +173,9 @@ class Libp2pService {
                 if (!async) {
                     const result = await handler(data);
                     this.logger.info(`Sending response from ${this.config.id} to ${handlerProps.connection.remotePeer._idB58String}: event=${eventName};`);
-
+                    const stringifiedData = await this.workerPool.exec('JSONStringify', [data]);
                     await pipe(
-                        [JSON.stringify(result)],
+                        [Buffer.from(stringifiedData)],
                         stream,
                     )
                 } else {
