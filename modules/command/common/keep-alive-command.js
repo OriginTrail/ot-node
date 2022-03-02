2150e7e7a48ac9c80527ff20cb8d09982dc65142
Milos Kotlar <kotlarmilos@gmail.com>
2022-03-02 17:08:52 +0100

    fix signaling proof

--- modules/command/common/keep-alive-command.js
+++ modules/command/common/keep-alive-command.js
@@ -48,8 +48,11 @@ class KeepAliveCommand extends Command {
             this.logger.error(`An error has occurred with signaling data. ${e.message}`);
         }
 
-        signalingMessage.proof.hash = this.validationService.calculateHash(signalingMessage);
-        signalingMessage.proof.signature = this.validationService.sign(signalingMessage.proof.hash);
+        const proof = {};
+        proof.hash = this.validationService.calculateHash(signalingMessage);
+        proof.signature = this.validationService.sign(proof.hash);
+
+        signalingMessage.proof = proof;
 
         const config = {
             method: 'post',
@@ -60,7 +63,7 @@ class KeepAliveCommand extends Command {
             data : JSON.stringify(signalingMessage)
         };
 
-        axios(config);
+        await axios(config);
         return Command.repeat();
     }
 
