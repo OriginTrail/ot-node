2c85dd46c7a55259083d3ba260102776bdb52fb9
zeroxbt <original.swazi@protonmail.com>
2022-03-25 11:49:31 +0100

    add all routes in dkg-client-helper functions

--- test/utilities/dkg-client-helper.js
+++ test/utilities/dkg-client-helper.js
@@ -12,113 +12,89 @@ class DkgClientHelper {
     async info() {
         return this.client.nodeInfo();
     }
-    //
-    // async provision (data, keywords) {
-    //     return this.client._publishRequest({
-    //         data,
-    //         keywords,
-    //         method: 'provision',
-    //         visibility: 'public',
-    //     }).catch((error) => {
-    //         console.log(`error provisioning dataset. ${error}`);
-    //     });
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
-    //
-    // async publish () {
-    //     return this.client.nodeInfo();
-    // }
+
+    async provision(data, keywords) {
+        return this.client._publishRequest({
+            data,
+            keywords,
+            method: 'provision',
+            visibility: 'public',
+        }).catch((error) => {
+            console.log(`error provisioning dataset. ${error}`);
+        });
+    }
+
+    async publish(data, keywords) {
+        return this.client._publishRequest({
+            data,
+            keywords,
+            method: 'publish',
+            visibility: 'public',
+        }).catch((error) => {
+            console.log(`error publishing dataset. ${error}`);
+        });
+    }
+
+    async update(data, keywords, ual) {
+        return this.client._publishRequest({
+            ual,
+            data,
+            keywords,
+            method: 'update',
+            visibility: 'public',
+        }).catch((error) => {
+            console.log(`error publishing dataset. ${error}`);
+        });
+    }
+
+    async resolve(ids) {
+        return this.client
+            ._resolveRequest({
+                ids,
+            }).catch((error) => {
+                console.log(`error resolving. ${error}`);
+            });
+    }
+
+    async search(resultType, query) {
+        return this.client
+            ._searchRequest({
+                resultType,
+                query,
+            }).catch((error) => {
+                console.log(`error searching. ${error}`);
+            });
+    }
+
+    async getSearchResult(resultType, handler_id) {
+        return this.client
+            ._getSearchResult({
+                resultType,
+                handler_id,
+            })
+            .catch((error) => {
+                console.log(`error getting search result. ${error}`);
+            });
+    }
+
+    async query(query) {
+        return this.client
+            ._queryRequest({
+                query,
+            }).catch((error) => {
+                console.log(`error querying. ${error}`);
+            });
+    }
+
+    async getResult(operation, handler_id) {
+        return this.client
+            ._getResult({
+                handler_id,
+                operation,
+            }).catch((error) => {
+                console.log(`error getting result. ${error}`);
+            });
+    }
 }
+
 module.exports = DkgClientHelper;
-//
-// const client = );
-//
-// exports.info = async () => client.nodeInfo();
-//
-// exports.publish = async (data, keywords) => client
-//     ._publishRequest({
-//         data,
-//         keywords,
-//         method: 'publish',
-//         visibility: 'public',
-//     })
-//     .catch((error) => {
-//         console.log(`error publishing dataset. ${error}`);
-//     });
-//
-// exports.update = async (data, keywords, ual) => client.assets
-//     .update(ual, {
-//         data,
-//         keywords,
-//         method: 'update',
-//         visibility: 'public',
-//     })
-//     .catch((error) => {
-//         console.log(`error updating dataset. ${error}`);
-//     });
-//
-// exports.resolve = async (ids) => client
-//     ._resolveRequest({
-//         ids,
-//     })
-//     .catch((error) => {
-//         console.log(`error resolving. ${error}`);
-//     });
-//
-// exports.search = async (resultType, query) => client
-//     ._searchRequest({
-//         resultType,
-//         query,
-//     })
-//     .catch((error) => {
-//         console.log(`error searching. ${error}`);
-//     });
-//
-// exports.getSearchResult = async (resultType, handler_id) => client
-//     ._getSearchResult({
-//         resultType,
-//         handler_id,
-//     })
-//     .catch((error) => {
-//         console.log(`error getting search result. ${error}`);
-//     });
-//
-// exports.query = async (query) => client
-//     ._queryRequest({
-//         query,
-//     })
-//     .catch((error) => {
-//         console.log(`error querying. ${error}`);
-//     });
-//
-// exports.getResult = async (operation, handler_id) => client
-//     ._getResult({
-//         handler_id,
-//         operation,
-//     })
-//     .catch((error) => {
-//         console.log(`error getting result. ${error}`);
-//     });
