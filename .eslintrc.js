6d5e767626621322d0388c8e8f4ae6d08f7d86ce
vterzic <veljkot@gmail.com>
2022-03-28 15:32:46 +0200

    format files to match project's already existing tabWidth 4 rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,20 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: ['airbnb', 'prettier'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2017,
-  },
-  rules: {
-    'linebreak-style': ['error', 'unix'],
-    camelcase: 0,
-    'class-methods-use-this': 0,
-    'consistent-return': 0,
-    'no-restricted-syntax': 0,
-    'guard-for-in': 0,
-    'no-console': 'warn',
-  },
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: ['airbnb', 'prettier'],
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2017,
+    },
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        camelcase: 0,
+        'class-methods-use-this': 0,
+        'consistent-return': 0,
+        'no-restricted-syntax': 0,
+        'guard-for-in': 0,
+        'no-console': 'warn',
+    },
 };
