e5c1422709c5ea4acfe1c87970f81d6cb9f57224
angrymob <hansi1337gmail.com>
2022-03-29 15:39:15 +0200

    Add Testcase for findAssertsionByKeyword and helper funcions for param validatio

--- test/modules/sparlql-query-service.test.js
+++ test/modules/sparlql-query-service.test.js
@@ -4,17 +4,22 @@ const {
     before,
     after,
 } = require('mocha');
+const chai = require('chai');
+
 const {
     assert,
     expect,
-} = require('chai');
+} = chai;
+
 const Sparql = require('../../external/sparqlquery-service');
 const Logger = require('../../modules/logger/logger');
 
-const config = { url: 'http://localhost:9999/blazegraph/namespace/kb/sparql' };
+const config = { url: 'http://35.217.28.54:9999/blazegraph/namespace/kb/sparql' };
 
 let sparqlService = null;
 let logger = null;
+chai.use(require('chai-as-promised'));
+
 describe('Sparql module', () => {
     before('Initialize Logger', async () => {
         logger = new Logger('trace', false);
@@ -26,10 +31,77 @@ describe('Sparql module', () => {
         sparqlService = new Sparql(config);
         await sparqlService.initialize(logger);
     });
-    it('Execute HealthCheck', async () => {
-        const a = await sparqlService.healthCheck();
-        expect(a)
+    it('Check for cleanup', async () => {
+        // Success
+        expect(sparqlService.cleanEscapeCharacter('keywordabc'))
             .to
-            .equal(true);
+            .equal('keywordabc');
+        // Fail
+        expect(sparqlService.cleanEscapeCharacter('keywordabc\''))
+            .to
+            .equal('keywordabc\\\'');
     });
+    it('Check limit creation', async () => {
+        // Success
+        expect(() => sparqlService.createLimitQuery({ limit: 'abc' }))
+            .to
+            .throw(Error);
+
+        expect(() => sparqlService.createLimitQuery({ limit: Math.random() }))
+            .to
+            .throw(Error);
+
+        expect(sparqlService.createLimitQuery({}))
+            .to
+            .equal('');
+        // var randomnumber = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+        // eslint-disable-next-line no-bitwise
+        const random = (Math.random() * (99999999 - 1 + 1)) << 0;
+        const negativeRandom = random * -1;
+
+        expect(sparqlService.createLimitQuery({ limit: random }))
+            .to
+            .equal(`LIMIT ${random}`);
+
+        expect(() => sparqlService.createLimitQuery({ limit: negativeRandom }))
+            .to
+            .throw(Error);
+    });
+
+    it('Check FindAssertionsByKeyword', async () => {
+        await expect(sparqlService.findAssertionsByKeyword('abc', { limit: 'aaaa' }, false))
+            .to
+            .be
+            .rejectedWith(Error);
+
+        const test = await sparqlService.findAssertionsByKeyword('pub', {
+            limit: 5,
+            prefix: true
+        }, true);
+        // eslint-disable-next-line no-unused-expressions
+        expect(test)
+            .to
+            .be
+            .not
+            .null;
+
+        await expect(sparqlService.findAssertionsByKeyword('abc', { limit: '90' }, 'test'))
+            .to
+            .be
+            .rejectedWith(Error);
+
+        await expect(sparqlService.findAssertionsByKeyword('abc', {
+            limit: '90',
+            prefix: 'test',
+        }))
+            .to
+            .be
+            .rejectedWith(Error);
+
+        await expect(sparqlService.findAssertionsByKeyword('abc', { limit: '90' }))
+            .to
+            .be
+            .rejectedWith(Error);
+    })
+        .timeout(100000);
 });
