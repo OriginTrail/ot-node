eee71bbd1c48c54808c0ec58da3adc41ded76a4e
angrymob <hansi1337gmail.com>
2022-03-30 15:14:37 +0200

    Implement findAssetsByKeyword; Refactor filterParameters to own method

--- external/sparqlquery-service.js
+++ external/sparqlquery-service.js
@@ -6,6 +6,14 @@ class SparqlqueryService {
         this.config = config;
     }
 
+    const;
+    filtertype = {
+        KEYWORD: 'keyword',
+        KEYWORDPREFIX: 'keywordPrefix',
+        TYPES: 'types',
+        ISSUERS: 'issuers'
+    };
+
     async initialize(logger) {
         this.logger = logger;
         this.logger.info('Sparql Query module initialized successfully');
@@ -50,11 +58,10 @@ class SparqlqueryService {
             throw new Error('Localquery is not an boolean');
         }
         let limitQuery = '';
-        query = this.cleanEscapeCharacter(query);
         limitQuery = this.createLimitQuery(options);
 
         const publicVisibilityQuery = !localQuery ? ' ?assertionId schema:hasVisibility "public" .' : '';
-        const filterQuery = options.prefix ? `FILTER contains(lcase(?keyword),'${query}')` : `FILTER (lcase(?keyword) = '${query}')`;
+        const filterQuery = options.prefix ? this.createFilterParameter(query, this.filtertype.KEYWORDPREFIX) : this.createFilterParameter(query, this.filtertype.KEYWORD);
 
         const sparqlQuery = `PREFIX schema: <http://schema.org/>
                             SELECT distinct ?assertionId
@@ -70,7 +77,46 @@ class SparqlqueryService {
     }
 
     async findAssetsByKeyword(query, options, localQuery) {
-        this.logger.info('dummy for ESLint');
+        if (options.prefix && !(typeof options.prefix === 'boolean')) {
+            this.logger.error(`Failed FindAssetsByKeyword: ${options.prefix} is not a boolean`);
+            throw new Error('Prefix is not an boolean');
+        }
+        if (localQuery && !(typeof localQuery === 'boolean')) {
+            this.logger.error(`Failed FindAssetsByKeyword: ${localQuery} is not a boolean`);
+            throw new Error('Localquery is not an boolean');
+        }
+        query = this.cleanEscapeCharacter(query);
+        const limitQuery = this.createLimitQuery(options);
+
+        const publicVisibilityQuery = !localQuery ? 'schema:hasVisibility "public" :' : '';
+        const filterQuery = options.prefix ? this.createFilterParameter(query, this.filtertype.KEYWORDPREFIX) : this.createFilterParameter(query, this.filtertype.KEYWORD);
+        const issuerFilter = options.issuers ? this.createFilterParameter(options.issuers, this.filtertype.ISSUERS) : '';
+        const typesFilter = options.types ? this.createFilterParameter(options.types, this.filtertype.TYPES) : '';
+
+        const sparqlQuery = `PREFIX schema: <http://schema.org/>
+                            SELECT ?assertionId
+                            WHERE {
+                                ?assertionId schema:hasTimestamp ?latestTimestamp ;
+                                 ${publicVisibilityQuery}
+                                                     schema:hasUALs ?assetId .
+                                    {
+                                        SELECT ?assetId (MAX(?timestamp) AS ?latestTimestamp)
+                                        WHERE {
+                                            ?assertionId schema:hasKeywords ?keyword ;
+                                                         schema:hasIssuer ?issuer ;
+                                                         schema:hasType ?type ;
+                                                         schema:hasTimestamp ?timestamp ;
+                                                         schema:hasUALs ?assetId .
+                                ${filterQuery}
+                                ${issuerFilter}
+                                ${typesFilter}
+                                        }
+                                        GROUP BY ?assetId
+                                        ${limitQuery}
+                                    }
+                            }`;
+        const result = await this.execute(sparqlQuery);
+        return result;
     }
 
     async healthCheck() {
@@ -97,12 +143,31 @@ class SparqlqueryService {
         return query.replace(/['|[\]\\]/g, '\\$&');
     }
 
+    createFilterParameter(queryParameter, type) {
+        queryParameter = this.cleanEscapeCharacter(queryParameter);
+
+        switch (type) {
+        case this.filtertype.KEYWORD:
+            return `FILTER (lcase(?keyword) = '${queryParameter}')`;
+        case this.filtertype.KEYWORDPREFIX:
+            return `FILTER contains(lcase(?keyword),'${queryParameter}')`;
+        case this.filtertype.ISSUERS:
+            return `FILTER (?issuer IN (${JSON.stringify(queryParameter)
+                .slice(1, -1)}))`;
+        case this.filtertype.TYPES:
+            return `FILTER (?type IN (${JSON.stringify(queryParameter)
+                .slice(1, -1)}))`;
+        }
+        return '';
+
+    }
+
     createLimitQuery(options) {
         if (options.limit && !Number.isInteger(options.limit)) {
-            this.logger.error(`Failed FindassertionsByKeyword: ${options.limit} is not a number`);
+            this.logger.error(`Failed creating Limit query: ${options.limit} is not a number`);
             throw new Error('Limit is not a number');
         } else if (Number.isInteger(options.limit) && options.limit < 0) {
-            this.logger.error(`Failed FindassertionsByKeyword: ${options.limit} is negative number`);
+            this.logger.error(`Failed creating Limit query: ${options.limit} is negative number`);
             throw new Error('Limit is not a number');
         }
         return options.limit ? `LIMIT ${options.limit}` : '';
