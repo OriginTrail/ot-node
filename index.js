60eddf674bc3fe249177d247c8cda3a61eda364b
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2022-04-01 12:15:36 +0200

    Updated user configuration initialization

--- index.js
+++ index.js
@@ -17,13 +17,17 @@ const defaultConfig = JSON.parse(JSON.stringify(configjson[process.env.NODE_ENV]
 config = rc(pjson.name, defaultConfig);
 
 (async () => {
+    let userConfig = null;
     try {
-        let userConfig = null;
         if (process.env.NODE_ENV === 'development' && process.argv.length === 3) {
             const configurationFilename = process.argv[2];
             userConfig = JSON.parse(fs.readFileSync(process.argv[2]));
             userConfig.configFilename = configurationFilename;
         }
+    } catch (error) {
+        console.log('Unable to read user configuration from file: ', process.argv[2]);
+    }
+    try {
         const node = new OTNode(userConfig);
         await node.start();
     } catch (e) {
