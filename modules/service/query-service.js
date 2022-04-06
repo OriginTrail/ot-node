efe370a3cefa2f673e04ae0fdb902015d8c8e1da
zeroxbt <original.swazi@protonmail.com>
2022-04-06 13:37:08 +0200

    add fix to handle search result

--- modules/service/query-service.js
+++ modules/service/query-service.js
@@ -194,7 +194,7 @@ class QueryService {
             }
 
             const rawNquads = assertion.nquads ? assertion.nquads : assertion.rdf;
-            const jsonld = await this.dataService.createAssertion(rawNquads);
+            const {jsonld} = await this.dataService.createAssertion(rawNquads);
             let object = handlerData.find(
                 (x) => x.type === jsonld.metadata.type && x.id === jsonld.metadata.UALs[0],
             );
