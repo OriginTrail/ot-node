d7023177f53952d9cffb965440a7784b6978398a
zeroxbt <original.swazi@protonmail.com>
2022-04-18 14:05:53 +0200

    remove graphdb and add fuseki to installer

--- installer/installer.sh
+++ installer/installer.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-OS_VERSION=$(lsb_release -sr)
-GRAPHDB_FILE=$(ls /root/graphdb*.zip)
-GRAPHDB_DIR=$(echo $GRAPHDB_FILE | sed 's|-dist.zip||')
 OTNODE_DIR="/root/ot-node"
 N1=$'\n'
 GREEN='\033[0;32m'
@@ -67,59 +64,37 @@ else
 fi
 
 while true; do
-    read -p "Please select the database you would like to use: [1]GraphDB [2]Blazegraph [E]xit: " choice
+    read -p "Please select the database you would like to use: [1]Fuseki [2]Blazegraph [E]xit: " choice
     case "$choice" in
-        [1gG]* ) echo -e "GraphDB selected. Proceeding with installation."; DATABASE=graphdb; break;;
+        [1gG]* ) echo -e "Fuseki selected. Proceeding with installation."; DATABASE=fuseki; break;;
         [2bB]* ) echo -e "Blazegraph selected. Proceeding with installation."; DATABASE=blazegraph; break;;
         [Ee]* ) echo "Installer stopped by user"; exit;;
         * ) echo "Please make a valid choice and try again.";;
     esac
 done
 
-if [[ $DATABASE = "graphdb" ]]; then
+if [[ $DATABASE = "fuseki" ]]; then
     
-    echo -n "Checking that the GraphDB file is present in /root: "
+    echo -n "Downloading Apache Jena Fuseki: " 
 
-    if [[ ! -f $GRAPHDB_FILE ]]; then
+    OUTPUT=$(wget https://repo1.maven.org/maven2/org/apache/jena/jena-fuseki-server/4.4.0/jena-fuseki-server-4.4.0.jar 2>&1)
+
+    if [[ $? -ne 0 ]]; then
         echo -e "${RED}FAILED${NC}"
-        echo "The graphdb file needs to be downloaded to /root. Please create an account at https://www.ontotext.com/products/graphdb/graphdb-free/ and click the standalone version link in the email."
+        echo "There was an error downloading Fuseki."
+        echo $OUTPUT
         exit 1
     else
         echo -e "${GREEN}SUCCESS${NC}"
     fi
 
-    echo -n "Unzipping GraphDB: "
-OUTPUT=$(unzip -o $GRAPHDB_FILE >/dev/null 2>&1)
-OUTPUT=$(unzip -o $GRAPHDB_FILE 2>&1)
+    echo -n "Copying fuseki service file: "
 
-if [[ $? -ne 0 ]]; then
-    echo -e "${RED}FAILED${NC}"
-    echo "There was an error unzipping GraphDB."
-    echo $OUTPUT
-    exit 1
-else
-    echo -e "${GREEN}SUCCESS${NC}"
-fi
-
-echo -n "Rename GraphDB directory: "
-OUTPUT=$(mv $GRAPHDB_DIR graphdb-free 2>&1)
-
-if [[ $? -ne 0 ]]; then
-    echo -e "${RED}FAILED${NC}"
-    echo "There was an error unzipping GraphDB."
-    echo $OUTPUT
-    exit 1
-else
-    echo -e "${GREEN}SUCCESS${NC}"
-fi
-
-    echo -n "Copying graphdb service file: "
-
-    OUTPUT=$(cp $OTNODE_DIR/installer/data/graphdb.service /lib/systemd/system/ 2>&1)
+    OUTPUT=$(cp $OTNODE_DIR/installer/data/fuseki.service /lib/systemd/system/ 2>&1)
 
     if [[ $? -ne 0 ]]; then
         echo -e "${RED}FAILED${NC}"
-        echo "There was an error copying the graphdb service file."
+        echo "There was an error copying the fuseki service file."
         echo $OUTPUT
         exit 1
     else
@@ -128,44 +103,45 @@ fi
 
     systemctl daemon-reload
 
-    echo -n "Enable GraphDB service on boot: "
+    echo -n "Enable Fuseki service on boot: "
 
-    OUTPUT=$(systemctl enable graphdb 2>&1)
+    OUTPUT=$(systemctl enable fuseki 2>&1)
 
     if [[ $? -ne 0 ]]; then
         echo -e "${RED}FAILED${NC}"
-        echo "There was an error enabling the GraphDB service."
+        echo "There was an error enabling Fuseki."
         echo $OUTPUT
         exit 1
     else
         echo -e "${GREEN}SUCCESS${NC}"
     fi
 
-    echo -n "Starting GraphDB: "
+    echo -n "Starting Blazegraph: "
 
-    OUTPUT=$(systemctl start graphdb 2>&1)
+    OUTPUT=$(systemctl start fuseki 2>&1)
 
     if [[ $? -ne 0 ]]; then
         echo -e "${RED}FAILED${NC}"
-        echo "There was an error starting GraphDB."
+        echo "There was an error starting Fuseki."
         echo $OUTPUT
         exit 1
     else
         echo -e "${GREEN}SUCCESS${NC}"
     fi
 
-    echo -n "Confirming GraphDB has started: "
+    echo -n "Confirming Fuseki has started: "
 
-    IS_RUNNING=$(systemctl show -p ActiveState --value graphdb)
+    IS_RUNNING=$(systemctl show -p ActiveState --value fuseki)
 
     if [[ $IS_RUNNING == "active" ]]; then
         echo -e "${GREEN}SUCCESS${NC}"
     else
         echo -e "${RED}FAILED${NC}"
-        echo "There was an error starting GraphDB."
+        echo "There was an error starting Fuseki."
         echo $OUTPUT
         exit 1
     fi
+
 fi
 
 if [[ $DATABASE = "blazegraph" ]]; then
