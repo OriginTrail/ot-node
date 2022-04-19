7f96d7af7f8d06a578bd222b37c276f1dc72d27a
angrymob <hansi1337gmail.com>
2022-04-19 08:44:54 +0200

    Add missing functionality; Implement feedback from reviewers;

--- external/sparqlquery-service.js
+++ external/sparqlquery-service.js
@@ -1,7 +1,5 @@
-const axios = require('axios');
-const Engine = require('@comunica/query-sparql').QueryEngine;
-const N3 = require('n3');
 const constants = require('../modules/constants');
+const Engine = require('@comunica/query-sparql').QueryEngine;
 
 class SparqlqueryService {
     constructor(config) {
@@ -21,11 +19,12 @@ class SparqlqueryService {
         this.context = {
             sources: [{
                 type: 'sparql',
-                value: `${this.config.url}`,
+                value: `${this.config.sparqlEndpoint}`,
             }],
+            baseIRI: 'http://schema.org/',
             destination: {
                 type: 'sparql',
-                value: `${this.config.url}`,
+                value: `${this.config.sparqlEndpoint}`,
             },
             log: this.logger,
         };
@@ -58,71 +57,36 @@ class SparqlqueryService {
     }
 
     async resolve(uri) {
-        const query = `PREFIX schema: <http://schema.org/>
-                        CONSTRUCT { ?s ?p ?o }
-                        WHERE {
-                          GRAPH <${constants.DID_PREFIX}:${uri}> {
-                            ?s ?p ?o
-                          }
-                        }`;
-
-        let nquads = await this.construct(query);
-
-        const writer = new N3.Writer();
-        writer.addQuads(nquads);
-        writer.end(async (error, result) => {
-            if (nquads.length) {
-                nquads = result;
-                nquads = nquads.split('\n');
-                nquads = nquads.filter((x) => x !== '');
-                nquads = await this.transformBlankNodes(nquads);
-            } else {
-                nquads = null;
-            }
-            return nquads;
-        });
-    }
-
-    async transformBlankNodes(nquads) {
-        // Find minimum blank node value to assign it to _:c14n0
-        let minimumBlankNodeValue = -1;
-        for (const nquad of nquads) {
-            if (nquad.includes('_:t')) {
-                const blankNodes = nquad.split(' ')
-                    .filter((s) => s.includes('_:t'));
-                for (const bn of blankNodes) {
-                    const bnValue = Number(bn.substring(3));
-                    if (minimumBlankNodeValue === -1 || minimumBlankNodeValue > bnValue) {
-                        minimumBlankNodeValue = bnValue;
-                    }
-                }
-            }
-        }
-
-        // Transform blank nodes, example: _:t145 -> _:c14n3
-        let bnName;
-        for (const nquadIndex in nquads) {
-            const nquad = nquads[nquadIndex];
-            if (nquad.includes('_:t')) {
-                const blankNodes = nquad.split(' ')
-                    .filter((s) => s.includes('_:t'));
-                for (const bn of blankNodes) {
-                    const bnValue = Number(bn.substring(3));
-                    bnName = `_:c14n${bnValue - minimumBlankNodeValue}`;
-                    nquads[nquadIndex] = nquads[nquadIndex].replace(bn, bnName);
-                }
-            }
-        }
-
-        return nquads;
+        this.logger.info('to be implemented by subclass');
     }
 
     async assertionsByAsset(uri) {
-        this.logger.info('dummy for ESLint');
+        const query = `PREFIX schema: <http://schema.org/>
+            SELECT ?assertionId ?issuer ?timestamp
+            WHERE {
+                 ?assertionId schema:hasUALs "${uri}" ;
+                     schema:hasTimestamp ?timestamp ;
+                     schema:hasIssuer ?issuer .
+            }
+            ORDER BY DESC(?timestamp)`;
+        const result = await this.execute(query);
+
+        return result;
     }
 
     async findAssertions(nquads) {
-        this.logger.info('dummy for ESLint');
+        const query = `SELECT ?g
+                       WHERE {
+                            GRAPH ?g {
+                            ${nquads}
+                            }
+                       }`;
+        let graph = await this.execute(query);
+        graph = graph.results.bindings.map((x) => x.g.value.replace(`${constants.DID_PREFIX}:`, ''));
+        if (graph.length && graph[0] === 'http://www.bigdata.com/rdf#nullGraph') {
+            return [];
+        }
+        return graph;
     }
 
     async findAssertionsByKeyword(query, options, localQuery) {
@@ -171,7 +135,7 @@ class SparqlqueryService {
         const typesFilter = options.types ? this.createFilterParameter(options.types, this.filtertype.TYPES) : '';
 
         const sparqlQuery = `PREFIX schema: <http://schema.org/>
-                            SELECT ?assertionId
+                            SELECT ?assertionId ?assetId
                             WHERE {
                                 ?assertionId schema:hasTimestamp ?latestTimestamp ;
                                  ${publicVisibilityQuery}
@@ -193,16 +157,11 @@ class SparqlqueryService {
                                     }
                             }`;
         const result = await this.execute(sparqlQuery);
-        return result.map((value) => value.get('assertionId'));
+        return result;
     }
 
     async healthCheck() {
-        try {
-            const response = await axios.head(`${this.config.url}`, {});
-            return response.data !== null;
-        } catch (e) {
-            return false;
-        }
+        return true;
     }
 
     async executeQuery(query) {
@@ -239,14 +198,18 @@ class SparqlqueryService {
     }
 
     createLimitQuery(options) {
-        if (options.limit && !Number.isInteger(options.limit)) {
+        if (!options.limit) {
+            return '';
+        }
+        const queryLimit = Number(options.limit);
+        if (Number.isNaN(queryLimit) || !Number.isInteger(queryLimit)) {
             this.logger.error(`Failed creating Limit query: ${options.limit} is not a number`);
             throw new Error('Limit is not a number');
         } else if (Number.isInteger(options.limit) && options.limit < 0) {
             this.logger.error(`Failed creating Limit query: ${options.limit} is negative number`);
             throw new Error('Limit is not a number');
         }
-        return options.limit ? `LIMIT ${options.limit}` : '';
+        return `LIMIT ${queryLimit}`;
     }
 }
 
