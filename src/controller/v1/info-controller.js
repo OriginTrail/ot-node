8084a89134146c1ebafa981f3b83820f29fead47
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2022-06-24 15:59:59 +0200

    Version bump, added info route

--- src/controller/v1/info-controller.js
+++ src/controller/v1/info-controller.js
@@ -1,6 +1,12 @@
+const pjson = require('../../../package.json');
+
 class InfoController {
     handleHttpApiInfoRequest(req, res) {
+        const { version } = pjson;
 
+        res.status(200).send({
+            version,
+        });
     }
 }
 
