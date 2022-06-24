d0a0a23b54abf93ac57c3e011d9ad822038d02eb
NZT48 <nikolaztodorovic26@gmail.com>
2022-06-24 12:36:34 +0200

    Remove unused comment

--- src/commands/search/sender/search-assertions-request-command.js
+++ src/commands/search/sender/search-assertions-request-command.js
@@ -40,64 +40,6 @@ class searchAssertionsRequestCommand extends ProtocolRequestCommand {
         );
     }
 
-    /**
-     * Executes command and produces one or more events
-     * @param command
-     */
-    // async execute(command) {
-    //     const { query, handlerId, nodes, sessionIds } = command.data;
-    //
-    //     const messages = sessionIds.map((sessionId) => ({
-    //         header: {
-    //             sessionId,
-    //             messageType: NETWORK_MESSAGE_TYPES.REQUESTS.PROTOCOL_REQUEST,
-    //         },
-    //         data: { query },
-    //     }));
-    //
-    //     let failedResponses = 0;
-    //     const sendMessagePromises = nodes.map(async (node, index) => {
-    //         try {
-    //             const response = await this.networkModuleManager.sendMessage(
-    //                 NETWORK_PROTOCOLS.SEARCH_ASSERTIONS,
-    //                 node,
-    //                 messages[index],
-    //             );
-    //             if (
-    //                 !response ||
-    //                 response.header.messageType !== NETWORK_MESSAGE_TYPES.RESPONSES.ACK
-    //             )
-    //                 failedResponses += 1;
-    //         } catch (e) {
-    //             failedResponses += 1;
-    //             this.handleError(
-    //                 handlerId,
-    //                 e,
-    //                 `Error while sending search assertions request for query ${query} to node ${node._idB58String}. Error message: ${e.message}. ${e.stack}`,
-    //             );
-    //         }
-    //     });
-    //
-    //     const assertionIds = await Promise.any(sendMessagePromises);
-    //
-    //     try {
-    //         const cachedAssertionIds = await this.handlerIdService.getCachedHandlerIdData(handlerId);
-    //
-    //         // TODO: merge assertionsIds and cachedAssertionIds
-    //
-    //         await this.handlerIdService.cacheHandlerIdData(handlerId, nquads);
-    //
-    //         await this.handlerIdService.updateHandlerIdStatus(
-    //             handlerId,
-    //             HANDLER_ID_STATUS.COMPLETED,
-    //         );
-    //     } catch (e) {
-    //         await this.handlerIdService.updateFailedHandlerId(handlerId, e.message);
-    //     }
-    //
-    //     return this.continueSequence(command.data, command.sequence);
-    // }
-
     handleError(handlerId, error, msg) {
         this.logger.error({
             msg,
