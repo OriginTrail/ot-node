e828616f557366894e9c643913ba4f2a1ebd42ad
vterzic <veljkot@gmail.com>
2022-07-28 10:49:29 +0200

    Fix model association bug

--- src/modules/repository/implementation/sequelize/models/token.js
+++ src/modules/repository/implementation/sequelize/models/token.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
-            Token.hasOne(models.User, { as: 'user' });
+            Token.belongsTo(models.User, { as: 'user' });
         }
     }
     Token.init(
