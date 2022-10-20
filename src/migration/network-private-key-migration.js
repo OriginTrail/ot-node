422109fb195f0c93c9927fe58ae28624b8ebb316
zeroxbt <original.swazi@protonmail.com>
2022-10-20 15:08:41 +0200

    add try catch to migration

--- src/migration/network-private-key-migration.js
+++ src/migration/network-private-key-migration.js
@@ -9,10 +9,14 @@ class NetworkPrivateKeyMigration extends BaseMigration {
             this.config.modules?.network?.implementation?.['libp2p-service']?.config?.privateKey;
 
         if (networkPrivateKey) {
-            const directoryPath = join(this.config.appDataPath, LIBP2P_KEY_DIRECTORY);
-            const fullPath = join(directoryPath, LIBP2P_KEY_FILENAME);
-            await mkdir(directoryPath, { recursive: true });
-            await writeFile(fullPath, networkPrivateKey);
+            try {
+                const directoryPath = join(this.config.appDataPath, LIBP2P_KEY_DIRECTORY);
+                const fullPath = join(directoryPath, LIBP2P_KEY_FILENAME);
+                await mkdir(directoryPath, { recursive: true });
+                await writeFile(fullPath, networkPrivateKey);
+            } catch (error) {
+                this.logger.warn(`Unable to execute migration: ${this.migrationName}`);
+            }
         }
     }
 }
