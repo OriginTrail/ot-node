0272b0f19f0cf6eb959f91b2b64ebd6bbbe0e704
zeroxbt <89495162+zeroxbt@users.noreply.github.com>
2022-12-12 14:14:23 +0100

    fix local score calculation (#2273)
    * fix local score calculation
    
    * Develop merged
    
    Co-authored-by: Djordje Kovacevic <djordje.kovacevic@origin-trail.com>

--- src/service/service-agreement-service.js
+++ src/service/service-agreement-service.js
@@ -1,4 +1,4 @@
-import { ethers } from 'ethers';
+import { ethers, BigNumber } from 'ethers';
 
 class ServiceAgreementService {
     constructor(ctx) {
@@ -27,57 +27,51 @@ class ServiceAgreementService {
 
     async calculateScore(peerId, blockchainId, keyword, hashFunctionId) {
         const peerRecord = await this.repositoryModuleManager.getPeerRecord(peerId, blockchainId);
-        /* const keyHash = await this.validationModuleManager.callHashFunction(
-                    hashFunctionId,
-                    keyword,
-                );
-
-                 const hashFunctionName = this.validationModuleManager.getHashFunctionName(hashFunctionId);
-
-                const distanceUint8Array = this.shardingTableService.calculateDistance(
-                    peerRecord[hashFunctionName],
-                    keyHash,
-                );
-
-                // todo: store this in a more appropriate way
-                if (!this.log2PLDSFParams) {
-                    this.log2PLDSFParams = await this.blockchainModuleManager.getLog2PLDSFParams(
-                        blockchainId,
-                    );
-                }
-
-                const {
-                    distanceMappingCoefficient,
-                    stakeMappingCoefficient,
-                    multiplier,
-                    logArgumentConstant,
-                    a,
-                    stakeExponent,
-                    b,
-                    c,
-                    distanceExponent,
-                    d,
-                } = this.log2PLDSFParams;
-
-                const distanceUint256BN = BigNumber.from(distanceUint8Array);
-
-                const mappedStake = BigNumber.from(this.blockchainModuleManager.convertToWei(blockchainId, peerRecord.stake)).div(
-                    stakeMappingCoefficient,
-                );
-                const mappedDistance = distanceUint256BN.div(distanceMappingCoefficient);
-
-                const dividend = mappedStake.pow(stakeExponent).mul(a).add(b);
-                const divisor = mappedDistance.pow(distanceExponent).mul(c).add(d);
-                const score = Math.floor(
-                    multiplier * Math.log2(logArgumentConstant + dividend.toNumber() / divisor.toNumber()),
-                );  */
-        return this.blockchainModuleManager.callScoreFunction(
-            blockchainId,
-            1,
+        const keyHash = await this.validationModuleManager.callHashFunction(
             hashFunctionId,
-            peerRecord.peer_id,
             keyword,
+        );
+
+        const hashFunctionName = this.validationModuleManager.getHashFunctionName(hashFunctionId);
+
+        const distanceUint8Array = this.shardingTableService.calculateDistance(
+            peerRecord[hashFunctionName],
+            keyHash,
+        );
+
+        // todo: store this in a more appropriate way
+        if (!this.log2PLDSFParams) {
+            this.log2PLDSFParams = await this.blockchainModuleManager.getLog2PLDSFParams(
+                blockchainId,
+            );
+        }
+
+        const {
+            distanceMappingCoefficient,
+            stakeMappingCoefficient,
+            multiplier,
+            logArgumentConstant,
+            a,
+            stakeExponent,
+            b,
+            c,
+            distanceExponent,
+            d,
+        } = this.log2PLDSFParams;
+
+        const distanceUint256BN = BigNumber.from(distanceUint8Array);
+
+        const mappedStake = BigNumber.from(
             this.blockchainModuleManager.convertToWei(blockchainId, peerRecord.stake),
+        ).div(stakeMappingCoefficient);
+        const mappedDistance = distanceUint256BN.div(distanceMappingCoefficient);
+
+        const dividend = mappedStake.pow(stakeExponent).mul(a).add(b);
+        const divisor = mappedDistance.pow(distanceExponent).mul(c).add(d);
+
+        return Math.floor(
+            Number(multiplier) *
+                Math.log2(Number(logArgumentConstant) + dividend.toNumber() / divisor.toNumber()),
         );
     }
 }
