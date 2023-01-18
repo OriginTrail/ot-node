b31c1c405f4672a1920d9dc06e17abbfea2abd7e
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-01-18 10:45:00 +0100

    Updated fuseki database repository initialization

--- src/modules/triple-store/implementation/ot-fuseki/ot-fuseki.js
+++ src/modules/triple-store/implementation/ot-fuseki/ot-fuseki.js
@@ -2,6 +2,35 @@ import axios from 'axios';
 import OtTripleStore from '../ot-triple-store.js';
 
 class OtFuseki extends OtTripleStore {
+    async initialize(config, logger) {
+        await super.initialize(config, logger);
+        await Promise.all(
+            Object.keys(this.repositories).map(async (repository) => {
+                const { url, name } = this.repositories[repository];
+
+                const datasets = await axios.get(`${url}$/datasets`);
+                let exists = false;
+                for (const dataset of datasets.data.datasets) {
+                    if (dataset['ds.name'] === `/${name}`) {
+                        exists = true;
+                        break;
+                    }
+                }
+                if (!exists) {
+                    await axios.post(
+                        `${url}$/datasets?dbName=${name}&dbType=tdb`,
+                        {},
+                        {
+                            headers: {
+                                'Content-Type': 'text/plain',
+                            },
+                        },
+                    );
+                }
+            }),
+        );
+    }
+
     initializeSparqlEndpoints(repository) {
         const { url, name } = this.repositories[repository];
         this.repositories[repository].sparqlEndpoint = `${url}/${name}/sparql`;
@@ -10,7 +39,7 @@ class OtFuseki extends OtTripleStore {
 
     async healthCheck(repository) {
         try {
-            const response = await axios.get(`${this.repositories[repository].url}/$/ping`, {});
+            const response = await axios.get(`${this.repositories[repository].url}$/ping`, {});
             if (response.data !== null) {
                 return true;
             }
