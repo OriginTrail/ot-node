77539a67160edbb353c9a4fcb5ced33d959c7e4e
zeroxbt <original.swazi@protonmail.com>
2023-03-23 14:25:35 +0100

    add repository to query request schema

--- src/controllers/http-api/request-schema/query-schema.js
+++ src/controllers/http-api/request-schema/query-schema.js
@@ -1,4 +1,4 @@
-import { QUERY_TYPES } from '../../../constants/constants.js';
+import { QUERY_TYPES, TRIPLE_STORE_REPOSITORIES } from '../../../constants/constants.js';
 
 export default () => ({
     type: 'object',
@@ -9,6 +9,9 @@ export default () => ({
         },
         query: {
             type: 'string',
+        },
+        repository: {
+            enum: [Object.values(TRIPLE_STORE_REPOSITORIES)],
         },
     },
 });
