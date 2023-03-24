4131273b662290bba34da0359d9c234558ea0d9d
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-03-24 10:44:26 +0100

    Updated lns script

--- src/service/file-service.js
+++ src/service/file-service.js
@@ -105,7 +105,7 @@ class FileService {
     }
 
     getPendingStorageFileName(blockchain, contract, tokenId) {
-        return `${blockchain}_${contract}_${tokenId}`;
+        return `${blockchain}:${contract}:${tokenId}`;
     }
 
     getPendingStorageCachePath(repository) {
