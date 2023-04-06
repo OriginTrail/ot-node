22345deb98b7213578939f087cc403ef14efe7d3
marko-ot <marko.brkic@origin-trail.com>
2023-04-06 13:27:21 +0200

    Operation id service mock updated

--- test/unit/mock/operation-id-service-mock.js
+++ test/unit/mock/operation-id-service-mock.js
@@ -3,11 +3,16 @@ class OperationIdServiceMock {
         this.repositoryModuleManager = ctx.repositoryModuleManager;
     }
 
-    async cacheOperationIdData() {
+    async cacheOperationIdData(operationId, data) {
+        console.log(operationId, data);
+
         return {};
     }
 
-    async updateOperationIdStatus(operationId, status) {
+    async updateOperationIdStatus(operationId, status, errorMessage = null, errorType = null) {
+        if (errorMessage) console.log(errorMessage);
+        if (errorType) console.log(errorType);
+
         await this.repositoryModuleManager.updateOperationIdRecord(
             {
                 status,
