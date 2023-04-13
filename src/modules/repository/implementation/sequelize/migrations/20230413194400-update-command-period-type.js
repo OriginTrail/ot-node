1cd9a16b5527508cfbb575282a1d3b2857097d9d
zeroxbt <original.swazi@protonmail.com>
2023-04-13 19:52:13 +0200

    fix table name

--- src/modules/repository/implementation/sequelize/migrations/20230413194400-update-command-period-type.js
+++ src/modules/repository/implementation/sequelize/migrations/20230413194400-update-command-period-type.js
@@ -1,11 +1,11 @@
 export async function up({ context: { queryInterface, Sequelize } }) {
-    await queryInterface.changeColumn('command', 'period', {
+    await queryInterface.changeColumn('commands', 'period', {
         type: Sequelize.BIGINT,
     });
 }
 
 export async function down({ context: { queryInterface, Sequelize } }) {
-    await queryInterface.changeColumn('command', 'period', {
+    await queryInterface.changeColumn('commands', 'period', {
         type: Sequelize.BIGINT,
     });
 }
