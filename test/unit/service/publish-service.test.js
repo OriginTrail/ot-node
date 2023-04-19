9a3de1ac5b28c694a06a96b225d8b8633cb96598
Marko Brkic <mbrkic16@gmail.com>
2023-04-19 10:31:23 +0200

    Unit tests for publish, get and update services (#2419)
    * setup and create publish service test
    
    * created template for test file
    
    * Add mock services and fix imports
    
    * operation id service and repo mocks v1.0
    
    * Operation id service mock updated
    
    * Added id to mock response statuses and create res
    
    * publish service test not erroring
    
    * individual test - successful publish done (probably not perfect)
    
    * Operation status -> Operation request status
    
    * add sinon spy to spy on logger
    
    * improve succesful test with sinon logger spy
    
    * small id fix and removing some console logs
    
    * small id fix and removing some console logs
    
    * individual test - too high ACK ask passing
    
    * individual test - failed response status passing
    
    * individual test - publish leads to scheduling operation for leftover nodes
    
    * disabling linter on test functions as i need to have some unused vars
    
    * added additional checks
    
    * added caching data check for successful publish
    
    * get-service-test.js added, succesful get completes individual test done
    
    * Change test names to better address what tests do
    
    * individual test - Completed get completes with high ACK ask
    
    * individual test - get leads to scheduling operation
    
    * get tests done
    
    * all update tests done - service is same as get so copy pasted
    
    * feedback to stop validating loggers
    
    * publish tests improved - spying on actual methods rather than console.log
    
    * removed useless comments
    
    * add restore to new spy in publish test
    
    * improve get tests w spying method (not console)
    
    * improve update tests w spying method (not console)
    
    * Operation service - test for getResponseStatuses
    
    * fix eslint warnings
    
    * updated linter config
    
    * mock helper functions added
    
    * individual test - operation service validates assertion correctly
    
    * validate assertion test that fails due to assertion size mismatch
    
    * validate assertion test that fails due to triple number  mismatch
    
    * validate assertion test that fails due to chunk number mismatch
    
    * validate assertion test that fails due to mismatch in assertion id
    
    * remove console logs
    
    ---------
    
    Co-authored-by: marko_ot <brka@DESKTOP-A84HQ45>
    Co-authored-by: marko-ot <marko.brkic@origin-trail.com>
    Co-authored-by: djordjekovac <djordje.kovacevic@origin-trail.com>

---
+++ test/unit/service/publish-service.test.js
@@ -0,0 +1,153 @@
+import { beforeEach, afterEach, describe, it } from 'mocha';
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { OPERATION_REQUEST_STATUS } from '../../../src/constants/constants.js';
+import RepositoryModuleManagerMock from '../mock/repository-module-manager-mock.js';
+import ValidationModuleManagerMock from '../mock/validation-module-manager-mock.js';
+import BlockchainModuleManagerMock from '../mock/blockchain-module-manager-mock.js';
+import OperationIdServiceMock from '../mock/operation-id-service-mock.js';
+import CommandExecutorMock from '../mock/command-executor-mock.js';
+import PublishService from '../../../src/service/publish-service.js';
+import Logger from '../../../src/logger/logger.js';
+
+let publishService;
+let cacheOperationIdDataSpy;
+let commandExecutorAddSpy;
+
+describe('Publish service test', async () => {
+    beforeEach(() => {
+        const repositoryModuleManagerMock = new RepositoryModuleManagerMock();
+
+        publishService = new PublishService({
+            repositoryModuleManager: repositoryModuleManagerMock,
+            operationIdService: new OperationIdServiceMock({
+                repositoryModuleManager: repositoryModuleManagerMock,
+            }),
+            commandExecutor: new CommandExecutorMock(),
+            validationModuleManager: new ValidationModuleManagerMock(),
+            blockchainModuleManager: new BlockchainModuleManagerMock(),
+            logger: new Logger(),
+        });
+        cacheOperationIdDataSpy = sinon.spy(
+            publishService.operationIdService,
+            'cacheOperationIdData',
+        );
+        commandExecutorAddSpy = sinon.spy(publishService.commandExecutor, 'add');
+    });
+
+    afterEach(() => {
+        cacheOperationIdDataSpy.restore();
+        commandExecutorAddSpy.restore();
+    });
+
+    it('Completed publish completes with low ACK ask', async () => {
+        await publishService.processResponse(
+            {
+                data: {
+                    operationId: '5195d01a-b437-4aae-b388-a77b9fa715f1',
+                    numberOfFoundNodes: 1,
+                    leftoverNodes: [],
+                    keyword: 'origintrail',
+                    batchSize: 10,
+                    minAckResponses: 1,
+                },
+            },
+            OPERATION_REQUEST_STATUS.COMPLETED,
+            {},
+        );
+
+        const returnedResponses = publishService.repositoryModuleManager.getAllResponseStatuses();
+
+        expect(cacheOperationIdDataSpy.calledWith('5195d01a-b437-4aae-b388-a77b9fa715f1', {})).to.be
+            .true;
+
+        expect(returnedResponses.length).to.be.equal(2);
+
+        expect(
+            returnedResponses[returnedResponses.length - 1].status ===
+                OPERATION_REQUEST_STATUS.COMPLETED,
+        ).to.be.true;
+    });
+
+    it('Completed publish fails with high ACK ask', async () => {
+        await publishService.processResponse(
+            {
+                data: {
+                    operationId: '5195d01a-b437-4aae-b388-a77b9fa715f1',
+                    numberOfFoundNodes: 1,
+                    leftoverNodes: [],
+                    keyword: 'origintrail',
+                    batchSize: 10,
+                    minAckResponses: 12,
+                },
+            },
+            OPERATION_REQUEST_STATUS.COMPLETED,
+            {},
+        );
+
+        const returnedResponses = publishService.repositoryModuleManager.getAllResponseStatuses();
+
+        expect(returnedResponses.length).to.be.equal(2);
+
+        expect(
+            returnedResponses[returnedResponses.length - 1].status ===
+                OPERATION_REQUEST_STATUS.FAILED,
+        ).to.be.true;
+    });
+
+    it('Failed publish fails with low ACK ask', async () => {
+        await publishService.processResponse(
+            {
+                data: {
+                    operationId: '5195d01a-b437-4aae-b388-a77b9fa715f1',
+                    numberOfFoundNodes: 1,
+                    leftoverNodes: [],
+                    keyword: 'origintrail',
+                    batchSize: 10,
+                    minAckResponses: 1,
+                },
+            },
+            OPERATION_REQUEST_STATUS.FAILED,
+            {},
+        );
+
+        const returnedResponses = publishService.repositoryModuleManager.getAllResponseStatuses();
+
+        expect(returnedResponses.length).to.be.equal(2);
+
+        expect(
+            returnedResponses[returnedResponses.length - 1].status ===
+                OPERATION_REQUEST_STATUS.FAILED,
+        ).to.be.true;
+    });
+
+    it('Completed publish leads to scheduling operation for leftover nodes and status stays same', async () => {
+        await publishService.processResponse(
+            {
+                data: {
+                    operationId: '5195d01a-b437-4aae-b388-a77b9fa715f1',
+                    numberOfFoundNodes: 1,
+                    leftoverNodes: [1, 2, 3, 4],
+                    keyword: 'origintrail',
+                    batchSize: 10,
+                    minAckResponses: 12,
+                },
+            },
+            OPERATION_REQUEST_STATUS.COMPLETED,
+            {},
+        );
+
+        const returnedResponses = publishService.repositoryModuleManager.getAllResponseStatuses();
+
+        expect(returnedResponses.length).to.be.equal(2);
+
+        expect(
+            commandExecutorAddSpy.calledWith('5195d01a-b437-4aae-b388-a77b9fa715f1', [1, 2, 3, 4]),
+        );
+
+        expect(
+            returnedResponses[returnedResponses.length - 1].status ===
+                OPERATION_REQUEST_STATUS.COMPLETED,
+        ).to.be.true;
+    });
+});
