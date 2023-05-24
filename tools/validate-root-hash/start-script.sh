1b69e0c24c47d0b96f6601d07cec84f951a8ff33
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-05-24 12:06:26 +0200

    Start script added

---
+++ tools/validate-root-hash/start-script.sh
@@ -0,0 +1,5 @@
+node validate-root-hash.js 0 50000 https://proxima-node-30.origin-trail.network
+node validate-root-hash.js 50000 100000 https://proxima-node-29.origin-trail.network
+node validate-root-hash.js 100000 150000 https://proxima-node-28.origin-trail.network
+node validate-root-hash.js 150000 200000 https://proxima-node-27.origin-trail.network
+node validate-root-hash.js 200000 latest https://proxima-node-26.origin-trail.network
