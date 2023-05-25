3de3bfdcdba10ff577c6388412df17e85058fec3
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-05-25 11:53:25 +0200

    Updated info route to return aset sync metrics

--- src/controllers/http-api/info-http-api-controller.js
+++ src/controllers/http-api/info-http-api-controller.js
@@ -7,46 +7,53 @@ const { version } = require('../../../package.json');
 class InfoController extends BaseController {
     constructor(ctx) {
         super(ctx);
-
+        this.config = ctx.config;
         this.blockchainModuleManager = ctx.blockchainModuleManager;
         this.repositoryModuleManager = ctx.repositoryModuleManager;
     }
 
-    handleInfoRequest(req, res) {
+    async handleInfoRequest(req, res) {
         const response = {
             version,
         };
         response.assetSyncEnabled = this.config.assetSync.enabled;
         if (response.assetSyncEnabled) {
             response.assetSyncStatus = [];
-            this.blockchainModuleManager.getImplementationNames().map(async (blockchain) => {
-                this.blockchainModuleManager
-                    .getAssetStorageAddresses(blockchain)
-                    .map(async (assetContractAddress) => {
-                        try {
-                            const latestCreatedKnowledgeAssetTokenId =
-                                await this.blockchainModuleManager.getLatestTokenId(
-                                    blockchain,
-                                    assetContractAddress,
-                                );
-                            const latestSyncedKnowledgeAssetTokenId =
-                                await this.repositoryModuleManager.getLatestAssetSyncRecord(
-                                    blockchain,
-                                    assetContractAddress,
-                                );
-                            response.assetSyncStatus.push({
-                                blockchain,
-                                assetContractAddress,
-                                latestCreatedKnowledgeAssetTokenId,
-                                latestSyncedKnowledgeAssetTokenId,
-                            });
-                        } catch (error) {
-                            this.logger.warn(
-                                `Error while trying to get asset sync status for blockchain: ${blockchain} and contract: ${assetContractAddress}`,
-                            );
-                        }
-                    });
-            });
+            await Promise.all(
+                this.blockchainModuleManager.getImplementationNames().map(async (blockchain) => {
+                    await Promise.all(
+                        this.blockchainModuleManager
+                            .getAssetStorageAddresses(blockchain)
+                            .map(async (assetContractAddress) => {
+                                try {
+                                    const latestCreatedKnowledgeAssetTokenId =
+                                        await this.blockchainModuleManager.getLatestTokenId(
+                                            blockchain,
+                                            assetContractAddress,
+                                        );
+                                    const latestSyncedKnowledgeAsset =
+                                        await this.repositoryModuleManager.getLatestAssetSyncRecord(
+                                            blockchain,
+                                            assetContractAddress,
+                                        );
+                                    response.assetSyncStatus.push({
+                                        blockchain,
+                                        assetContractAddress,
+                                        latestCreatedKnowledgeAssetTokenId: Number(
+                                            latestCreatedKnowledgeAssetTokenId,
+                                        ),
+                                        latestSyncedKnowledgeAssetTokenId:
+                                            latestSyncedKnowledgeAsset.tokenId,
+                                    });
+                                } catch (error) {
+                                    this.logger.warn(
+                                        `Error while trying to get asset sync status for blockchain: ${blockchain} and contract: ${assetContractAddress}`,
+                                    );
+                                }
+                            }),
+                    );
+                }),
+            );
         }
 
         this.returnResponse(res, 200, response);
