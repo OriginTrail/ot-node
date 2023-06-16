367b950ad55ed3012597afd32459a80de445ecc5
djordjekovac <djordje.kovacevic@origin-trail.com>
2023-06-16 11:05:19 +0200

    Operational db data archive (#2579)
    * Added blockchain event cleaner command
    
    * Added operation response cleanup commands
    
    * Updated cleaner commands

---
+++ src/commands/cleaners/publish-response-cleaner-command.js
@@ -0,0 +1,51 @@
+import CleanerCommand from './cleaner-command.js';
+import {
+    REPOSITORY_ROWS_FOR_REMOVAL_MAX_NUMBER,
+    OPERATIONS,
+    PUBLISH_RESPONSE_CLEANUP_TIME_DELAY,
+    PUBLISH_RESPONSE_CLEANUP_TIME_MILLS,
+    ARCHIVE_PUBLISH_RESPONSES_FOLDER,
+} from '../../constants/constants.js';
+
+class PublishResponseCleanerCommand extends CleanerCommand {
+    constructor(ctx) {
+        super(ctx);
+        this.logger = ctx.logger;
+        this.repositoryModuleManager = ctx.repositoryModuleManager;
+        this.archiveService = ctx.archiveService;
+    }
+
+    async findRowsForRemoval(nowTimestamp) {
+        return this.repositoryModuleManager.findProcessedOperationResponse(
+            nowTimestamp - PUBLISH_RESPONSE_CLEANUP_TIME_DELAY,
+            REPOSITORY_ROWS_FOR_REMOVAL_MAX_NUMBER,
+            OPERATIONS.PUBLISH,
+        );
+    }
+
+    getArchiveFolderName() {
+        return ARCHIVE_PUBLISH_RESPONSES_FOLDER;
+    }
+
+    async deleteRows(ids) {
+        return this.repositoryModuleManager.removeOperationResponse(ids, OPERATIONS.PUBLISH);
+    }
+
+    /**
+     * Builds default command
+     * @param map
+     * @returns {{add, data: *, delay: *, deadline: *}}
+     */
+    default(map) {
+        const command = {
+            name: 'publishResponseCleanerCommand',
+            data: {},
+            period: PUBLISH_RESPONSE_CLEANUP_TIME_MILLS,
+            transactional: false,
+        };
+        Object.assign(command, map);
+        return command;
+    }
+}
+
+export default PublishResponseCleanerCommand;
