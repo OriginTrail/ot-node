4b60f5af5d549567e7bf93c6e5839812d22c5121
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-07-06 11:34:23 +0200

    Log updated

--- test/bdd/steps/blockchain.mjs
+++ test/bdd/steps/blockchain.mjs
@@ -4,7 +4,7 @@ import fs from 'fs';
 import LocalBlockchain from './lib/local-blockchain.mjs';
 
 Given(/^the blockchain is set up$/, { timeout: 240_000 }, function blockchainSetup(done) {
-    this.logger.log('Starting blockchain');
+    this.logger.log('Starting blockchain!');
     expect(this.state.localBlockchain, "localBlockchain shouldn't be defined").to.be.equal(null);
     const blockchainConsole = new console.Console(
         fs.createWriteStream(`${this.state.scenarionLogDir}/blockchain.log`),
