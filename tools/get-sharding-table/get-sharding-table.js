2f92226485e6f4cd3db95c4c84e7f141f3dace7a
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-09-05 09:48:32 +0200

    Added get sharding table script

---
+++ tools/get-sharding-table/get-sharding-table.js
@@ -0,0 +1,9 @@
+if (!process.argv[2]) {
+    console.log('Missing argument BLOCKCHAIN');
+    console.log('Usage: npm run get-sharding-table (testnet, mainnet)');
+    process.exit(1);
+}
+
+(async () => {
+    // initialize blockchain manager and sharding table
+})();
