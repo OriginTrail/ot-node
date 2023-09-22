51c722727de5aaec37447e582d634b53c11a763b
Djordje Kovacevic <djordje.kovacevic@origin-trail.com>
2023-09-22 09:41:45 +0200

    Typo fixed

--- src/modules/http-client/implementation/middleware/authorization-middleware.js
+++ src/modules/http-client/implementation/middleware/authorization-middleware.js
@@ -6,7 +6,7 @@ const getToken = (req) => {
 
 export default (authService) => async (req, res, next) => {
     const urlElements = req.url.split('/');
-    const operation = urlElements[urlElements.length() - 1].split('?')[0].toUpperCase();
+    const operation = urlElements[urlElements.length - 1].split('?')[0].toUpperCase();
 
     if (authService.isPublicOperation(operation)) {
         return next();
