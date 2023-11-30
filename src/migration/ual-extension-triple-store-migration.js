6c9e5024258c50cc11df938c65fe444303034dd3
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2023-11-30 11:59:55 +0100

    Execute triple store migration in chunks

--- src/migration/ual-extension-triple-store-migration.js
+++ src/migration/ual-extension-triple-store-migration.js
@@ -1,4 +1,5 @@
 import BaseMigration from './base-migration.js';
+import { TRIPLE_STORE_REPOSITORIES } from '../constants/constants.js';
 
 const CHAIN_IDS = {
     development: 31337,
@@ -18,39 +19,84 @@ class UalExtensionTripleStoreMigration extends BaseMigration {
     async executeMigration() {
         const oldBlockchainId = this.getOldBlockchainId();
         const newBlockchainId = `${oldBlockchainId}:${chainId}`;
+
+        const chunkSize = 5000;
+
+        const totalSubjectsQuery = `
+          SELECT (COUNT(*) AS ?totalObjects)
+          WHERE {
+            GRAPH <assets:graph> {
+              ?s ?p ?o .
+              FILTER (STRSTARTS(STR(?s), "did:dkg:${oldBlockchainId}/"))
+            }
+          }`;
         const updateSubjectQuery = `
-                    WITH <assets:graph>
-                    DELETE {
-                      ?s ?p ?o
-                    }
-                    INSERT {
-                      ?newSubject ?p ?o
-                    }
-                    WHERE {
-                      ?s ?p ?o .
-                      FILTER (STRSTARTS(STR(?s), "did:dkg:${oldBlockchainId}/"))
-                      BIND (IRI(REPLACE(STR(?s), "${oldBlockchainId}", "${newBlockchainId}")) AS ?newSubject)
-                    }
-        `;
-
-        await this.tripleStoreService.queryVoidAllRepositories(updateSubjectQuery);
-
-        const updateObjectQuery = `
         WITH <assets:graph>
             DELETE {
               ?s ?p ?o
             }
             INSERT {
-              ?s ?p ?newObject
+              ?newSubject ?p ?o
             }
             WHERE {
-              ?s ?p ?o .
-              FILTER(STRENDS(STR(?p), "blockchain"))
-              BIND ("${newBlockchainId}" AS ?newObject)
-            } 
-        `;
+              {
+                SELECT ?s ?p ?o (IRI(REPLACE(STR(?s), "${oldBlockchainId}", "${newBlockchainId}")) AS ?newSubject)
+                WHERE {
+                  ?s ?p ?o .
+                  FILTER (STRSTARTS(STR(?s), "did:dkg:${oldBlockchainId}/"))
+                }
+                LIMIT ${chunkSize}
+              }
+            }
+          `;
+        const updateObjectQuery = `
+        WITH <assets:graph>
+        DELETE {
+          ?s ?p ?o
+        }
+        INSERT {
+          ?s ?p "${newBlockchainId}" .
+        }
+        WHERE {
+          SELECT ?s ?p ?o
+          WHERE {
+            ?s ?p ?o .
+            FILTER(STRENDS(STR(?p), "blockchain"))
+          }
+          LIMIT ${chunkSize}
+        }
+          `;
+        for (const repository in TRIPLE_STORE_REPOSITORIES) {
+            // eslint-disable-next-line no-await-in-loop
+            const totalSubjectsResult = await this.tripleStoreService.select(
+                TRIPLE_STORE_REPOSITORIES[repository],
+                totalSubjectsQuery,
+            );
+            const totalSubjects = parseInt(
+                totalSubjectsResult[0].totalObjects.match(
+                    /"(\d+)"\^\^http:\/\/www.w3.org\/2001\/XMLSchema#integer/,
+                )[1],
+                10,
+            );
+            let offset = 0;
+            if (totalSubjects !== 0) {
+                do {
+                    // eslint-disable-next-line no-await-in-loop
+                    await this.tripleStoreService.queryVoid(
+                        TRIPLE_STORE_REPOSITORIES[repository],
+                        updateSubjectQuery,
+                    );
 
-        await this.tripleStoreService.queryVoidAllRepositories(updateObjectQuery);
+                    // eslint-disable-next-line no-await-in-loop
+                    await this.tripleStoreService.queryVoid(
+                        TRIPLE_STORE_REPOSITORIES[repository],
+                        updateObjectQuery,
+                    );
+
+                    offset += chunkSize;
+                } while (offset < totalSubjects);
+            }
+        }
     }
 
     getOldBlockchainId() {
