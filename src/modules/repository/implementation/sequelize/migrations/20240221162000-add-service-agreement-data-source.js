7798f45f77670493d86f047c2bf71dc47e14a69e
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-02-21 17:35:58 +0100

    Fix constant import in sql migration

--- src/modules/repository/implementation/sequelize/migrations/20240221162000-add-service-agreement-data-source.js
+++ src/modules/repository/implementation/sequelize/migrations/20240221162000-add-service-agreement-data-source.js
@@ -1,4 +1,4 @@
-import { SERVICE_AGREEMENT_SOURCES } from '../../../../../constants/constants';
+import { SERVICE_AGREEMENT_SOURCES } from '../../../../../constants/constants.js';
 
 export async function up({ context: { queryInterface, Sequelize } }) {
     await queryInterface.addColumn('service_agreement', 'data_source', {
