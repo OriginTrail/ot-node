674c6b5165f5c779d35bd52ef497c3b8fef729db
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-02-28 12:29:43 +0100

    Add paralelization

--- src/migration/get-old-service-agreements-migration.js
+++ src/migration/get-old-service-agreements-migration.js
@@ -1,6 +1,8 @@
 import BaseMigration from './base-migration.js';
 import { NODE_ENVIRONMENTS, SERVICE_AGREEMENT_SOURCES } from '../constants/constants.js';
 
+const BATCH_SIZE = 50;
+
 class GetOldServiceAgreementsMigration extends BaseMigration {
     constructor(
         migrationName,
@@ -32,64 +34,100 @@ class GetOldServiceAgreementsMigration extends BaseMigration {
                         0,
                         gnosisBlockchainImplementation,
                     );
+                const existinTokenIds = existingServiceAgreements.map(
+                    (serviceAgreement) => serviceAgreement.tokenId,
+                );
 
-                let tokenIdToBeFetched = 0;
-                let i = 0;
-                const missingAgreement = [];
-                while (i < existingServiceAgreements.lenght) {
-                    if (tokenIdToBeFetched < existingServiceAgreements[i].tokenId) {
-                        // eslint-disable-next-line no-await-in-loop
-                        const assertionIds = await this.blockchainModuleManager.getAssertionIds(
-                            gnosisBlockchainImplementation,
-                            // TODO: Add contract
-                            contract,
-                            tokenIdToBeFetched,
-                        );
-                        const keyword = this.blockchainModuleManager.encodePacked(
-                            gnosisBlockchainImplementation,
-                            ['address', 'bytes32'],
-                            [contract, assertionIds[0]],
-                        );
-                        const agreementId = this.serviceAgreementService.generateId(
-                            gnosisBlockchainImplementation,
-                            contract,
-                            tokenIdToBeFetched,
-                            keyword,
-                            1,
-                        );
-
-                        // eslint-disable-next-line no-await-in-loop
-                        const agreementData = await this.blockchainModuleManager.getAgreementData(
-                            gnosisBlockchainImplementation,
-                            agreementId,
-                        );
-                        // TODO: Use ...agreementData
-                        missingAgreement.push({
-                            blockchainId: gnosisBlockchainImplementation,
-                            assetStorageContractAddress: contract,
-                            tokenId: tokenIdToBeFetched,
-                            agreementId,
-                            startTime: agreementData.startTime,
-                            epochsNumber: agreementData.epochsNumber,
-                            epochLength: agreementData.epochLength,
-                            scoreFunctionId: agreementData.scoreFunctionId,
-                            stateIndex: 0,
-                            assertionId: assertionIds[0],
-                            hashFunctionId: 1,
-                            keyword,
-                            proofWindowOffsetPerc: agreementData.proofWindowOffsetPerc,
-                            dataSource: SERVICE_AGREEMENT_SOURCES.BLOCKCHAIN,
-                        });
-
-                        tokenIdToBeFetched += 1;
-                    } else {
-                        i += 1;
+                const missingTokenIds = [];
+                let expectedTokenId = 0;
+                existinTokenIds.forEach((tokenId) => {
+                    while (tokenId > expectedTokenId) {
+                        missingTokenIds.push(expectedTokenId);
+                        expectedTokenId += 1;
                     }
-                }
+                    expectedTokenId += 1;
+                });
 
-                this.repositoryModuleManager.bulkCreateServiceAgreementRecords(missingAgreement);
+                let batchNumber = 0;
+                // Check < or <= condition
+                while (batchNumber * BATCH_SIZE < existingServiceAgreements.lenght) {
+                    const promises = [];
+                    const missingAgreements = [];
+                    for (
+                        let i = batchNumber * BATCH_SIZE;
+                        i < (batchNumber + 1) * BATCH_SIZE;
+                        i += 1
+                    ) {
+                        const tokenIdToBeFetched = missingTokenIds[i];
+                        promises.push(
+                            this.getAndProccessMisingServiceAgreement(
+                                tokenIdToBeFetched,
+                                gnosisBlockchainImplementation,
+                                contract,
+                                missingAgreements,
+                            ),
+                        );
+                    }
+
+                    // eslint-disable-next-line no-await-in-loop
+                    await Promise.all(promises);
+
+                    // eslint-disable-next-line no-await-in-loop
+                    await this.repositoryModuleManager.bulkCreateServiceAgreementRecords(
+                        missingAgreements,
+                    );
+                    batchNumber += 1;
+                }
             }
         }
+    }
+
+    async getAndProccessMisingServiceAgreement(
+        tokenIdToBeFetched,
+        gnosisBlockchainImplementation,
+        contract,
+        missingAgreements,
+    ) {
+        const assertionIds = await this.blockchainModuleManager.getAssertionIds(
+            gnosisBlockchainImplementation,
+            // TODO: Add contract
+            contract,
+            tokenIdToBeFetched,
+        );
+        const keyword = this.blockchainModuleManager.encodePacked(
+            gnosisBlockchainImplementation,
+            ['address', 'bytes32'],
+            [contract, assertionIds[0]],
+        );
+        const agreementId = this.serviceAgreementService.generateId(
+            gnosisBlockchainImplementation,
+            contract,
+            tokenIdToBeFetched,
+            keyword,
+            1,
+        );
+
+        const agreementData = await this.blockchainModuleManager.getAgreementData(
+            gnosisBlockchainImplementation,
+            agreementId,
+        );
+        // TODO: Use ...agreementData
+        missingAgreements.push({
+            blockchainId: gnosisBlockchainImplementation,
+            assetStorageContractAddress: contract,
+            tokenId: tokenIdToBeFetched,
+            agreementId,
+            startTime: agreementData.startTime,
+            epochsNumber: agreementData.epochsNumber,
+            epochLength: agreementData.epochLength,
+            scoreFunctionId: agreementData.scoreFunctionId,
+            stateIndex: 0,
+            assertionId: assertionIds[0],
+            hashFunctionId: 1,
+            keyword,
+            proofWindowOffsetPerc: agreementData.proofWindowOffsetPerc,
+            dataSource: SERVICE_AGREEMENT_SOURCES.BLOCKCHAIN,
+        });
     }
 }
 
