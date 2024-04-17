23fb4d84a9a9300f3620b3a8f8799e8cb1cd6bc9
Aleksa Savic <savicaleksa83@gmail.com>
2024-04-17 12:06:44 +0200

    Refactor syncAsset a bit

--- src/commands/paranet/paranet-sync-command.js
+++ src/commands/paranet/paranet-sync-command.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-await-in-loop */
 import Command from '../command.js';
-import { ERROR_TYPE, PARANET_SYNC_FREQUENCY_MILLS } from '../../constants/constants.js';
+import { ERROR_TYPE, PARANET_SYNC_FREQUENCY_MILLS, OPERATION_ID_STATUS, CONTENT_ASSET_HASH_FUNCTION_ID } from '../../constants/constants.js';
 
 class StartParanetSyncCommands extends Command {
     constructor(ctx) {
@@ -12,168 +14,177 @@ class StartParanetSyncCommands extends Command {
     }
 
     async execute(command) {
-        const { operationId, paranetId } = command.data;
+        const { commandOperationId, paranetId } = command.data;
 
         this.logger.info(
-            `Paranet sync: Starting paranet sync command for ${paranetId} with operation id: ${operationId}`,
+            `Paranet sync: Starting paranet sync command for ${paranetId} with operation id: ${commandOperationId}`,
         );
 
         // get missed token ids for paranet
-
         // schedule get commands for each asset
-
         // store in paranet repository
+
+
+        // TODO: How do I get the params?
+        let blockchain = '';
+        let contract = '';
+        let tokenId = '';
+
+        const assertionIds = await this.blockchainModuleManager.getLatestAssertionId(
+            blockchain,
+            contract,
+            tokenId,
+        );
+
+        // Go through all except the last one
+        for (let stateIndex = assertionIds.length - 2; stateIndex > 0; stateIndex -= 1) {
+            // TODO: Also pass in where to which repo to move it
+            await this.syncAsset(blockchain, contract, tokenId, assertionIds, stateIndex);
+        }
+
+        // Then sync the last one, but put it in 'current' repo
 
         return Command.repeat();
     }
 
-    // async syncAsset(tokenId, blockchain, contract) {
-    //     const assertionIds = await this.blockchainModuleManager.getLatestAssertionId(
-    //         blockchain,
-    //         contract,
-    //         tokenId,
-    //     );
-    //
-    //     // eslint-disable-next-line for-direction
-    //     for (let stateIndex = assertionIds.length - 1; stateIndex < 0; stateIndex -= 1) {
-    //         try {
-    //             // if (
-    //             //     await this.repositoryModuleManager.isStateSynced(
-    //             //         blockchain,
-    //             //         contract,
-    //             //         tokenId,
-    //             //         stateIndex,
-    //             //     )
-    //             // ) {
-    //             //     this.logger.trace(
-    //             //         `ASSET_SYNC: StateIndex: ${stateIndex} for tokenId: ${tokenId} already synced blockchain: ${blockchain}`,
-    //             //     );
-    //             //     await this.repositoryModuleManager.updateAssetSyncRecord(
-    //             //         blockchain,
-    //             //         contract,
-    //             //         tokenId,
-    //             //         stateIndex,
-    //             //         ASSET_SYNC_PARAMETERS.STATUS.COMPLETED,
-    //             //         true,
-    //             //     );
-    //             //     continue;
-    //             // }
-    //
-    //             const statePresentInParanetRepository = await this.tripleStoreService.assetExists(
-    //                 paranet,
-    //                 tokenId,
-    //                 stateIndex,
-    //                 assertionIds,
-    //             );
-    //
-    //             if (await this.isStatePresentInRepository(tokenId, stateIndex, assertionIds)) {
-    //                 this.logger.trace(
-    //                     `ASSET_SYNC: StateIndex: ${stateIndex} for tokenId: ${tokenId} found in triple store blockchain: ${blockchain}`,
-    //                 );
-    //                 await this.repositoryModuleManager.createAssetSyncRecord(
-    //                     blockchain,
-    //                     contract,
-    //                     tokenId,
-    //                     stateIndex,
-    //                     ASSET_SYNC_PARAMETERS.STATUS.COMPLETED,
-    //                     true,
-    //                 );
-    //                 continue;
-    //             }
-    //
-    //             const ual = this.ualService.deriveUAL(blockchain, contract, tokenId);
-    //             this.logger.debug(
-    //                 `ASSET_SYNC: Fetching state index: ${stateIndex + 1} of ${
-    //                     assertionIds.length
-    //                 } for asset with ual: ${ual}. blockchain: ${blockchain}`,
-    //             );
-    //             const assertionId = assertionIds[stateIndex];
-    //
-    //             const operationId = await this.operationIdService.generateOperationId(
-    //                 OPERATION_ID_STATUS.GET.GET_START,
-    //             );
-    //
-    //             await Promise.all([
-    //                 this.operationIdService.updateOperationIdStatus(
-    //                     operationId,
-    //                     blockchain,
-    //                     OPERATION_ID_STATUS.GET.GET_INIT_START,
-    //                 ),
-    //
-    //                 this.repositoryModuleManager.createAssetSyncRecord(
-    //                     blockchain,
-    //                     contract,
-    //                     tokenId,
-    //                     stateIndex,
-    //                     ASSET_SYNC_PARAMETERS.STATUS.IN_PROGRESS,
-    //                 ),
-    //
-    //                 this.repositoryModuleManager.createOperationRecord(
-    //                     this.getService.getOperationName(),
-    //                     operationId,
-    //                     OPERATION_STATUS.IN_PROGRESS,
-    //                 ),
-    //             ]);
-    //
-    //             const hashFunctionId = CONTENT_ASSET_HASH_FUNCTION_ID;
-    //
-    //             this.logger.debug(
-    //                 `ASSET_SYNC: Get for ${ual} with operation id ${operationId} initiated. blockchain: ${blockchain}`,
-    //             );
-    //
-    //             await this.commandExecutor.add({
-    //                 name: 'networkGetCommand',
-    //                 sequence: [],
-    //                 delay: 0,
-    //                 data: {
-    //                     operationId,
-    //                     id: ual,
-    //                     blockchain,
-    //                     contract,
-    //                     tokenId,
-    //                     state: assertionId,
-    //                     hashFunctionId,
-    //                     assertionId,
-    //                     assetSync: true,
-    //                     stateIndex,
-    //                     assetSyncInsertedByCommand: true,
-    //                 },
-    //                 transactional: false,
-    //             });
-    //
-    //             await this.operationIdService.updateOperationIdStatus(
-    //                 operationId,
-    //                 blockchain,
-    //                 OPERATION_ID_STATUS.GET.GET_INIT_END,
-    //             );
-    //
-    //             let attempt = 0;
-    //             let getResult;
-    //             do {
-    //                 await setTimeout(ASSET_SYNC_PARAMETERS.GET_RESULT_POLLING_INTERVAL_MILLIS);
-    //
-    //                 getResult = await this.operationIdService.getOperationIdRecord(operationId);
-    //                 attempt += 1;
-    //             } while (
-    //                 attempt < ASSET_SYNC_PARAMETERS.GET_RESULT_POLLING_MAX_ATTEMPTS &&
-    //                 getResult?.status !== OPERATION_ID_STATUS.FAILED &&
-    //                 getResult?.status !== OPERATION_ID_STATUS.COMPLETED
-    //             );
-    //         } catch (error) {
-    //             this.logger.warn(
-    //                 `ASSET_SYNC: Unable to sync tokenId: ${tokenId}, for contract: ${contract} state index: ${stateIndex} blockchain: ${blockchain}, error: ${error}`,
-    //             );
-    //             await this.repositoryModuleManager.updateAssetSyncRecord(
-    //                 blockchain,
-    //                 contract,
-    //                 tokenId,
-    //                 stateIndex,
-    //                 ASSET_SYNC_PARAMETERS.STATUS.FAILED,
-    //                 true,
-    //             );
-    //         }
-    //     }
-    // }
+    async syncAsset(blockchain, contract, tokenId, assertionIds, stateIndex) {
+        try {
+            if (
+                await this.repositoryModuleManager.isStateSynced(
+                    blockchain,
+                    contract,
+                    tokenId,
+                    stateIndex,
+                )
+            ) {
+                this.logger.trace(
+                    `ASSET_SYNC: StateIndex: ${stateIndex} for tokenId: ${tokenId} already synced blockchain: ${blockchain}`,
+                );
+                await this.repositoryModuleManager.updateAssetSyncRecord(
+                    blockchain,
+                    contract,
+                    tokenId,
+                    stateIndex,
+                    ASSET_SYNC_PARAMETERS.STATUS.COMPLETED,
+                    true,
+                );
+                return;
+            }
+
+            const statePresentInParanetRepository = await this.tripleStoreService.assetExists(
+                paranetId,
+                tokenId,
+                stateIndex,
+                assertionIds,
+            );
+
+            if (statePresentInParanetRepository) {
+                this.logger.trace(
+                    `ASSET_SYNC: StateIndex: ${stateIndex} for tokenId: ${tokenId} found in triple store blockchain: ${blockchain}`,
+                );
+                await this.repositoryModuleManager.createAssetSyncRecord(
+                    blockchain,
+                    contract,
+                    tokenId,
+                    stateIndex,
+                    ASSET_SYNC_PARAMETERS.STATUS.COMPLETED,
+                    true,
+                );
+                return;
+            }
+
+            const ual = this.ualService.deriveUAL(blockchain, contract, tokenId);
+            this.logger.debug(
+                `ASSET_SYNC: Fetching state index: ${stateIndex + 1} of ${
+                    assertionIds.length
+                } for asset with ual: ${ual}. blockchain: ${blockchain}`,
+            );
+            const assertionId = assertionIds[stateIndex];
+
+            const operationId = await this.operationIdService.generateOperationId(
+                OPERATION_ID_STATUS.GET.GET_START,
+            );
+
+            await Promise.all([
+                this.operationIdService.updateOperationIdStatus(
+                    operationId,
+                    blockchain,
+                    OPERATION_ID_STATUS.GET.GET_INIT_START,
+                ),
+
+                this.repositoryModuleManager.createAssetSyncRecord(
+                    blockchain,
+                    contract,
+                    tokenId,
+                    stateIndex,
+                    ASSET_SYNC_PARAMETERS.STATUS.IN_PROGRESS,
+                ),
+
+                this.repositoryModuleManager.createOperationRecord(
+                    this.getService.getOperationName(),
+                    operationId,
+                    OPERATION_STATUS.IN_PROGRESS,
+                ),
+            ]);
+
+            const hashFunctionId = CONTENT_ASSET_HASH_FUNCTION_ID;
+
+            this.logger.debug(
+                `ASSET_SYNC: Get for ${ual} with operation id ${operationId} initiated. blockchain: ${blockchain}`,
+            );
+
+            await this.commandExecutor.add({
+                name: 'networkGetCommand',
+                sequence: [],
+                delay: 0,
+                data: {
+                    operationId,
+                    id: ual,
+                    blockchain,
+                    contract,
+                    tokenId,
+                    state: assertionId,
+                    hashFunctionId,
+                    assertionId,
+                    assetSync: true,
+                    stateIndex,
+                    assetSyncInsertedByCommand: true,
+                },
+                transactional: false,
+            });
+
+            await this.operationIdService.updateOperationIdStatus(
+                operationId,
+                blockchain,
+                OPERATION_ID_STATUS.GET.GET_INIT_END,
+            );
+
+            let attempt = 0;
+            let getResult;
+            do {
+                await setTimeout(ASSET_SYNC_PARAMETERS.GET_RESULT_POLLING_INTERVAL_MILLIS);
+
+                getResult = await this.operationIdService.getOperationIdRecord(operationId);
+                attempt += 1;
+            } while (
+                attempt < ASSET_SYNC_PARAMETERS.GET_RESULT_POLLING_MAX_ATTEMPTS &&
+                getResult?.status !== OPERATION_ID_STATUS.FAILED &&
+                getResult?.status !== OPERATION_ID_STATUS.COMPLETED
+            );
+        } catch (error) {
+            this.logger.warn(
+                `ASSET_SYNC: Unable to sync tokenId: ${tokenId}, for contract: ${contract} state index: ${stateIndex} blockchain: ${blockchain}, error: ${error}`,
+            );
+            await this.repositoryModuleManager.updateAssetSyncRecord(
+                blockchain,
+                contract,
+                tokenId,
+                stateIndex,
+                ASSET_SYNC_PARAMETERS.STATUS.FAILED,
+                true,
+            );
+        }
+    }
 
     /**
      * Recover system from failure
