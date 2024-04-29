5f4597ba70635b8c49ce6e933fc3d762ae227ced
Aleksa Savic <savicaleksa83@gmail.com>
2024-04-29 16:01:29 +0200

    Fix lint issues

--- src/modules/repository/implementation/sequelize/migrations/20240429083058-create-paranet.js
+++ src/modules/repository/implementation/sequelize/migrations/20240429083058-create-paranet.js
@@ -1,37 +1,36 @@
-'use strict';
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Paranets', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.STRING
-      },
-      paranetId: {
-        type: Sequelize.STRING
-      },
-      kaCount: {
-        type: Sequelize.INTEGER
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Paranets');
-  }
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Paranets', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            name: {
+                type: Sequelize.STRING,
+            },
+            description: {
+                type: Sequelize.STRING,
+            },
+            paranetId: {
+                type: Sequelize.STRING,
+            },
+            kaCount: {
+                type: Sequelize.INTEGER,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    async down(queryInterface) {
+        await queryInterface.dropTable('Paranets');
+    },
 };
--- src/modules/repository/implementation/sequelize/models/paranet.js
+++ src/modules/repository/implementation/sequelize/models/paranet.js
@@ -1,26 +1,27 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Paranet extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+import { Model } from 'sequelize';
+
+export default (sequelize, DataTypes) => {
+    class Paranet extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate() {
+            // define association here
+        }
     }
-  }
-  Paranet.init({
-    name: DataTypes.STRING,
-    description: DataTypes.STRING,
-    paranetId: DataTypes.STRING,
-    kaCount: DataTypes.INTEGER
-  }, {
-    sequelize,
-    modelName: 'Paranet',
-  });
-  return Paranet;
+    Paranet.init(
+        {
+            name: DataTypes.STRING,
+            description: DataTypes.STRING,
+            paranetId: DataTypes.STRING,
+            kaCount: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            modelName: 'Paranet',
+        },
+    );
+    return Paranet;
 };
