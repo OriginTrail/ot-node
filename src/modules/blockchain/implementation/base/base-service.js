9fbba4f236c0de9a24abaaa6196454fbe7354181
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-06-20 13:56:41 +0200

    Add base-service implementation

---
+++ src/modules/blockchain/implementation/base/base-service.js
@@ -0,0 +1,25 @@
+import Web3Service from '../web3-service.js';
+import { BLOCK_TIME_MILLIS } from '../../../../constants/constants.js';
+
+class BaseService extends Web3Service {
+    constructor(ctx) {
+        super(ctx);
+
+        this.baseTokenTicker = 'BASE';
+        this.tracTicker = 'TRAC';
+    }
+
+    getBlockTimeMillis() {
+        return BLOCK_TIME_MILLIS.BASE;
+    }
+
+    async getGasPrice() {
+        return this.provider.getGasPrice();
+    }
+
+    async getAgreementScoreFunctionId() {
+        return 2;
+    }
+}
+
+export default BaseService;
