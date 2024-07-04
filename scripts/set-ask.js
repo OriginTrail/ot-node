93eefacdb16bcef64190f29a515729b21bebf74e
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-07-04 10:09:10 +0200

    Update set scripts

--- scripts/set-ask.js
+++ scripts/set-ask.js
@@ -22,15 +22,16 @@ const devEnvironment =
     process.env.NODE_ENV === NODE_ENVIRONMENTS.TEST;
 
 async function getGasPrice(gasPriceOracleLink, hubContractAddress, provider) {
-    if (!gasPriceOracleLink) {
-        return devEnvironment ? undefined : 8;
-    }
     try {
-        if (
-            hubContractAddress === '0x6C861Cb69300C34DfeF674F7C00E734e840C29C0' ||
-            hubContractAddress === '0x144eDa5cbf8926327cb2cceef168A121F0E4A299'
-        ) {
-            return provider.getGasPrice();
+        if (!gasPriceOracleLink) {
+            if (
+                hubContractAddress === '0x6C861Cb69300C34DfeF674F7C00E734e840C29C0' ||
+                hubContractAddress === '0x144eDa5cbf8926327cb2cceef168A121F0E4A299' ||
+                hubContractAddress === '0xaBfcf2ad1718828E7D3ec20435b0d0b5EAfbDf2c'
+            ) {
+                return provider.getGasPrice();
+            }
+            return devEnvironment ? undefined : 8;
         }
         let gasPrice;
         const response = await axios.get(gasPriceOracleLink);
