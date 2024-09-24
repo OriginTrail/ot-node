2acebe538a1018ad8ab2ced56b73ad02f2b7f085
Zvonimir Sculac <sculaczvonimir@yahoo.com>
2024-09-24 16:04:41 +0200

    Add rpc-router changes

--- src/controllers/rpc/rpc-router.js
+++ src/controllers/rpc/rpc-router.js
@@ -9,6 +9,7 @@ class RpcRouter {
         this.publishRpcController = ctx.publishRpcController;
         this.getRpcController = ctx.getRpcController;
         this.updateRpcController = ctx.updateRpcController;
+        this.updateParanetRpcController = ctx.updateParanetRpcController;
     }
 
     initialize() {
