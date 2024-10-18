ece93bc7dcc41525d12d84c6703cb52e5ffda1b9
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-10-18 14:42:25 +0200

    Add incrementParanetKaCount

--- src/service/get-service.js
+++ src/service/get-service.js
@@ -142,6 +142,8 @@ class GetService extends OperationService {
                         ? responseData.syncedAssetRecord?.privateAssertionId
                         : null;
 
+                const paranetId = this.paranetService.getParanetIdFromUAL(paranetUAL);
+                await this.repositoryModuleManager.incrementParanetKaCount(paranetId, blockchain);
                 await this.repositoryModuleManager.createParanetSyncedAssetRecord(
                     blockchain,
                     ual,
