f6f0d5e02f775f8a97ae5bdb639234b0dfb22282
Mihajlo Pavlovic <mihajlo.pavlovic@origin-trail.com>
2024-12-04 19:14:55 +0100

    fix update blockchain migration

--- src/modules/repository/implementation/sequelize/migrations/20241202214500-update-blockchain-table.js
+++ src/modules/repository/implementation/sequelize/migrations/20241202214500-update-blockchain-table.js
@@ -1,11 +1,17 @@
 export async function up({ context: { queryInterface } }) {
-    await queryInterface.renameColumn('blockchain', 'blockchain_id', 'blockchain');
+    const tableInfo = await queryInterface.describeTable('blockchain');
 
+    if (tableInfo.blockchain_id) {
+        await queryInterface.renameColumn('blockchain', 'blockchain_id', 'blockchain');
+    }
     await queryInterface.removeColumn('blockchain', 'contract');
 }
 
 export async function down({ context: { queryInterface, Sequelize } }) {
-    await queryInterface.renameColumn('blockchain', 'blockchain', 'blockchain_id');
+    const tableInfo = await queryInterface.describeTable('blockchain');
+    if (tableInfo.blockchain) {
+        await queryInterface.renameColumn('blockchain', 'blockchain', 'blockchain_id');
+    }
 
     await queryInterface.addColumn('blockchain', 'contract', {
         type: Sequelize.STRING,
