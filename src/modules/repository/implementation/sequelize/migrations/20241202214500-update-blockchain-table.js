27aaf3a4423ae5acfa1ea87ea72a60154466b5d9
Uladzislau Hubar <hubar.uladzislau@gmail.com>
2024-12-04 20:20:19 +0100

    Reverted back migration changes

--- src/modules/repository/implementation/sequelize/migrations/20241202214500-update-blockchain-table.js
+++ src/modules/repository/implementation/sequelize/migrations/20241202214500-update-blockchain-table.js
@@ -1,18 +1,11 @@
 export async function up({ context: { queryInterface } }) {
-    const tableInfo = await queryInterface.describeTable('blockchain');
+    await queryInterface.renameColumn('blockchain', 'blockchain_id', 'blockchain');
 
-    if (tableInfo.blockchain_id) {
-        await queryInterface.renameColumn('blockchain', 'blockchain_id', 'blockchain');
-    }
-    await queryInterface.removeIndex('blockchain', 'contract_index');
     await queryInterface.removeColumn('blockchain', 'contract');
 }
 
 export async function down({ context: { queryInterface, Sequelize } }) {
-    const tableInfo = await queryInterface.describeTable('blockchain');
-    if (tableInfo.blockchain) {
-        await queryInterface.renameColumn('blockchain', 'blockchain', 'blockchain_id');
-    }
+    await queryInterface.renameColumn('blockchain', 'blockchain', 'blockchain_id');
 
     await queryInterface.addColumn('blockchain', 'contract', {
         type: Sequelize.STRING,
